704205b68db4f5dcfb38d0634639e76397612edd
Won-Kyu Park <wkpark@gmail.com>
2003-04-03 08:43:06 +0000

    Initial revision

---
+++ wikismiley.php
@@ -0,0 +1,25 @@
+<?
+# $Id$
+$Globals[smiley]=array(
+    "X-("=> array(16, 16, 0, "smile/x-(.gif"),
+    ":D"=>  array(16, 16, 0, "smile/8-D.gif"),
+    "<:("=> array(15, 15, 0, "frown.gif"),
+    ":o"=>  array(16, 16, 0, "smile/8-O.gif"),
+    ":("=>  array(16, 16, 0, "smile/8-(.gif"),
+    ":)"=>  array(16, 16, 0, "smile/8).gif"),
+    ":-)"=> array(16, 16, 0, "smile/8-).gif"),
+    "B)"=>  array(16, 16, 0, "smile/B).gif"),
+    ":))"=> array(16, 16, 0, "smile/8-)).gif"),
+    ";)"=>  array(16, 16, 0, "smile/;-).gif"),
+    ";|"=>  array(16, 16, 0, "smile/;-l.gif"),
+    ":I"=>  array(16, 16, 0, "smile/8-I.gif"),
+    ":|"=>  array(16, 16, 0, "smile/8-l.gif"),
+    "|-|"=> array(16, 16, 0, "smile/l-l.gif"),
+    "|-I"=> array(16, 16, 0, "smile/l-I.gif"),
+    "/!\\"=>array(16, 16, 0, "alert.gif"),
+    "\\?/"=>array(16, 16, 0, "icon-question.gif"),
+    "<!>"=> array(15, 15, 0, "attention.gif"),
+    "(!)"=> array(15, 15, 0, "idea.gif"),
+);
+
+?>
