66e30137d2d60fa17d555b2e35509176f68ee7eb
Won-Kyu Park <wkpark@gmail.com>
2003-05-05 05:09:38 +0000

    correctly implement the dot action

--- wikilib.php
+++ wikilib.php
@@ -203,29 +203,50 @@ class User {
 
 
 function do_dot($formatter,$options) {
-  define(DEPTH,2);
-  define(LEAF,3);
+  define(DEPTH,3);
+  define(LEAFCOUNT,3);
 
-  function getLeafs($pagename,$tree,$node,$depth=0) {
+  function getLeafs($pagename,$node,$count=LEAFCOUNT) {
     $p= new WikiPage($pagename);
     $f= new Formatter($p);
     $links=$f->get_pagelinks();
     $links=explode("\n",$links);
     foreach ($links as $page) {
-      if (!$tree[$page] && $page) {
+      if (!$node[$page] && $page) {
         $p= new WikiPage($page);
         $f= new Formatter($p);
         $leafs=$f->get_pagelinks();
         if ($leafs) {
           $leafs=explode("\n",$leafs);
-          $tree[$page]=$leafs;
+          #$tree[$page]=$leafs;
+          $nodelink[$page]=sizeof($leafs);
         }
       }
     }
+    if (sizeof($nodelink) > $count) {
+      arsort($nodelink);
+      $nodelink=array_slice($nodelink,0,$count);
+    }
+    if ($nodelink)
+      $node[$pagename]=array_keys($nodelink);
   }
 
+  function makeTree($pagename,$node,$depth=DEPTH,$count=LEAFCOUNT) {
+    if ($depth > 0) {
+      $depth--;
+      getLeafs($pagename,&$node,$count);
+      if ($node[$pagename])
+        foreach($node[$pagename] as $leaf)
+          makeTree($leaf,&$node,$depth,$count);
+    }
+  }
 
-  getLeafs($options[page],&$tree,&$node);
+  #getLeafs($options[page],&$node);
+  if ($options[w]) $count=$options[w];
+  else $count=LEAFCOUNT;
+  if ($options[d]) $depth=$options[d];
+  else $depth=DEPTH;
+  makeTree($options[page],&$node,$depth,$count);
 
   header("Content-Type: text/plain");
   $visualtour=$formatter->link_url("VisualTour");
@@ -234,16 +255,13 @@ function do_dot($formatter,$options) {
 digraph G {
   URL="$visualtour"
   node [URL="$pageurl", 
-fontcolor=black, fontname=sandolkwangsu , fontsize=10]\n
+fontcolor=black, fontsize=10]\n
 HEAD;
-  if (is_array($tree)) {
-  while (list($pagename,$leaf) = each ($tree)) {
-     foreach ($leaf as $node) {
-       if ($node)
-         print "\"$pagename\" ->\"$node\";\n";
-     }
-  }
-  }
+  while (list($leafname,$leaf) = @each ($node)) {
+      foreach ($node[$leafname] as $leaf) {
+        print "\"$leafname\" ->\"$leaf\";\n";
+      }
+    }
   print "};\n";
 }
 
@@ -255,7 +273,7 @@ function do_highlight($formatter,$options) {
   $formatter->highlight=$options[value];
   $formatter->send_page();
   $args[editable]=1;
-  $formatter->send_footer($args,$options[timer]);
+  $formatter->send_footer($args,$options);
 }
 
 function do_DeletePage($formatter,$options) {
@@ -269,7 +287,7 @@ function do_DeletePage($formatter,$options) {
       $DBInfo->deletePage($page);
       $title = sprintf('"%s" is deleted !', $page->name);
       $formatter->send_header("",$options);
-      $formatter->send_title($title);
+      $formatter->send_title($title,"",$options);
       $formatter->send_footer();
       return;
     } else {
@@ -311,7 +329,7 @@ function do_RcsPurge($formatter,$options) {
     $formatter->send_header("",$options);
     $formatter->send_title($title);
     $args[noaction]=1;
-    $formatter->send_footer($args,$options[timer]);
+    $formatter->send_footer($args,$options);
     return;
   }
 #    unset($options);
@@ -337,7 +355,7 @@ function do_RcsPurge($formatter,$options) {
     printf("<h3>No version selected to purge '%s'</h3>",$options[page]);
   }
   $args[noaction]=1;
-  $formatter->send_footer($args,$options[timer]);
+  $formatter->send_footer($args,$options);
 }
 
 function do_fullsearch($formatter,$options) {
@@ -360,7 +378,7 @@ function do_fullsearch($formatter,$options) {
 	($ret[hits] == 1) ? 'page' : 'pages',
 	 $ret[all]);
   $args[noaction]=1;
-  $formatter->send_footer($args,$options[timer]);
+  $formatter->send_footer($args,$options);
 }
 
 function do_goto($formatter,$options) {
@@ -391,7 +409,7 @@ function do_LikePages($formatter,$options) {
   print $opts[extra];
   print $out;
   print $opts[extra];
-  $formatter->send_footer("",$options[timer]);
+  $formatter->send_footer("",$options);
 }
 
 function do_rss_rc($formatter,$options) {
@@ -522,7 +540,7 @@ function do_titlesearch($formatter,$options) {
 	($ret[hits] == 1) ? 'page' : 'pages',
 	 $ret[all]);
   $args[noaction]=1;
-  $formatter->send_footer($args,$options[timer]);
+  $formatter->send_footer($args,$options);
 }
 
 function do_uploadfile($formatter,$options) {
