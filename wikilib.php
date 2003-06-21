a17f2163d5a24a27913e3f9d40be1d6da70e5889
Won-Kyu Park <wkpark@gmail.com>
2003-06-21 16:54:02 +0000

    gnuplot, dot are factored out as plugins now

--- wikilib.php
+++ wikilib.php
@@ -232,70 +232,6 @@ class User {
   }
 }
 
-
-function do_dot($formatter,$options) {
-  define(DEPTH,3);
-  define(LEAFCOUNT,3);
-
-  function getLeafs($pagename,$node,$count=LEAFCOUNT) {
-    $p= new WikiPage($pagename);
-    $f= new Formatter($p);
-    $links=$f->get_pagelinks();
-    $links=explode("\n",$links);
-    foreach ($links as $page) {
-      if (!$node[$page] && $page) {
-        $p= new WikiPage($page);
-        $f= new Formatter($p);
-        $leafs=$f->get_pagelinks();
-        if ($leafs) {
-          $leafs=explode("\n",$leafs);
-          #$tree[$page]=$leafs;
-          $nodelink[$page]=sizeof($leafs);
-        }
-      }
-    }
-    if (sizeof($nodelink) > $count) {
-      arsort($nodelink);
-      $nodelink=array_slice($nodelink,0,$count);
-    }
-    if ($nodelink)
-      $node[$pagename]=array_keys($nodelink);
-  }
-
-  function makeTree($pagename,$node,$depth=DEPTH,$count=LEAFCOUNT) {
-    if ($depth > 0) {
-      $depth--;
-      getLeafs($pagename,&$node,$count);
-      if ($node[$pagename])
-        foreach($node[$pagename] as $leaf)
-          makeTree($leaf,&$node,$depth,$count);
-    }
-  }
-
-  #getLeafs($options[page],&$node);
-  if ($options[w]) $count=$options[w];
-  else $count=LEAFCOUNT;
-  if ($options[d]) $depth=$options[d];
-  else $depth=DEPTH;
-  makeTree($options[page],&$node,$depth,$count);
-
-  header("Content-Type: text/plain");
-  $visualtour=$formatter->link_url("VisualTour");
-  $pageurl=$formatter->link_url("\\N");
-  print <<<HEAD
-digraph G {
-  URL="$visualtour"
-  node [URL="$pageurl", 
-fontcolor=black, fontsize=10]\n
-HEAD;
-  while (list($leafname,$leaf) = @each ($node)) {
-      foreach ($node[$leafname] as $leaf) {
-        print "\"$leafname\" ->\"$leaf\";\n";
-      }
-    }
-  print "};\n";
-}
-
 function do_download($formatter,$options) {
   global $DBInfo;
 
@@ -998,7 +934,18 @@ function do_post_css($formatter,$options) {
   global $DBInfo;
   global $HTTP_COOKIE_VARS;
 
-  if ($options[id]=="Anonymous" && isset($options[user_css])) {
+  if ($options[clear]) {
+    if ($options[id]=='Anonymous') {
+      header("Set-Cookie: MONI_CSS=dummy; expires=Tuesday, 01-Jan-1999 12:00:00 GMT; Path=".get_scriptname());
+      $options[css_url]="";
+    } else {
+      # save profile
+      $udb=new UserDB($DBInfo);
+      $userinfo=$udb->getUser($options[id]);
+      $userinfo->info[css_url]="";
+      $udb->saveUser($userinfo);
+    }
+  } else if ($options[id]=="Anonymous" && isset($options[user_css])) {
      setcookie("MONI_CSS",$options[user_css],time()+60*60*24*30,get_scriptname());
      # set the fake cookie
      $HTTP_COOKIE_VARS[MONI_CSS]=$options[user_css];
@@ -1390,9 +1337,12 @@ function macro_Css($formatter="") {
 
   $out.="
     </select>&nbsp; &nbsp; &nbsp;
-    <input type='submit' name='save' value='Change CSS' /> &nbsp;
-</form>
-";
+    <input type='submit' name='save' value='Change CSS' /> &nbsp;";
+
+  $out.="
+    <input type='submit' name='clear' value='Clear CSS cookie' /> &nbsp;";
+
+  $out.="</form>\n";
   return $out;
 }
 
@@ -1579,9 +1529,9 @@ function macro_LikePages($formatter="",$args="",$opts=array()) {
   }
 
   if (!$start && !$end) {
-    preg_match("/^(.{2,4})/",$pname,$match);
-    $start=$match[1];
-    $s_len=strlen($start);
+    preg_match("/^(.{2,4})/",$args,$match);
+    $s_len=strlen($match[1]);
+    $start=_preg_escape($match[1]);
   }
 
   if (!$end) {
@@ -2427,73 +2377,6 @@ function processor_php($formatter="",$value="") {
 #  $highlighted="<pre style='color:white;background-color:black;'>".
 #               $highlighted."\n</pre>";
   return $highlighted;
-}
-
-function processor_gnuplot($formatter="",$value="") {
-  global $DBInfo;
-  #$gnuplot="gnuplot";
-  $gnuplot="/usr/local/bin/gnuplot_pm3d";
-  $vartmp_dir="/var/tmp";
-  $cache_dir="pds/GnuPlot";
-
-  #
-  if ($value[0]=='#' and $value[1]=='!')
-    list($line,$value)=explode("\n",$value,2);
-
-  $plt=$value;
-
-# a sample for testing
-#  $plt='
-#set term gif
-#!  ls
-#plot sin(x)
-#';
-
-  # normalize plt
-  $plt="\n".$plt."\n";
-  $plt=preg_replace("/\n\s*![^\n]+\n/","\n",$plt); # strip shell commands
-  $plt=preg_replace("/[ ]+/"," ",$plt);
-  $plt=preg_replace("/\nset?\s+(t|o|si).*\n/", "\n",$plt);
-  
-  #print "<pre>$plt</pre>";
-  
-  $uniq=md5($plt);
-
-  $outpath="$cache_dir/$uniq.png";
-
-  $src="
-  set size 0.5,0.6
-set term png
-set out '$outpath'
-$plt
-";
-
-  if (!file_exists($cache_dir)) {
-    umask(000);
-    mkdir($cache_dir,0777);
-    umask(022);
-  }
-
-  #if (1 || $formatter->refresh || !file_exists("$cache_dir/$uniq.png")) {
-  if ($formatter->refresh || !file_exists("$cache_dir/$uniq.png")) {
-
-     $flog=tempnam($vartmp_dir,"GNUPLOT");
-
-     $cmd= "$gnuplot 2>$flog";
-     $fp=popen($cmd,"w");
-     fwrite($fp,$src);
-  
-#   while($s = fgets($fp, 1024)) {
-#     $log.= $s;
-#   }
-     pclose($fp);
-     $log=join(file($flog),"");
-     unlink($flog);
-  
-     if ($log)
-        $log ="<pre style='background-color:black;color:gold'>$log</pre>\n";
-  }
-  return $log."<img src='$DBInfo->url_prefix/$cache_dir/$uniq.png' alt='gnuplot' />";
 }
 
 ?>
