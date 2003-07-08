012dc9c0727668d2a0874ecfbfa43e5607b5fff6
Won-Kyu Park <wkpark@gmail.com>
2003-07-08 23:38:02 +0000

    Initial revision

---
+++ plugin/processor/latex.php
@@ -0,0 +1,68 @@
+<?php
+// Copyright 2003 by Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPL see COPYING
+// a latex processor plugin for the MoniWiki
+//
+// Usage: {{{#!latex
+// $ \alpha $
+// }}}
+// $Id$
+
+function processor_latex($formatter="",$value="") {
+  global $DBInfo;
+  # site spesific variables
+  $latex="latex";
+  $dvips="dvips";
+  $convert="convert";
+  $vartmp_dir="/var/tmp";
+  $cache_dir="pds/LaTeX";
+  $option='-interaction=batchmode ';
+
+  if ($value[0]=='#' and $value[1]=='!')
+    list($line,$value)=explode("\n",$value,2);
+
+  if (!$value) return;
+
+  if (!file_exists($cache_dir)) {
+    umask(000);
+    mkdir($cache_dir,0777);
+  }
+
+  $tex=$value;
+
+  $uniq=md5($tex);
+
+  $src="\documentclass[10pt,notitlepage]{article}
+\usepackage{amsmath}
+\usepackage{amsfonts}
+%%\usepackage[all]{xy}
+\\begin{document}
+\pagestyle{empty}
+$tex
+\end{document}
+";
+
+  if ($formatter->refresh || !file_exists("$cache_dir/$uniq.png")) {
+     $fp= fopen("$vartmp_dir/$uniq.tex", "w");
+     fwrite($fp, $src);
+     fclose($fp);
+
+     $outpath="$cache_dir/$uniq.png";
+
+     # Unix specific FIXME
+     $cmd= "cd $vartmp_dir; $latex $option $uniq.tex >/dev/null";
+     system($cmd);
+
+     $cmd= "cd $vartmp_dir; $dvips -D 600 $uniq.dvi -o $uniq.ps";
+     system($cmd);
+
+     $cmd= "$convert -transparent white -crop 0x0 -density 120x120 $vartmp_dir/$uniq.ps $outpath";
+     system($cmd);
+
+     system("rm $vartmp_dir/$uniq.*");
+  }
+  return "<img class='tex' src='$DBInfo->url_prefix/$cache_dir/$uniq.png' alt='tex'".
+         "title=\"$tex\" />";
+}
+
+?>
