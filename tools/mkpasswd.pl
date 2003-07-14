02dfbcb892f86a3f0a7903f7d52c463dc776f6df
Won-Kyu Park <wkpark@gmail.com>
2003-07-14 22:38:08 +0000

    fixed to use localtime to get a proper seed

--- tools/mkpasswd.pl
+++ tools/mkpasswd.pl
@@ -3,11 +3,13 @@
 # wkpark@kldp.org
 # $Id$
 if ($ARGV[0]) {
-   $seed= `echo $ARGV[0] |md5sum | cut -d' ' -f1`;
+   $seed=localtime;
+   $seed= `echo $seed$ARGV[0] |md5sum | cut -d' ' -f1`;
    chop $seed;
    if (not $seed) {
       $seed=time ^ $$;
    }
+
    print crypt("$ARGV[0]",$seed)."\n";
 } else {
    print "Usage: perl mkpasswd.pl yourpassword\n";
