2ed2040a7a1f135c2b381c3db9b5cc2a090174bb
Won-Kyu Park <wkpark@gmail.com>
2003-07-24 23:39:29 +0000

    Initial revision

---
+++ plugin/format.php
@@ -0,0 +1,38 @@
+<?php
+// Copyright 2003 by Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPL see COPYING
+// a format plugin to connect with processors for the MoniWiki
+// vim:et:ts=2:
+//
+// $Id$
+
+function do_format($formatter,$options) {
+  $mimes=array('text/plain'=>'html','text/xml'=>'text_xml');
+  $mimetype=$options['mimetype'];
+  if (!$mimetype) $mimetype='text/plain';
+
+  $pi=$formatter->get_instructions();
+  if ($pi['#format']=='xsltproc') {
+    $options['title']= _("You can only export WikiMarkup to XML");
+    do_invalid($formatter,$options);
+  }
+  if (!$formatter->page->exists()) do_invalid($formatter,$options);
+
+  // Detect File type
+  else if (array_key_exists($mimetype,$mimes)) {
+    header("Content-type: ".$mimetype);
+    print $formatter->processor_repl($mimes[$mimetype],$formatter->page->get_raw_body());
+  } else {
+    $processor=str_replace("/","_",$mimetype);
+    header("Content-type: text/plain");
+
+    if (getProcessor($processor))
+      print $formatter->processor_repl($processor,$formatter->page->get_raw_body());
+    else
+      do_invalid($formatter,$options);
+  }
+
+  return;
+}
+
+?>
