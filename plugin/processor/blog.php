8fd6a06c56458921c2ba945834f693676af2cfad
Won-Kyu Park <wkpark@gmail.com>
2003-08-09 10:16:07 +0000

    more blog like

--- plugin/processor/blog.php
+++ plugin/processor/blog.php
@@ -28,28 +28,44 @@ function processor_blog($formatter,$value="",$options) {
     if ($date && $date[10] == 'T') {
       $date[10]=' ';
       $time=strtotime($date." GMT");
-      $date= date("m-d [h:i a]",$time);
+      $date= "@ ".date("m-d [h:i a]",$time);
       $anchor= date("d",$time);
       if ($date_anchor != $anchor) {
-        $datetag= "<div class='blog-date'>".date("M d, Y",$time)." <a name='$anchor' id='$anchor'></a><a class='purple' href='#$anchor'>#</a></div>";
+        $datetag= "<div class='blog-date'>".date("M d, Y",$time)." <a name='$anchor' id='$anchor'></a><a class='purple' href='#$anchor'>$formatter->purple_icon</a></div>";
         $date_anchor= $anchor;
       }
     }
     $md5sum=md5(substr($line,7));
-    if (!$options['noaction'])
-    $comment_action="<div class='blog-user'>&raquo; ".$formatter->link_tag($formatter->page->urlname,"?action=blog&amp;value=$md5sum",_("Add comment"))."</div>\n";
   }
 
   $src= rtrim($value);
 
   if ($src) {
     $options['nosisters']=1;
-    $temp=explode("----\n",$src);
-    $comments="";
-    if ($comments=array_slice($temp,1)) {
-      $comments=join("[[BR]]-''''''---[[BR]]",$comments);
-      $src=$temp[0];
+    list($src,$comments)=explode("----\n",$src,2);
+
+    $add_button= _("Add comment");
+    if ($comments) {
+      $count=sizeof(explode("----\n",$comments));
+
+      if ($options['noaction'] or $DBInfo->show_comments)
+        $comments=preg_replace("/----\n/","[[BR]]-''''''---[[BR]]",$comments);
+      else {
+        $comments='';
+        $add_button=($count == 1) ? _("%d comment"):_("%d comments");
+        $add_button=sprintf($add_button,$count);
+      }
     }
+
+    if (!$options['noaction'] and $md5sum) {
+      $action= $formatter->link_tag($formatter->page->urlname,"?action=blog&amp;value=$md5sum",$add_button);
+      if (getPlugin('SendPing'))
+        $action.= ' | '.$formatter->link_tag($formatter->page->urlname,"?action=sendping&amp;value=$md5sum",_("send ping"));
+    }
+
+    if ($action)
+      $action="<div class='blog-action'>&raquo; ".$action."</div>\n";
+
     ob_start();
     $formatter->send_page($src,$options);
     $msg= ob_get_contents();
@@ -68,13 +84,13 @@ function processor_blog($formatter,$value="",$options) {
     #$tag=normalize($title);
     $tag=$md5sum;
     if ($tag[0]=='%') $tag="n".$tag;
-    $purple="<a class='purple' href='#$tag'>#</a>";
+    $purple="<a class='purple' href='#$tag'>$formatter->purple_icon</a>";
     $title=preg_replace("/(".$formatter->wordrule.")/e",
                         "\$formatter->link_repl('\\1')",$title);
     $out.="<div class='blog-title'><a name='$tag' id='$tag'></a>$title $purple</div>\n";
   }
-  $out.="<div class='blog-user'>Submitted by $user @ $date</div>\n".
-    "<div class='blog-comment'>$msg$comments$comment_action</div>\n".
+  $out.="<div class='blog-user'>Submitted by $user $date</div>\n".
+    "<div class='blog-comment'>$msg$comments$action</div>\n".
     "</div>\n";
   return $out;
 }
