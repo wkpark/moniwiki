bd604b3f0ea57cb46fe5dfd6c6f2d861e56a9bb9
Won-Kyu Park <wkpark@gmail.com>
2003-08-15 14:06:23 +0000

    redesigned

--- theme/sampleleft/footer.php
+++ theme/sampleleft/footer.php
@@ -5,4 +5,8 @@
   if ($lastedit)
     print "last modified $lastedit $lasttime<br />";
 ?>
+<center>
+<font class='t8'>Since 08-06-2002</font><br />
+<font class='t8'>Copyright under <b>GNU</b> <a href="GFDL"><b>GFDL</b></a></font>
+</center>
 </div>
--- theme/sampleleft/header.php
+++ theme/sampleleft/header.php
@@ -13,31 +13,34 @@ body {
 -->
 </style>
 <div id='wikiHeader'>
-<div style='text-align:right;font-size:8px;'><a href='?action=edit'>edit</a></div>
+<img alt='ChemWiki' src='<?=$this->url_prefix?>/imgs/wikiwiki.gif' />
+<?=$title?>
 </div>
+<div style='background-color:#a8a8a8;text-align:right;font-size:8px;'><?=$icons?><?=$rss_icon?></div>
 <?=$msg?>
 <table border='0' cellpadding='0' cellspacing='0' width='100%'><tr valign='top'>
 <td id='wikiMenu' width='200'>
 <table border='0' width='100%' cellpadding='0' cellspacing='0'><tr>
-<td>&nbsp;<font style='font-family:Georgia,Lucida;font-size:22px;color:white;'><b>Moni Wiki</b></font></td></tr>
 <tr><td align='right'><?=$goto_form?></td></tr>
 </table>
 <?
+print '<div style="padding-left:10px;font-size:12px">';
+print "$menu<br />";
+print "</div>";
+print "<hr />\n";
+print '<div style="padding-left:10px;font-size:12px">';
+$args['editable']=1;
+print $this->get_actions($args,$options);
+print "<br />";
+print $this->link_tag("","?action=randompage","RandomPage");
+print '</div>';
+print "<hr />\n";
 print '<div style="font-size:10px">';
 if ($options['id']=='Anonymous')
   print macro_calendar($this,"blog,noweek",'Blog');
 else
   print macro_calendar($this,"blog,noweek",$options['id']);
 print '</div>';
-print "<br />\n";
-print '<font style="font-size:14px">';
-print "$menu<br />";
-print $this->link_tag("","?action=randompage","RandomPage");
-print "<br />\n";
-print "<br />\n";
-$args['editable']=1;
-print $this->get_actions($args,$options);
-print '</font>';
 
 ?>
 </td>
