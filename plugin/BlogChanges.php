e54f840eeae4afc00cb92f26d7ff2a48c2421f32
Won-Kyu Park <wkpark@gmail.com>
2003-08-17 12:59:15 +0000

    enhanced and simplified: summary option added

--- plugin/BlogChanges.php
+++ plugin/BlogChanges.php
@@ -1,12 +1,12 @@
 <?php
 // Copyright 2003 by Won-Kyu Park <wkpark at kldp.org>
 // All rights reserved. Distributable under GPL see COPYING
-// rss_blog action plugin for the MoniWiki
+// a BlogChanges action plugin for the MoniWiki
 //
 // $Id$
 
 class Blog_cache {
-  function get_blogs($date) {
+  function get_blogs() {
     global $DBInfo;
 
     $blogs=array();
@@ -15,34 +15,101 @@ class Blog_cache {
 
     while ($file = readdir($handle)) {
       if (is_dir($DBInfo->cache_dir."/blog/".$file)) continue;
-      if (preg_match("/^$date/",$file))
-        $blogs[] = $file;
+      $blogs[] = $file;
     }
     closedir($handle);
     return $blogs;
   }
 
-  function get_all($date) {
+  function get_simple($blogs,$date) {
     global $DBInfo;
-    $all=Blog_cache::get_blogs($date);
-    $lines=array();
-    foreach ($all as $stamped_blog) {
-      $fname=$DBInfo->cache_dir."/blog/".$stamped_blog;
+    $rule="/^($date\d*)".'_2e('.join('|',$blogs).')$/';
+    $logs=array();
+    $handle = @opendir($DBInfo->cache_dir."/blogchanges");
+    if (!$handle) return array();
 
-      #strip datestamp
-      list($datestamp,$blog)=explode('_2e',$stamped_blog,2);
-      $pagename=$DBInfo->keyToPagename($blog);
-      $items=file($fname);
-      foreach ($items as $line) {
-        list($author,$datestamp,$dummy)=explode(' ',$line);
-        $datestamp[10]=' ';
-        $timestamp= strtotime($datestamp." GMT");
-        $datestamp= date("Ymd",$timestamp);
-        if (preg_match("/^$date/",$datestamp))
-          $lines[]=$pagename." ".rtrim($line);
+    while ($file = readdir($handle)) {
+      $fname=$DBInfo->cache_dir."/blogchanges/".$file;
+      if (is_dir($fname)) continue;
+      if (preg_match($rule,$file,$match)) {
+        $datestamp=$match[1];
+        $blog=$match[2];
+        $pagename=$DBInfo->keyToPagename($blog);
+
+        $items=file($fname);
+        foreach ($items as $line) {
+          list($author,$datestamp,$dummy)=explode(' ',$line);
+          $datestamp[10]=' ';
+          $timestamp= strtotime($datestamp." GMT");
+          $datestamp= date("Ymd",$timestamp);
+          $logs[]=explode(' ',$pagename." ".rtrim($line),4);
+        }
       }
     }
-    return $lines;
+    return $logs;
+  }
+
+  function get_rc_blogs($date) {
+    global $DBInfo;
+    $all=Blog_cache::get_blogs();
+    $rule="/^($date\d*)".'_2e('.join('|',$all).')$/';
+    $blogs=array();
+    $handle = @opendir($DBInfo->cache_dir."/blogchanges");
+    if (!$handle) return array();
+
+    while ($file = readdir($handle)) {
+      $fname=$DBInfo->cache_dir."/blogchanges/".$file;
+      if (is_dir($fname)) continue;
+      if (preg_match($rule,$file,$match))
+        $blogs[]=$match[2];
+    }
+    return array_unique($blogs);
+  }
+
+  function get_summary($blogs,$date) {
+    global $DBInfo;
+
+    if (!$blogs) return array();
+
+    $check=strlen($date);
+    if (($check < 4) or !preg_match('/^\d+/',$date)) $date=date('Y\-m');
+    else {
+      if ($check==6) $date=substr($date,0,4).'\-'.substr($date,4);
+      else if ($check==8) $date=substr($date,0,4).'\-'.substr($date,4,2).'\-'.substr($date,6);
+      else if ($check!=4) $date=date('Y\-m');
+    }
+
+    $entries=array();
+    $logs=array();
+
+    foreach ($blogs as $blog) {
+      $pagename=$DBInfo->keyToPagename($blog);
+      $page=$DBInfo->getPage($pagename);
+
+      $raw=$page->get_raw_body();
+      $temp= explode("\n",$raw);
+
+      foreach ($temp as $line) {
+        if (!$state) {
+          if (preg_match("/^({{{)?#!blog\s([^ ]+\s($date"."[^ ]+)\s.*)$/",$line,$match)) {
+            $entry=explode(" ",$pagename." ".$match[2],4);
+            if ($match[1]) $endtag='}}}';
+            $state=1;
+          }
+          continue;
+        }
+        if (preg_match("/^$endtag$/",$line)) {
+          $state=0;
+          $temp=explode("----\n",$summary,2);
+          $entry[]=$temp[0];
+          $entries[]=$entry;
+          $summary='';
+          continue;
+        }
+        $summary.=$line."\n";
+      }
+    }
+    return $entries;
   }
 }
 
@@ -70,11 +137,12 @@ function macro_BlogChanges($formatter,$value,$options='') {
 
   $opts=explode(",",$value);
 
-  if ($options['date']) {
-    $date=$options['date'];
-    # check error and set default value
-    if (!preg_match('/^\d+$/',$date)) $date=date('Ym');
-  } else $date=date('Ym'); # default: show BlogChages monthly
+  $options['date']=$_GET['date'];
+
+  # check error and set default value
+  # default: show BlogChages monthly
+  if (!$options['date'] or !preg_match('/^\d+$/',$options['date'])) $date=date('Ym');
+  else $date=$options['date'];
 
   if (strlen($date)==6) {
     $pre_month=intval(substr($date,4))-1;
@@ -85,28 +153,20 @@ function macro_BlogChanges($formatter,$value,$options='') {
     }
     $pre_date=$pre_year.sprintf('%02d',$pre_month);
   }
-  
-  if (in_array('all',$opts)) {
-    $lines=Blog_cache::get_all($date);
-    $logs=array();
-    foreach ($lines as $line) $logs[]=explode(" ",$line,4);
-    #uniq($logs);
-    usort($logs,'BlogCompare');
-  } else {
-    if ($value and $DBInfo->hasPage($value)) {
-      $p=$DBInfo->getPage($value);
-      $raw_body=$p->get_raw_body();
-    } else
-      $raw_body=$formatter->page->get_raw_body();
-    $temp= explode("\n",$raw_body);
 
-    $logs=array();
-    foreach ($temp as $line) {
-      if (preg_match("/^{{{#!blog (.*)$/",$line,$match)) {
-        $logs[]=explode(" ",$options['page']." ".$match[1],4);
-      }
-    }
-  }
+  if (in_array('all',$opts)) {
+    if (in_array('summary',$opts))
+      $blogs=Blog_cache::get_rc_blogs($date);
+    else
+      $blogs=Blog_cache::get_blogs();
+  } else
+    $blogs=array($DBInfo->pageToKeyname($formatter->page->name));
+
+  if (in_array('summary',$opts))
+    $logs=Blog_cache::get_summary($blogs,$date);
+  else
+    $logs=Blog_cache::get_summary($blogs,$date);
+  usort($logs,'BlogCompare');
 
   if (in_array('simple',$opts)) {
     $bra="";
@@ -125,15 +185,16 @@ function macro_BlogChanges($formatter,$value,$options='') {
   if (!in_array('nouser',$opts))
     $template.='by $user';
 
-  $template.='</span>$sep\n";';
+  if (in_array('summary',$opts))
+    $template.='</span><div class=\"blog-summary\">$summary</div>$sep\n";';
+  else
+    $template.='</span>$sep\n";';
     
   $time_current= time();
   $items="";
 
-  #if (!$logs) return "";
-
   foreach ($logs as $log) {
-    list($page, $user,$date,$title)= $log;
+    list($page, $user,$date,$title,$summary)= $log;
     $url=qualifiedUrl($formatter->link_url(_urlencode($page)));
 
     if (!$opts['nouser'] and $user and $DBInfo->hasPage($user))
@@ -148,14 +209,14 @@ function macro_BlogChanges($formatter,$value,$options='') {
     $time=strtotime($date." GMT");
     $date= date("m-d [h:i a]",$time);
 
-    #$items.="$bullet<a href='$url#$tag'>$title</a> <span class='blog-user'>@ $date </span>$sep\n";
     eval($template);
     $items.=$out;
   }
   $url=qualifiedUrl($formatter->link_url($DBInfo->frontpage));
 
   # make pnut
-  $pnut=$formatter->link_to("?action=blogchanges&amp;mode=$value&amp;date=$pre_date",'&laquo; '._("Previous"));
+  $pnut=$formatter->link_to("?date=$pre_date",'&laquo; '._("Previous"));
+  #$pnut=$formatter->link_to("?action=blogchanges&amp;mode=$value&amp;date=$pre_date",'&laquo; '._("Previous"));
   return $bra.$items.$cat.$pnut;
 }
 ?>
