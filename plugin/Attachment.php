a37b51297a5ec2530ba9db85941515d07f8a1322
Won-Kyu Park <wkpark@gmail.com>
2003-10-11 21:29:18 +0000

    enhanced macro_attachment() to import attachments from another pages

--- plugin/Attachment.php
+++ plugin/Attachment.php
@@ -1,7 +1,7 @@
 <?php
 // Copyright 2003 by Won-Kyu Park <wkpark at kldp.org>
 // All rights reserved. Distributable under GPL see COPYING
-// a simple Img macro plugin for the MoniWiki
+// a Attachment macro plugin for the MoniWiki
 // vim:et:ts=2:
 //
 // Usage: [[Attachment(filename)]]
@@ -10,18 +10,30 @@
 
 function macro_Attachment($formatter,$value) {
   global $DBInfo;
-  $key=$DBInfo->pageToKeyname($formatter->page->name);
+  if (($dummy=strtok($value,',:')) and $DBInfo->hasPage($dummy)) {
+    $key=$DBInfo->pageToKeyname($dummy);
+    $value=strtok('');
+    $pagename=$dummy;
+  } else {
+    $pagename=$formatter->page->name;
+    $key=$DBInfo->pageToKeyname($formatter->page->name);
+  }
   $upload_file=$DBInfo->upload_dir."/$key/$value";
 
   if (file_exists($upload_file)) {
-    $url=$formatter->link_url($formatter->page->urlname,"?action=download&amp;value=$value");
-    if (preg_match("/\.(png|gif|jpeg|jpg)$/",$upload_file))
+    $url=$formatter->link_url(_urlencode($pagename),"?action=download&amp;value=$value");
+    if (preg_match("/\.(png|gif|jpeg|jpg)$/i",$upload_file))
       return "<span class=\"attach\"><img src='$url' alt='$value' /></span>";
     else
       return "<span class=\"attach\"><img align='middle' src='$DBInfo->imgs_dir/uploads-16.png' />".
         $formatter->link_to("?action=download&amp;value=$value",$value).'</span>';
   }
-  return '<span class="attach">'.$formatter->link_to("?action=UploadFile&amp;rename=$value",sprintf(_("Upload new Attachment \"%s\""),$value)).'</span>';
+  if ($pagename == $formatter->page->name)
+    return '<span class="attach">'.$formatter->link_to("?action=UploadFile&amp;rename=$value",sprintf(_("Upload new Attachment \"%s\""),$value)).'</span>';
+
+  $p=$DBInfo->getPage($pagename);
+  $f=new Formatter($p);
+    return '<span class="attach">'.$f->link_to("?action=UploadFile&amp;rename=$value",sprintf(_("Upload new Attachment \"%s\" on the \"%s\""),$value, $pagename)).'</span>';
 }
 
 ?>
