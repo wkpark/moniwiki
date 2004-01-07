0bc497f738f7caae75ee85ab498f9052e3893d6a
Won-Kyu Park <wkpark@gmail.com>
2004-01-07 01:10:34 +0000

    add new action plugin to get rid of #300091

---
+++ plugin/rcs.php
@@ -0,0 +1,49 @@
+<?php
+// Copyright 2003 by Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPL see COPYING
+// a rcs action plugin for the MoniWiki
+//
+// $Id$
+
+function do_rcs($formatter,$options) {
+  global $DBInfo;
+
+  $supported=array('-kkv','-kk');
+  
+  if (isset($options['param'])) {
+    if ($DBInfo->hasPage($formatter->page->name) and in_array($options['param'],$supported)) {
+      $key=$DBInfo->getPageKey($formatter->page->name);
+      system( "rcs $options[param] $key" );
+      $title = sprintf(_("Change options for \"%s\""), $formatter->page->name);
+      $formatter->send_header("",$options);
+      $formatter->send_title($title,"",$options);
+      $formatter->send_footer("",$options);
+      return;
+    } else {
+      $title = sprintf(_("Fail to rcs \"%s\" !"), $formatter->page->name);
+    }
+  }
+  if (!$title)
+    $title = sprintf(_("Change options for \"%s\" ?"), $options['page']);
+  $formatter->send_header("",$options);
+  $formatter->send_title($title,"",$options);
+  print "<form method='post'>
+<table border='0'>\n";
+  if ($DBInfo->security->is_protected("rcs",$options))
+    print "<tr><td align='right'>"._("Password").": </td><td><input type='password' name='passwd' /> ".
+    _("Only WikiMaster can execute rcs");
+  print "<tr>";
+  print "<td><b>rcs</b> <select name='param' />\n";
+  print "<option value='-kk'>-kk</option>\n";
+  print "<option value='-kkv'>-kkv</option>\n</select>\n</td>\n";
+  print "<td><input type='submit' name='button_rcs' value='apply' /></td>";
+  print "</td></tr>\n";
+
+  print "
+</table>
+    <input type=hidden name='action' value='rcs' />
+    </form>";
+#  $formatter->send_page();
+  $formatter->send_footer("",$options);
+}
+?>
