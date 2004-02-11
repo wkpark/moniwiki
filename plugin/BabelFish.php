670c5105455e62b20b48b9e8a4a68f68eae256c8
Won-Kyu Park <wkpark@gmail.com>
2004-02-11 08:47:31 +0000

    more fixes

--- plugin/BabelFish.php
+++ plugin/BabelFish.php
@@ -8,9 +8,11 @@
 // $Id$
 
 #<script language="JavaScript1.2" src="http://www.altavista.com/r?entr"></script>
-function macro_BabelFish($formatter,$value) {
+function macro_BabelFish($formatter,$value,$ret=array()) {
   $langs=array('ko','ja','en','de','fr','it','es','pt','zh');
-  $supported=array('ko_en','en_ko','en_de','en_fr');
+  $supported=array('ko_en','en_ko','en_de','en_fr','en_pt','en_it','en_es',
+                   'en_ja','en_zh','fr_en','es_en','pt_en','it_en','zh_en',
+                   'fr_de','de_en','de_fr','ja_en','ru_en');
   $msg=_("BabelFish Translation");
 
   if (!$value)
@@ -28,29 +30,33 @@ EOF;
 
   $lp=$from."_".$to;
   $URL=qualifiedUrl($formatter->link_url($formatter->page->urlname));
-  $TR="http://babelfish.altavista.com/babelfish/urlload?tt=url";
+  #$TR="http://babelfish.altavista.com/babelfish/urlload?tt=url";
+  $TR="http://babelfish.altavista.com/babelfish/tr?doit=done";
+  #$TR="http://babelfish.altavista.com/babelfish/tr?doit=done&urltext=http://chemie.skku.ac.kr/wiki/wiki.php/BabelFishMacro&lp=en_ja
   if (in_array($lp,$supported)) {
     $URL=urlencode($URL);
     $TR.="&lp=$lp";
   } else {
-    // XXX
-    // not supported by http://babelfish.altavista.com/
+    // XXX not supported by http://babelfish.altavista.com/
     $lp=$from.'_en';
     $URL=urlencode($TR."&lp=$lp&url=$URL");
     $lp='en_'.$to;
     $TR.="&lp=$lp";
   }
+  $goto=$TR.'&url='.$URL;
   return <<<EOF
-<img src='$formatter->imgs_dir/$from-16.png' /> <a href="$TR&url=$URL"><img border='0' src='$formatter->imgs_dir/fishloop.gif' title='$msg' alt='BabelFish@altavista' /></a><img src='$formatter->imgs_dir/$to-16.png' />
+<img src='$formatter->imgs_dir/$from-16.png' /> <a href="$goto"><img border='0' src='$formatter->imgs_dir/fishloop.gif' title='$msg' alt='BabelFish@altavista' /></a><img src='$formatter->imgs_dir/$to-16.png' />
 EOF;
 
 }
 
 function do_babelfish($formatter,$options) {
-  $formatter->send_header();
-  $formatter->send_title();
-  $ret= macro_BabelFish($formatter,$options['value']);
-  $formatter->send_page($ret);
+  global $DBInfo;
+
+  $formatter->send_header('',$options);
+  $formatter->send_title('','',$options);
+  print macro_BabelFish($formatter,$options['value']);
+  $formatter->send_page();
   $formatter->send_footer("",$options);
   return;
 }
