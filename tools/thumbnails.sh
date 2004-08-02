133a822681af4e1a8888946a55443adf65949269
Won-Kyu Park <wkpark@gmail.com>
2004-08-02 02:00:01 +0000

    fixed #300340 by Sung-Hyuk Do

--- tools/thumbnails.sh
+++ tools/thumbnails.sh
@@ -7,15 +7,15 @@ DIR=$1
 
 #SIZE=$2
 
-IMGS=`find $1`
+IMGS=`find $1 -maxdepth 1`
 
 echo -e "*** Make thumbnails ***"
-if [ ! -f $1/thumbnails ]; then
+if [ ! -d $1/thumbnails ]; then
   mkdir $1/thumbnails
 fi
 for X in $IMGS; do
   name=`basename $X`; dir=`dirname $X`;
-  isimg=`file $X | grep -i 'image'`; [ "x$isimg" == "x" ] && continue;
+  isimg=`file $X | grep -i 'image'`; [ "x$isimg" = "x" ] && continue;
   if [ ! -f $dir/thumbnails/$name ]; then
     echo -n " *** "
     echo "convert -scale 300x200 $X $dir/thumbnails/$name"
