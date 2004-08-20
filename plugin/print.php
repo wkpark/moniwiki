072a032023ee9dd8741a380873cbb8a2d6c1e49f
Won-Kyu Park <wkpark@gmail.com>
2004-08-20 11:11:11 +0000

    add some ID tags for CSS (printHeader, printFooter etc) show external urls with a internal $external_on variable

--- plugin/print.php
+++ plugin/print.php
@@ -10,8 +10,22 @@ function do_print($formatter,$options) {
   global $DBInfo;
   $options['css_url']=$DBInfo->url_prefix."/css/print.css";
   $formatter->send_header("",$options);
+  print "<div id='printHeader'>";
   print "<h2>$options[page]</h2>";
+  print "</div>";
+  print "<div id='wikiContent'>";
+  $formatter->external_on=1;
   $formatter->send_page();
+  print "</div></div>";
+  print "<div id='printFooter'>";
+  print sprintf(_("Retrieved from %s"),
+    qualifiedUrl($formatter->link_url($formatter->page->name))).'<br/>';
+  if ($mtime=$formatter->page->mtime()) {
+    $lastedit=date("Y-m-d",$mtime);
+    $lasttime=date("H:i:s",$mtime);
+    print sprintf(_("last modified %s %s"),$lastedit,$lasttime);
+  }
+  print "</div></body></html>";
   return;
 }
 
