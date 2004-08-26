21359adc438c7089883e2a1632aa91996050ed1e
Won-Kyu Park <wkpark@gmail.com>
2004-08-26 10:40:00 +0000

    fix some CIDR related bug

--- lib/checkip.php
+++ lib/checkip.php
@@ -1,14 +1,16 @@
 <?php
-// http://kr.php.net/ip2long/
-// made by wkpark 2003/11/25
+// Copyright 2003-2004 Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPL see COPYING
+//
+// See also http://kr.php.net/ip2long/
 // 
 
-function is_valid($network)
+function is_valid_network($network)
 {
-    $nums = explode(".", $network);
+    $nums = explode('.', $network);
     if (sizeof($nums) != 4)
         return false;
-    if (!preg_match("/^(\d{1,3}\.){3}\d{1,3}$/", $network))
+    if (!preg_match('/^(\d{1,3}\.){3}\d{1,3}$/', $network))
         return false;
     foreach ($nums as $num)
         if ($num > 255)
@@ -17,29 +19,30 @@ function is_valid($network)
 }
 
 function check_ip($rules,$ip) {
-    if (!$rules or !$ip) return true; // do not ckeck
+    if (!$rules or !$ip) return false; // do not ckeck
     $ip = ip2long($ip);
 
-    $rules = explode(":",$rules);
+    $rules = explode(':',$rules);
     foreach ($rules as $rule)
     {
-        list($network,$netmask)=explode("/",$rule);
-        if (!is_valid($network)) continue; // ignore error
+        list($network,$netmask)=explode('/',$rule);
+        if (!is_valid_network($network)) continue; // ignore error
 
         $network = ip2long($network);
 
         if ($netmask) {
             // echo "$ip ;$netmask; $network\n";
 
-            if(is_valid($netmask))
+            if(is_valid_network($netmask))
                 $netmask = ip2long($netmask);
             else if ($netmask >= 0 && $netmask <= 32)
-                $netmask = (65535 << (32 - $netmask)) & 65535;
+                $netmask = 0xffffffff << (32 - $netmask);
             else
                 continue; // ignore error
 
             // echo "$ip ;$netmask; $network\n";
-
+            // print long2ip($netmask);
+            // echo sprintf("%u", ip2long($ip));
             if(($ip & $netmask) == ($network & $netmask))
                 return true;
         } else if ($ip == $network) {
