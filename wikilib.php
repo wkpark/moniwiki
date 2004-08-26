893e9935cc5efe07357201442616e8a18f1e5337
Won-Kyu Park <wkpark@gmail.com>
2004-08-26 13:07:04 +0000

    fix last changes

--- wikilib.php
+++ wikilib.php
@@ -18,10 +18,9 @@ function find_needle($body,$needle,$exclude='',$count=0) {
   $lines=explode("\n",$body);
   $out="";
   $matches=preg_grep("/($needle)/i",$lines);
-  if ($exclude) {
-    $matches=preg_grep("/($exclude)/i",$matches,PREG_GREP_INVERT);
-    if ($matches) return '';
-  }
+  if ($exclude)
+    if (preg_grep("/($exclude)/i",$matches)) return '';
+
   if (count($matches) > $count) $matches=array_slice($matches,0,$count);
   foreach ($matches as $line) {
     $line=preg_replace("/($needle)/i","<strong>\\1</strong>",str_replace("<","&lt;",$line));
