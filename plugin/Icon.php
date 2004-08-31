60cf59c4bcaab6a71281c4ff55599f8947fa2fa4
Won-Kyu Park <wkpark@gmail.com>
2004-08-31 15:52:10 +0000

    support icon themes: a recent MoinMoin feature adapted

--- plugin/Icon.php
+++ plugin/Icon.php
@@ -8,10 +8,15 @@
 function macro_Icon($formatter,$value='',$extra='') {
   global $DBInfo;
 
-  $realdir=basename($DBInfo->imgs_dir);
-  $dir=strtok($value,'-');
-  $img=strtok('');
-  if (is_dir($realdir.'/'.$dir)) $value=$dir.'/'.$img;
+  if (strpos($value,'-')) {
+    $dir=strtok($value,'-');
+    print $dir;
+    $realdir=basename($DBInfo->imgs_dir);
+    $img=strtok('');
+    if (is_dir($realdir.'/'.$dir)) $value=$dir.'/'.$img;
+  } else if (! preg_match('/\.(gif|png|jpg|jpeg)$/',$value)) {
+    $value=$DBInfo->iconset.'/'.$value.'.png';
+  }
 
   $out=$DBInfo->imgs_dir."/$value";
   $out="<img src='$out' border='0' alt='icon' align='middle' />";
