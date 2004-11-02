c5187cdb6904718ab851fec39a3e6749381347b1
Won-Kyu Park <wkpark@gmail.com>
2004-11-02 18:01:31 +0000

    more fixes

--- plugin/Bar.php
+++ plugin/Bar.php
@@ -11,7 +11,7 @@ function macro_Bar($formatter,$value) {
     global $DBInfo;
 
     $imgdir=$DBInfo->imgs_dir;
-    $iconset='red';
+    $iconset='blue';
     $full_width=0;
     $notext=0;
 
@@ -27,26 +27,32 @@ function macro_Bar($formatter,$value) {
         $val=substr($dum,0,-1);
         if ($val > 100.0) $val=100;
     } else {
+        $p=strpos($dum,'/'); # parse 10/80
+        if ($p !== false)
+            $dum=((int)strtok($dum,'/'))/((int)strtok(''));
+
         if ($dum > 1.0) $val=100;
         else $val=$dum*100.0;
     }
 
     $ival=0;
     if ($val < 100.0) $ival=100.0 - $val;
-    $img="<div style='white-space: nowrap'><img src='$imgdir/vote/$iconset/b_leftbar.gif' align='middle' />";
-    $img.="<img src='$imgdir/vote/$iconset/b_mainbar.gif' ".
+    $img="<span style='white-space: nowrap'>";
+    $img.="<img src='$imgdir/vote/$iconset/leftbar.gif' align='middle' />";
+    $img.="<span style='white-space: nowrap'>";
+    $img.="<img src='$imgdir/vote/$iconset/mainbar.gif' ".
         "height='14' width='$val%' align='middle' />";
     if ($full_width && $ival != 0) {
-        $img.="<img src='$imgdir/vote/$iconset/mainbar.gif' ".
-            " height='14' width='$ival%' align='middle' />";
-        $img.="<img src='$imgdir/vote/$iconset/rightbar.gif' align='middle' />";
-    } else {
+        $img.="<img src='$imgdir/vote/$iconset/b_mainbar.gif' ".
+            " height='14' width='$ival%' align='middle' /></span>";
         $img.="<img src='$imgdir/vote/$iconset/b_rightbar.gif' align='middle' />";
+    } else {
+        $img.="</span><img src='$imgdir/vote/$iconset/rightbar.gif' align='middle' />";
     }
     $state=((int)$val).'%';
     if (!$notext)
-        $img.=' '.$state.'</div>';
-
+        $img.=' '.$state;
+    $img.='</span>';
     return $img;
 }
 
