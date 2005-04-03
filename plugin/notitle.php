c4d44f6d473f2ae7e4d9779ab967c501f155d356
Won-Kyu Park <wkpark@gmail.com>
2005-04-03 09:15:02 +0000

    add a new plugin for the FreeFeel wiki :)

---
+++ plugin/notitle.php
@@ -0,0 +1,27 @@
+<?php
+// Copyright 2005 Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPL see COPYING
+// a notitle action plugin
+//
+// $Id$
+
+function do_notitle($formatter,$options) {
+    global $DBInfo;
+
+    $formatter->sister_on=0;
+    #$options['css_url']=$DBInfo->url_prefix."/css/print.css";
+    $formatter->send_header("",$options);
+    #print "<div id='printHeader'>";
+    #print "<h2>$options[page]</h2>";
+    #print "</div>";
+    kbd_handler();
+    print "<div id='wikiContent'>";
+    $formatter->external_on=1;
+    $formatter->send_page();
+    print "</div></div>";
+    print "</body></html>";
+    return;
+}
+
+// vim:et:sts=4:
+?>
