4e580ecc02f32904fe833cf2f5c0fff66165562a
Won-Kyu Park <wkpark@gmail.com>
2005-04-12 13:59:08 +0000

    minor update

--- plugin/Test.php
+++ plugin/Test.php
@@ -1,25 +1,24 @@
 <?php
-// Copyright 2003 by Won-Kyu Park <wkpark at kldp.org>
+// Copyright 2003-2005 Won-Kyu Park <wkpark at kldp.org>
 // All rights reserved. Distributable under GPL see COPYING
 // a sample plugin for the MoniWiki
 //
 // Usage: [[Test]]
 //
 // $Id$
-// vim:et:ts=2:
 
 function macro_Test($formatter,$value) {
-  return "HelloWorld !\n";
-
+    return "HelloWorld !\n";
 }
 
 function do_test($formatter,$options) {
-  $formatter->send_header();
-  $formatter->send_title();
-  $ret= macro_Test($formatter,$options[value]);
-  $formatter->send_page($ret);
-  $formatter->send_footer("",$options);
-  return;
+    $formatter->send_header();
+    $formatter->send_title();
+    $ret= macro_Test($formatter,$options[value]);
+    $formatter->send_page($ret);
+    $formatter->send_footer("",$options);
+    return;
 }
 
+// vim:et:sts=4:
 ?>
