eb9b5a2759c8d4a2516084fd6c1087f313373a42
Won-Kyu Park <wkpark@gmail.com>
2005-09-05 16:56:05 +0000

    use dynamic input form.

---
+++ local/rc.js
@@ -0,0 +1,23 @@
+// dynamic input form
+function daysago(obj) {
+  var ele=obj.parentNode;
+  if (ele.childNodes[0].innerHTML=='...') {
+    //ele.innerHTML="<input type='text' name='daysago'"+
+    //  " size='5' onChange='daysago(this)' />";
+    //ele.focus(); // not work
+    var node = document.createElement("input");
+    node.setAttribute("type","text");
+    node.setAttribute("size","5");
+    node.setAttribute("onChange","daysago(this)");
+    ele.parentNode.replaceChild(node,ele);
+    node.focus();
+  } else {
+    var my=''+self.location;
+    if (my.match(/ago=/,my))
+      my=my.replace(/ago=\d+/,'ago='+obj.value);
+    else
+      my+='?ago='+obj.value;
+    self.location=my;
+  }
+  return false;
+}
