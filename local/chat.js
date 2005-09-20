e2a9abf44f5e4f678a4377ff95369389fc95abe7
Won-Kyu Park <wkpark@gmail.com>
2005-09-20 10:11:23 +0000

    sound, nic support added etc.

--- local/chat.js
+++ local/chat.js
@@ -19,17 +19,61 @@ function sendMsg(ev,obj,url,id,num) {
      if (cc!=13) return;
    }
 
-   if (obj!=null) value=escape(obj.value);
-   nurl=url.replace(/\/ajax/,
-      '\/ajax&value='+value+'&room='+id+'&item='+num);
-   if (obj!=null) obj.value='';
-   var msg=HTTPGet(nurl);
-   var chat=document.getElementById(id);
-   //var node=document.createElement('li');
-   //node.innerHTML=msg;
-   //chat.appendChild(node);
-   chat.innerHTML=msg;
+   var d=new Date();
+   var last=document.getElementById('laststamp');
+   var nic='';
+   if (last) last='&laststamp='+last.innerHTML;
+   else last='';
 
+   if (obj!=null) {
+      value=escape(obj.value);
+      obj.value=''; // clear
+      nic=obj.parentNode.getElementsByTagName('input')[0];
+      if (nic) nic='&nic='+escape(nic.value);
+      else nic='';
+   }
+
+   var soundon_id=id.replace(/^chat/,'')+'soundon';
+   var soundon=document.getElementById(soundon_id).className;
+
+   nurl=url.replace(/\/ajax/,
+     '\/ajax&value='+value+'&room='+id+nic+'&item='+num+
+     '&stamp='+d.getTime()+last);
+
+   var msg=HTTPGet(nurl);
+   if (msg != 'false') {
+      var chat=document.getElementById(id);
+      //var node=document.createElement('li');
+      //node.innerHTML=msg;
+      //chat.appendChild(node);
+      chat.innerHTML=msg;
+      if (soundon == 'soundOn') Sound('pass');
+   }
    return;
 }
 
+function Sound(sndobj) {
+  var sound=document.getElementById(sndobj);
+  if (sound) { try { sound.Play(); } catch (e) { sound.DoPlay(); } }
+}
+
+function setSound(id,surl) {
+    var sound=document.getElementById('effect');
+    var node=document.createElement('embed');
+    node.setAttribute('src',surl);
+    node.setAttribute('id',id);
+    node.setAttribute('autostart','false');
+    node.setAttribute('style','visibility:hidden');
+    node.setAttribute('loop','false');
+    node.setAttribute('height','1px');
+    node.setAttribute('width','1px');
+    sound.insertBefore(node,sound.firstChild);
+}
+
+function OnOff(obj) {
+    if (obj.className=='soundOff') {
+        obj.className='soundOn';
+    } else {
+        obj.className='soundOff';
+    }
+}
