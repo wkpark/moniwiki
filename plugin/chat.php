1edc2e766eac901d0faff4da1d1e259a7a45dbff
Won-Kyu Park <wkpark@gmail.com>
2005-09-20 10:10:56 +0000

    sound and nic support added more sophisticated method to retrieve last modified msgs IE bug workarround suggested by drssay

--- plugin/chat.php
+++ plugin/chat.php
@@ -19,7 +19,7 @@ function macro_Chat($formatter,$value,$options=array()) {
     foreach ($args as $arg) {
         if (is_int($arg)) {
             $itemnum=$arg;
-        } else {
+        } else if (preg_match('/[a-z\s]+/i',$arg)){
             $tag=str_replace(' ','',ucfirst($arg));
         }
     }
@@ -33,6 +33,7 @@ function macro_Chat($formatter,$value,$options=array()) {
     //ob_end_flush();
     $msg=ob_get_contents();
     ob_end_clean();
+    if ($msg=='false') $msg=_("No messages");
     if (!$chat_script)
         $script=<<<EOF
 <script type='text/javascript' src='$DBInfo->url_prefix/local/ajax.js'></script>
@@ -42,17 +43,20 @@ EOF;
     $chat_script=1;
     return <<<EOF
 $script
+<span id="effect"></span>
 <div class="wikiChat">
-<div id="chat$tag">$msg</div>
-<form onSubmit='return false'>
-<input type='text' size='40' class='wikiChat' onkeypress='sendMsg(event,this,"$url","chat$tag",$itemnum);' />
-</form>
-</div>
 <script language='javascript'>
 <!--
 setInterval('sendMsg("poll",null,"$url","chat$tag",$itemnum)',10000);
+setSound('pass','$DBInfo->url_prefix/local/pass.au');
 //-->
 </script>
+<div id="chat$tag">$msg</div>
+<form onSubmit='return false'>
+<input type='text' size='10' class='chatUser' /> <input type='text' size='40' class='chatMsg' onkeypress='sendMsg(event,this,"$url","chat$tag",$itemnum);' />
+<input type='button' id='{$tag}soundon' class='soundOff' onclick="Sound('pass');OnOff(this)" />
+</form>
+</div>
 EOF;
 }
 
@@ -67,13 +71,23 @@ function do_chat($formatter,$options) {
 function ajax_chat($formatter,$options) {
     global $DBInfo;
     $user=new User(); # get cookie
+    $id=$user->id;
+    $nic='';
+    $udb=new UserDB($DBInfo);
     if ($user->id != 'Anonymous') {
-        $udb=new UserDB($DBInfo);
         $udb->checkUser($user);
+    }
+    if ($options['nic']) {
+        if (!$udb->_exists($options['nic'])) {
+            $nic=' '.$options['nic'];
+        } else if ($user->id=='Anonymous') {
+            $nic=' '.$options['nic'].'_'.substr(md5($_SERVER['REMOTE_ADDR']),0,4);
+        }
     }
     // %uD55C%uD558
     $value=_stripslashes($options['value']);
     $value=preg_replace('/%u([a-f0-9]{4})/i','&#x\\1;',$value);
+    $nic=preg_replace('/%u([a-f0-9]{4})/i','&#x\\1;',$nic);
     $itemnum=_stripslashes($options['item']);
     if ($itemnum > 50 or $itemnum <= 0) $itemnum=20;
     $room=escapeshellcmd(_stripslashes($options['room']));
@@ -90,20 +104,32 @@ function ajax_chat($formatter,$options) {
         $room=substr($room,4);
         $log=$DBInfo->upload_dir.'/Chat/'.$room.'.log';
     }
+    if (!$value) {
+        if (!file_exists($log)) {
+            print 'false';
+            return;
+        }
+        $mtime=filemtime($log);
+        if ($mtime <= $options['laststamp']) {
+            print 'false';
+            return;
+        }
+    }
+    
     $lines=array();
     $fp=fopen($log,'a+');
     while (is_resource($fp)) {
         fseek($fp,0,SEEK_END);
-        if ($value) {
-            fwrite($fp,time()."\t".$user->id."\t".rtrim($value)."\n");
-        }
+        if ($value)
+            fwrite($fp,time()."\t".$user->id.$nic."\t".rtrim($value)."\n");
+
         if (($fz=filesize($log))==0) break;
         fseek($fp,0,SEEK_END);
         if ($fz < 512) {
             fseek($fp,0);
             $ll=rtrim(fread($fp,512));
             $lines=explode("\n",$ll);
-            break;   
+            break;
         }
         $a=-1;
         $end=0;
@@ -142,21 +168,32 @@ function ajax_chat($formatter,$options) {
     $smiley_repl="\$formatter->smiley_repl('\\1')";
     $save=$formatter->sister_on;
     $formatter->sister_on=0;
+    $save2=$formatter->nonexists;
+    $formatter->nonexists='always';
     foreach ($lines as $line) {
-        $dumm=explode("\t",$line,3);
+        list($time,$user,$msg)=explode("\t",$line,3);
+        if (($p=strpos($user,' '))===false) {
+            if ($user!='Anonymous') $user='['.$user.']';
+        } else {
+            $user='[wiki:'.$user.']';
+        }
         $line='<span class="date">'.
-            gmdate("H:i:s",$dumm[0]+$options['tz_offset']).'</span>'.
-            '<span class="user">&lt;['.$dumm[1].']></span>'.$dumm[2];
+            gmdate("H:i:s",$time+$options['tz_offset']).'</span>'.
+            '<span class="user">&lt;'.$user.'></span>'.$msg;
         $line=preg_replace($smiley_rule,$smiley_repl,$line);
         $out.='<li>'.preg_replace("/(".$formatter->wordrule.")/e",
-            "\$formatter->link_repl('\\1')",$line).'</li>';
+           "\$formatter->link_repl('\\1')",$line).'</li>';
+        #$out.='<li>'.$line.'</li>';
     }
     $formatter->sister_on=$save;
+    $formatter->nonexists=$save2;
     if ($options['option_method']=='ajax') {
         $formatter->header('Expires','0');
+        $formatter->header('Cache-Control','no-cache');
         $formatter->header('Pragma','no-cache');
     }
-    print '<ul>'.$debug.$out.'</ul>';
+    $stamp='<span id="laststamp" style="display:none">'.time().'</span>';
+    print '<ul>'.$debug.$out.'</ul>'.$stamp;
 }
 
 // vim:et:sts=4:
