dcec5e92c521ec016a47e133ac5671b9965d2424
Won-Kyu Park <wkpark@gmail.com>
2005-10-13 13:06:31 +0000

    enhanded new action

--- plugin/new.php
+++ plugin/new.php
@@ -7,21 +7,33 @@
 // $Id$
 
 function do_new($formatter,$options) {
-  $title=_("Create a new page");
-  $formatter->send_header("",$options);
-  $formatter->send_title($title,"",$options);
-  $url=$formatter->link_url($formatter->page->urlname);
+  if (!$options['value']) {
+    $title=_("Create a new page");
+    $formatter->send_header("",$options);
+    $formatter->send_title($title,"",$options);
+    $url=$formatter->link_url($formatter->page->urlname);
 
-  $msg=_("Enter a page name");
-  print <<<FORM
+    $msg=_("Enter a page name");
+    $fixname=_("Normalize this page name");
+    $btn=_("Create a new page");
+    print <<<FORM
 <form method='get' action='$url'>
-    $msg: <input type='hidden' name='action' value='goto' />
+    $msg: <input type='hidden' name='action' value='new' />
     <input name='value' size='30' />
-    <input type='submit' value='Create' />
+    <input type='checkbox' name='fixname' checked='checked' />$fixname<br />
+    <input type='submit' value='$btn' />
     </form>
 FORM;
-
-  $formatter->send_footer();
+    $formatter->send_footer();
+  } else {
+    $pgname=$options['value'];
+    if ($options['fixname']) $pgname=normalize($pgname);
+    $options['page']=$pgname;
+    $page=new WikiPage($pgname);
+    $f=new Formatter($page,$options);
+    do_edit($f,$options);
+    return true;
+  }
 }
 
 ?>
