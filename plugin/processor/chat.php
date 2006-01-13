f077d614ac1a93e1c9472015df9043d8ea3741d5
Won-Kyu Park <wkpark@gmail.com>
2006-01-13 15:32:33 +0000

    fix indentation.

--- plugin/processor/chat.php
+++ plugin/processor/chat.php
@@ -1,52 +1,51 @@
 <?php
-// Copyright 2003 by Won-Kyu Park <wkpark at kldp.org>
+// Copyright 2003-2006 Won-Kyu Park <wkpark at kldp.org>
 // All rights reserved. Distributable under GPL see COPYING
-// sample plugin for the MoniWiki
+// a sample plugin for the MoniWiki
 //
 // Usage: {{{#!chat ID @date@ title
 // Hello World
 // }}}
-// this processor is used internally by the Blog action
 // $Id$
 
 function processor_chat($formatter,$value="") {
-  if ($value[0]=='#' and $value[1]=='!')
-    list($line,$value)=explode("\n",$value,2);
-  if ($line) {
-    # get parameters
-    list($tag, $user, $date, $title)=explode(" ",$line, 4);
+    if ($value[0]=='#' and $value[1]=='!')
+        list($line,$value)=explode("\n",$value,2);
+    if ($line) {
+        # get parameters
+        list($tag, $user, $date, $title)=explode(" ",$line, 4);
 
     if (preg_match('/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/',$user))
-      $user="Anonymous[$user]";
+        $user="Anonymous[$user]";
 
     if ($date && $date[10] == 'T') {
-      $date[10]=' ';
-      $time=strtotime($date." GMT");
-      $date= "@ ".date("Y-m-d [h:i a]",$time);
+        $date[10]=' ';
+            $time=strtotime($date." GMT");
+            $date= "@ ".date("Y-m-d [h:i a]",$time);
+        }
     }
-  }
 
-  $src= $value;
+    $src= $value;
 
-  if ($src) {
-    $options[nosisters]=1;
-    ob_start();
-    $formatter->send_page($src,$options);
-    $msg= ob_get_contents();
-    ob_end_clean();
-  }
+    if ($src) {
+        $options['nosisters']=1;
+        ob_start();
+        $formatter->send_page($src,$options);
+        $msg= ob_get_contents();
+        ob_end_clean();
+    }
 
-  $out="<table align='center' width='90%' border='0' class='wiki' cellpadding='4' cellspacing='0'>";
-  if ($title) {
-    $title=preg_replace("/(".$formatter->wordrule.")/e",
+    $out="<table align='center' width='90%' border='0' class='wiki' cellpadding='4' cellspacing='0'>";
+    if ($title) {
+        $title=preg_replace("/(".$formatter->wordrule.")/e",
                         "\$formatter->link_repl('\\1')",$title);
-    $out.="<tr><td><b>$title</b></td></tr>\n";
-  }
-  $out.="<tr><td><font size='-1'>Submitted by $user $date</font></td></tr>\n".
-    "<tr><td class='wiki'>$msg</td></tr>\n".
-    "</table>\n";
-  return $out;
+        $out.="<tr><td><b>$title</b></td></tr>\n";
+    }
+    $out.="<tr><td><font size='-1'>Submitted by $user $date</font></td></tr>\n".
+        "<tr><td class='wiki'>$msg</td></tr>\n".
+        "</table>\n";
+    return $out;
 }
 
-// vim:et:ts=2:
+// vim:et:sts=4:
 ?>
