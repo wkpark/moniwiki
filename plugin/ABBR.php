5b746e287b85b751be21db9197242dc42cbbb1b6
Won-Kyu Park <wkpark@gmail.com>
2006-01-26 15:55:03 +0000

    add a new macro

---
+++ plugin/ABBR.php
@@ -0,0 +1,15 @@
+<?php
+// Copyright 2005 Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPL see COPYING
+// a sample plugin for the MoniWiki
+//
+// Usage: [[ABBR(HTTP Hyper Text Transper Protocol)]]
+//
+// $Id$
+
+function macro_ABBR($formatter,$value) {
+    $sym=strtok($value,' '); $val=strtok('');
+    return '<abbr title="'.$val.'">'.$sym.'</abbr>';
+}
+// vim:et:sts=4:
+?>
