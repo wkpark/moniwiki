48d37f8563ea10a89c756237866c67b4950360e2
Won-Kyu Park <wkpark@gmail.com>
2006-02-01 08:20:10 +0000

    snapshot from wikiwyg.net update images

---
+++ local/Wikiwyg/lib/Wikiwyg.js
@@ -0,0 +1,573 @@
+/*==============================================================================
+Wikiwyg - Turn any HTML div into a wikitext /and/ wysiwyg edit area.
+
+DESCRIPTION:
+
+Wikiwyg is a Javascript library that can be easily integrated into any
+wiki or blog software. It offers the user multiple ways to edit/view a
+piece of content: Wysiwyg, Wikitext, Raw-HTML and Preview.
+
+The library is easy to use, completely object oriented, configurable and
+extendable.
+
+See the Wikiwyg documentation for details.
+
+AUTHORS:
+
+    Brian Ingerson <ingy@cpan.org>
+    Casey West <casey@geeknest.com>
+    Chris Dent <cdent@burningchrome.com>
+    Matt Liggett <mml@pobox.com>
+    Ryan King <rking@panoptic.com>
+    Dave Rolsky <autarch@urth.org>
+
+COPYRIGHT:
+
+    Copyright (c) 2005 Socialtext Corporation 
+    655 High Street
+    Palo Alto, CA 94301 U.S.A.
+    All rights reserved.
+
+Wikiwyg is free software. 
+
+This library is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+General Public License for more details.
+
+    http://www.gnu.org/copyleft/lesser.txt
+
+ =============================================================================*/
+
+/*==============================================================================
+Subclass - this can be used to create new classes
+ =============================================================================*/
+
+Subclass = function(name, base) {
+    if (!name) die("Can't create a subclass without a name");
+
+    var parts = name.split('.');
+    var subclass = window;
+    for (var i = 0; i < parts.length; i++) {
+        if (! subclass[parts[i]])
+            subclass[parts[i]] = function() {};
+        subclass = subclass[parts[i]];
+    }
+
+    if (base) {
+        var baseclass = eval('new ' + base + '()');
+        subclass.prototype = baseclass;
+        subclass.prototype.baseclass = base;
+        subclass.prototype.superfunc = Subclass.generate_superfunc();
+    }
+    subclass.prototype.classname = name;
+    return subclass.prototype;
+}
+
+Subclass.generate_superfunc = function() {
+    return function(func) {
+        var p;
+        var found = false;
+        var caller_func = arguments.callee.caller;
+        for (var b = this.classname; b; b = p.baseclass) {
+            p = eval(b + '.prototype');
+            if (! found) {
+                if (p[func] && p[func] == caller_func)
+                    found = true;
+                continue;
+            }
+            if (p[func] && p[func] != caller_func)
+                return p[func];
+        }
+        die(
+            "No superfunc function for: " + func + "\n" +
+            "baseclass was: " + this.baseclass + "\n" +
+            "caller was: " + arguments.callee.caller
+        );
+    }
+}
+
+/*==============================================================================
+Wikiwyg - Primary Wikiwyg base class
+ =============================================================================*/
+
+// Constructor and class methods
+proto = new Subclass('Wikiwyg');
+
+Wikiwyg.VERSION = '0.12';
+
+// Browser support properties
+Wikiwyg.ua = navigator.userAgent.toLowerCase();
+Wikiwyg.is_ie = (
+    Wikiwyg.ua.indexOf("msie") != -1 &&
+    Wikiwyg.ua.indexOf("opera") == -1 && 
+    Wikiwyg.ua.indexOf("webtv") == -1
+);
+Wikiwyg.is_gecko = (
+    Wikiwyg.ua.indexOf('gecko') != -1 &&
+    Wikiwyg.ua.indexOf('safari') == -1
+);
+Wikiwyg.is_safari = (
+    Wikiwyg.ua.indexOf('safari') != -1
+);
+Wikiwyg.is_opera = (
+    Wikiwyg.ua.indexOf('opera') != -1
+);
+Wikiwyg.browserIsSupported = (
+    Wikiwyg.is_gecko ||
+    Wikiwyg.is_ie
+);
+
+// Wikiwyg environment setup public methods
+proto.createWikiwygArea = function(div, config) {
+    this.set_config(config);
+    this.initializeObject(div, config);
+};
+
+proto.config = {
+    javascriptLocation: 'lib/',
+    doubleClickToEdit: false,
+    toolbarClass: 'Wikiwyg.Toolbar',
+    firstMode: null,
+    modeClasses: [
+        'Wikiwyg.Wysiwyg',
+        'Wikiwyg.Wikitext',
+        'Wikiwyg.Preview'
+    ]
+};
+
+proto.initializeObject = function(div, config) {
+    if (! Wikiwyg.browserIsSupported) return;
+    if (this.enabled) return;
+    this.enabled = true;
+    this.div = div;
+    this.divHeight = this.div.offsetHeight;
+    if (!config) config = {};
+
+    this.mode_objects = {};
+    for (var i = 0; i < this.config.modeClasses.length; i++) {
+        var class_name = this.config.modeClasses[i];
+        var mode_object = eval('new ' + class_name + '()');
+        mode_object.wikiwyg = this;
+        mode_object.set_config(config[mode_object.classtype]);
+        mode_object.initializeObject();
+        this.mode_objects[class_name] = mode_object;
+    }
+    var firstMode = this.config.firstMode
+        ? this.config.firstMode
+        : this.config.modeClasses[0];
+    this.setFirstModeByName(firstMode);
+
+    if (this.config.toolbarClass) {
+        var class_name = this.config.toolbarClass;
+        this.toolbarObject = eval('new ' + class_name + '()');
+        this.toolbarObject.wikiwyg = this;
+        this.toolbarObject.set_config(config.toolbar);
+        this.toolbarObject.initializeObject();
+        this.placeToolbar(this.toolbarObject.div);
+    }
+
+    // These objects must be _created_ before the toolbar is created
+    // but _inserted_ after.
+    for (var i = 0; i < this.config.modeClasses.length; i++) {
+        var mode_class = this.config.modeClasses[i];
+        var mode_object = this.modeByName(mode_class);
+        this.insert_div_before(mode_object.div);
+    }
+
+    if (this.config.doubleClickToEdit) {
+        var self = this;
+        this.div.ondblclick = function() { self.editMode() }; 
+    }
+}
+
+// Wikiwyg environment setup private methods
+proto.set_config = function(user_config) {
+    for (var key in this.config)
+        if (user_config && user_config[key])
+            this.config[key] = user_config[key];
+        else if (this[key] != null)
+            this.config[key] = this[key];
+}
+
+proto.insert_div_before = function(div) {
+    div.style.display = 'none';
+    if (! div.iframe_hack) {
+        this.div.parentNode.insertBefore(div, this.div);
+    }
+}
+
+// Wikiwyg actions - public methods
+proto.saveChanges = function() {
+    alert('Wikiwyg.prototype.saveChanges not subclassed');
+}
+
+proto.editMode = function() { // See IE, below
+    this.current_mode = this.first_mode;
+    this.current_mode.fromHtml(this.div.innerHTML);
+    this.toolbarObject.resetModeSelector();
+    this.current_mode.enableThis();
+}
+
+proto.displayMode = function() {
+    for (var i = 0; i < this.config.modeClasses.length; i++) {
+        var mode_class = this.config.modeClasses[i];
+        var mode_object = this.modeByName(mode_class);
+        mode_object.disableThis();
+    }
+    this.toolbarObject.disableThis();
+    this.div.style.display = 'block';
+    this.divHeight = this.div.offsetHeight;
+}
+
+proto.switchMode = function(new_mode_key) {
+    var new_mode = this.modeByName(new_mode_key);
+    var old_mode = this.current_mode;
+    var self = this;
+    new_mode.enableStarted();
+    old_mode.disableStarted();
+    old_mode.toHtml(
+        function(html) {
+            self.previous_mode = old_mode;
+            new_mode.fromHtml(html);
+            old_mode.disableThis();
+            new_mode.enableThis();
+            new_mode.enableFinished();
+            old_mode.disableFinished();
+            self.current_mode = new_mode;
+        }
+    );
+}
+
+proto.modeByName = function(mode_name) {
+    return this.mode_objects[mode_name]
+}
+
+proto.cancelEdit = function() {
+    this.displayMode();
+}
+
+proto.fromHtml = function(html) {
+    this.div.innerHTML = html;
+}
+
+proto.placeToolbar = function(div) {
+    this.insert_div_before(div);
+}
+
+proto.setFirstModeByName = function(mode_name) {
+    if (!this.modeByName(mode_name))
+        die('No mode named ' + mode_name);
+    this.first_mode = this.modeByName(mode_name);
+}
+
+// Class level helper methods
+Wikiwyg.unique_id_base = 0;
+Wikiwyg.createUniqueId = function() {
+    return 'wikiwyg_' + Wikiwyg.unique_id_base++;
+}
+
+Wikiwyg.liveUpdate = function(method, url, query, callback) {
+    var req = new XMLHttpRequest();
+    var data = null;
+    if (method == 'GET')
+        url = url + '?' + query;
+    else
+        data = query;
+    req.open(method, url);
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            try {
+                response_text = req.responseText;
+            }
+            catch(e) {
+                return;
+            }
+            callback(response_text);
+        }
+    }
+    if (method == 'POST') {
+        req.setRequestHeader(
+            'Content-Type', 
+            'application/x-www-form-urlencoded'
+        );
+    }
+    req.send(data);
+}
+
+Wikiwyg.htmlUnescape = function(escaped) {
+    // thanks to Randal Schwartz for the correct solution to this one
+    // (from CGI.pm, CGI::unescapeHTML())
+    return escaped.replace(
+        /&(.*?);/g,
+        function(dummy,s) {
+            return s.match(/^amp$/i) ? '&' :
+                s.match(/^quot$/i) ? '"' :
+                s.match(/^gt$/i) ? '>' :
+                s.match(/^lt$/i) ? '<' :
+                s.match(/^#(\d+)$/) ?
+                    String.fromCharCode(s.replace(/#/,'')) :
+                s.match(/^#x([0-9a-f]+)$/i) ?
+                    String.fromCharCode(s.replace(/#/,'0')) :
+                s
+        }
+    );
+}
+
+Wikiwyg.showById = function(id) {
+    document.getElementById(id).style.visibility = 'inherit';
+}
+
+Wikiwyg.hideById = function(id) {
+    document.getElementById(id).style.visibility = 'hidden';
+}
+
+
+Wikiwyg.changeLinksMatching = function(attribute, pattern, func) {
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        var my_attribute = link.getAttribute(attribute);
+        if (my_attribute && my_attribute.match(pattern)) {
+            link.setAttribute('href', '#');
+            link.onclick = func;
+        }
+    }
+}
+
+Wikiwyg.createElementWithAttrs = function(element, attrs, doc) {
+    if (doc == null)
+        doc = document;
+    return Wikiwyg.create_element_with_attrs(element, attrs, doc);
+}
+
+// See IE, below
+Wikiwyg.create_element_with_attrs = function(element, attrs, doc) {
+    var elem = doc.createElement(element);
+    for (name in attrs)
+        elem.setAttribute(name, attrs[name]);
+    return elem;
+}
+
+die = function(e) { // See IE, below
+    throw(e);
+}
+
+String.prototype.times = function(n) {
+    return n ? this + this.times(n-1) : "";
+}
+
+String.prototype.ucFirst = function () {
+    return this.substr(0,1).toUpperCase() + this.substr(1,this.length);
+}
+
+/*==============================================================================
+Base class for Wikiwyg classes
+ =============================================================================*/
+proto = new Subclass('Wikiwyg.Base');
+
+proto.set_config = function(user_config) {
+    for (var key in this.config) {
+        if (user_config != null && user_config[key] != null)
+            this.merge_config(key, user_config[key]);
+        else if (this[key] != null)
+            this.merge_config(key, this[key]);
+        else if (this.wikiwyg.config[key] != null)
+            this.merge_config(key, this.wikiwyg.config[key]);
+    }
+}
+
+proto.merge_config = function(key, value) {
+    if (value instanceof Array) {
+        this.config[key] = value;
+    }
+    // cross-browser RegExp object check
+    else if (typeof value.test == 'function') {
+        this.config[key] = value;
+    }
+    else if (value instanceof Object) {
+        if (!this.config[key])
+            this.config[key] = {};
+        for (var subkey in value) {
+            this.config[key][subkey] = value[subkey];
+        }
+    }
+    else {
+        this.config[key] = value;
+    }
+}
+
+/*==============================================================================
+Base class for Wikiwyg Mode classes
+ =============================================================================*/
+proto = new Subclass('Wikiwyg.Mode', 'Wikiwyg.Base');
+
+proto.enableThis = function() {
+    this.div.style.display = 'block';
+    this.display_unsupported_toolbar_buttons('none');
+    this.wikiwyg.toolbarObject.enableThis();
+    this.wikiwyg.div.style.display = 'none';
+}
+
+proto.display_unsupported_toolbar_buttons = function(display) {
+    if (!this.config) return;
+    var disabled = this.config.disabledToolbarButtons;
+    if (!disabled || disabled.length < 1) return;
+
+    var toolbar_div = this.wikiwyg.toolbarObject.div;
+    var toolbar_buttons = toolbar_div.childNodes;
+    for (var i in disabled) {
+        var action = disabled[i];
+
+        for (var i in toolbar_buttons) {
+            var button = toolbar_buttons[i];
+            var src = button.src;
+            if (!src) continue;
+
+            if (src.match(action)) {
+                button.style.display = display;
+                break;
+            }
+        }
+    }
+}
+
+proto.enableStarted = function() {}
+proto.enableFinished = function() {}
+proto.disableStarted = function() {}
+proto.disableFinished = function() {}
+
+proto.disableThis = function() {
+    this.display_unsupported_toolbar_buttons('inline');
+    this.div.style.display = 'none';
+}
+
+proto.process_command = function(command) {
+    if (this['do_' + command])
+        this['do_' + command](command);
+}
+
+proto.enable_keybindings = function() { // See IE
+    if (!this.key_press_function) {
+        this.key_press_function = this.get_key_press_function();
+        this.get_keybinding_area().addEventListener(
+            'keypress', this.key_press_function, true
+        );
+    }
+}
+
+proto.get_key_press_function = function() {
+    var self = this;
+    return function(e) {
+        if (! e.ctrlKey) return;
+        var key = String.fromCharCode(e.charCode).toLowerCase();
+        var command = '';
+        switch (key) {
+            case 'b': command = 'bold'; break;
+            case 'i': command = 'italic'; break;
+            case 'u': command = 'underline'; break;
+            case 'd': command = 'strike'; break;
+            case 'l': command = 'link'; break;
+        };
+
+        if (command) {
+            e.preventDefault();
+            e.stopPropagation();
+            self.process_command(command);
+        }
+    };
+}
+
+proto.get_edit_height = function() {
+    var height = parseInt(
+        this.wikiwyg.divHeight *
+        this.config.editHeightAdjustment
+    );
+    var min = this.config.editHeightMinimum;
+    return height < min
+        ? min
+        : height;
+}
+
+proto.setHeightOf = function(elem) {
+    elem.height = this.get_edit_height() + 'px';
+}
+
+proto.sanitize_html = function(html) { // See IE, below
+    var dom = this.create_dom(html);
+    return this.element_transforms(dom, {
+        del: {
+            name: 'strike',
+            attr: { }
+        },
+        strong: {
+            name: 'span',
+            attr: { style: 'font-weight: bold;' }
+        },
+        em: {
+            name: 'span',
+            attr: { style: 'font-style: italic;' }
+        }
+    }).innerHTML;
+}
+
+proto.create_dom = function(html) {
+    var dom = document.createElement('div');
+    dom.innerHTML = html;
+    return dom;
+}
+
+proto.element_transforms = function(dom, el_transforms) {
+    for (var orig in el_transforms) {
+        var elems = dom.getElementsByTagName(orig);
+        if (elems.length == 0) continue;
+        for (var i = 0; i < elems.length; i++) {
+            var elem = elems[i];
+            var replace = el_transforms[orig];
+            var new_el =
+              Wikiwyg.createElementWithAttrs(replace.name, replace.attr);
+            new_el.innerHTML = elem.innerHTML;
+            elem.parentNode.replaceChild(new_el, elem);
+        }
+    }
+    return dom;
+}
+
+/*==============================================================================
+Support for Internet Explorer in Wikiwyg
+ =============================================================================*/
+if (Wikiwyg.is_ie) {
+
+Wikiwyg.create_element_with_attrs = function(element, attrs, doc) {
+     var str = '';
+     // Note the double-quotes (make sure your data doesn't use them):
+     for (name in attrs)
+         str += ' ' + name + '="' + attrs[name] + '"';
+     return doc.createElement('<' + element + str + '>');
+}
+
+die = function(e) {
+    alert(e);
+    throw(e);
+}
+
+proto = Wikiwyg.Mode.prototype;
+
+proto.enable_keybindings = function() {}
+
+proto.sanitize_html = function(html) {
+    var dom = this.create_dom(html);
+    return this.element_transforms(dom, {
+        del: {
+            name: 'strike',
+            attr: { }
+        }
+    }).innerHTML;
+}
+
+} // end of global if statement for IE overrides
