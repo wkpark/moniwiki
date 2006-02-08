f542d6edaa6c3dad7847ffc5f6e54f1f6abfcde6
Won-Kyu Park <wkpark@gmail.com>
2006-02-08 02:38:04 +0000

    fixed css id for the Comment macro

--- plugin/Comment.php
+++ plugin/Comment.php
@@ -51,7 +51,7 @@ function macro_Comment($formatter,$value,$options=array()) {
     $hidden.='<input type="hidden" name="comment_id" value="'.$value.'" />';
   $form = "<form name='editform' method='post' action='$url'>\n";
   $form.= <<<FORM
-<textarea class="wiki" id="content" name="savetext"
+<textarea class="wiki" name="savetext"
  rows="$rows" cols="$cols">$savetext</textarea><br />
 FORM;
   if ($options['id'] == 'Anonymous')
