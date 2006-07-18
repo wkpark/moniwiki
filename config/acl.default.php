980006ed7a8b6139d1df68431a8af13f47ede465
Won-Kyu Park <wkpark@gmail.com>
2006-07-18 13:09:07 +0000

    update

--- config/acl.default.php
+++ config/acl.default.php
@@ -7,7 +7,7 @@
 # $Id$
 @Guest	Anonymous
 *	@ALL		deny	*
-*	@ALL		allow	ticket
+*	@ALL		allow	edit,ticket,savepage
 *	@User		allow	*
 # some pages are allowed to edit
 WikiSandBox	@Guest	allow	edit,info,diff
@@ -18,6 +18,7 @@ WikiSandBox	@Guest	allow	edit,info,diff
 *	@ALL		allow	read,userform,rss_rc,aclinfo,fortune,deletepage,fixmoin,ticket
 # some pages have restrict permission
 MoniWiki	@ALL	deny	edit,uploadfile,diff
+HelpOn*		@User	deny	edit,uploadfile,diff
 # special pages
 ACL	@ALL		deny    edit,diff,info # a shared ACL file (not supported yet XXX)
 # set group members
