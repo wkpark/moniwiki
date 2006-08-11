3d4261eb363638b6e73d00372bdde5524cacbe24
Won-Kyu Park <wkpark@gmail.com>
2006-08-11 13:16:35 +0000

    apply RFC 2231 for Opera 9, RFC 2047 for Mozilla and rawurlencode() for IE $use_resume_download,$download_mode added.

--- plugin/download.php
+++ plugin/download.php
@@ -15,6 +15,9 @@ function do_download($formatter,$options) {
     return; 
   }
   $value=&$options['value'];
+  $down_mode=$options['mode']{0}=='a' ? 'attachment':
+    ($DBInfo->download_mode ? $DBInfo->download_mode:'inline');
+
   if (($p=strpos($value,':')) !== false or ($p=strpos($value,'/')) !== false) {
     $subpage=substr($value,0,$p);
     $file=substr($value,$p+1);
@@ -52,8 +55,10 @@ function do_download($formatter,$options) {
 
   $file=$subdir.$file[count($file)-1];
 
-  if (!file_exists("$dir/$file")) 
+  if (!file_exists("$dir/$file")) {
+    header("HTTP/1.1 404 Not Found");
     return;
+  }
 
   $lines = @file($DBInfo->data_dir.'/mime.types');
   if ($lines) {
@@ -69,13 +74,36 @@ function do_download($formatter,$options) {
     }
   } else
     $mime=array();
+
+  # set filename
   if (preg_match("/\.(.{1,4})$/",$file,$match))
     $mimetype=strtolower($mime[$match[1]]);
   if (!$mimetype) $mimetype="application/x-unknown";
 
+  if (strstr($_SERVER['HTTP_USER_AGENT'], 'MSIE')) {
+    // IE: rawurlencode()
+    $fname='filename="'.rawurlencode($file).'"';
+    // fix IE bug
+    $fname = preg_replace('/\./', '%2e',
+        $fname, substr_count($fname, '.') - 1);
+
+    #header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    #header('Pragma: public');
+  } else if (strstr($_SERVER['HTTP_USER_AGENT'], 'Opera')) {
+    // Opera 9: RFC 2231
+    $fname='filename*='.$DBInfo->charset."*".rawurlencode($file).'';
+  } else // Mozilla: RFC 2047
+    $fname='filename="=?'.$DBInfo->charset.'?B?'.base64_encode($file).'?="';
+
+  if ($DBInfo->use_resume_download) {
+    $header=array("Content-Description: MoniWiki PHP Downloader");
+    dl_file_resume($mimetype,$dir.'/'.$file,$fname,$down_mode,$header);
+    return; 
+  }
+
   header("Content-Type: $mimetype\r\n");
-  header("Content-Disposition: inline; filename=\"$file\"" );
-  #header("Content-Disposition: attachment; filename=\"$file\"" );
+  header("Content-Length: ".filesize($dir.'/'.$file));
+  header("Content-Disposition: $down_mode; ".$fname );
   header("Content-Description: MoniWiki PHP Downloader" );
   Header("Pragma: no-cache");
   Header("Expires: 0");
@@ -87,4 +115,63 @@ function do_download($formatter,$options) {
 function macro_download($formatter,$value) {
   return $formatter->link_to("?action=download&amp;value=$value",$value);
 }
+
+function dl_file_resume($ctype,$file,$fname,$mode='inline',$header='') {
+   # from http://kr2.php.net/manual/en/function.fread.php#63893
+   # ans some modification
+  
+   //Gather relevent info about file
+   $len = filesize($file);
+  
+   //Begin writing headers
+   header("Cache-Control:");
+   header("Cache-Control: public");
+   if (is_array($header)) foreach($header as $h) header($h);
+  
+   //Use the switch-generated Content-Type
+   header("Content-Type: $ctype");
+   if (strstr($_SERVER['HTTP_USER_AGENT'], "MSIE")) {
+       # workaround for IE filename bug with multiple periods / multiple dots
+       # in filename that adds square brackets to filename
+       # - eg. setup.abc.exe becomes setup[1].abc.exe
+       $fname = preg_replace('/\./', '%2e',
+           $fname, substr_count($fname, '.') - 1);
+   }
+   header("Accept-Ranges: bytes");
+  
+   $size=filesize($file);
+   //check if http_range is sent by browser (or download manager)
+   if(isset($_SERVER['HTTP_RANGE'])) {
+       list($a, $range)=explode("=",$_SERVER['HTTP_RANGE']);
+       //if yes, download missing part
+       str_replace($range, "-", $range);
+       $size2=$size-1;
+       $new_length=$size2-$range;
+       header("HTTP/1.1 206 Partial Content");
+       header("Content-Range: bytes $range$size2/$size");
+       header("Content-Length: $new_length");
+       header("Content-Disposition: $mode; $fname");
+   } else {
+       $size2=$size-1;
+       header("Content-Range: bytes 0-$size2/$size");
+       header("Content-Length: ".$size);
+       header("Content-Disposition: $mode; $fname");
+   }
+   //open the file
+   $fp=fopen("$file","rb");
+   //seek to start of missing part
+   fseek($fp,$range);
+   //start buffered download
+   while(!feof($fp)){
+       //reset time limit for big files
+       set_time_limit(0);
+       print(fread($fp,1024*8));
+       flush();
+       ob_flush();
+   }
+   fclose($fp);
+   exit;
+}
+
+// vim:et:sts=4:
 ?>
