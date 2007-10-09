943fb808cf989dcbdf84ce136e8a5bbf5d199149
Won-Kyu Park <wkpark@gmail.com>
2007-10-09 05:11:45 +0000

    fixed xml tag

--- plugin/Play.php
+++ plugin/Play.php
@@ -140,8 +140,9 @@ function macro_Play($formatter,$value) {
       }
 
       $tracks="<track>\n".implode("</track>\n<track>\n",$list)."</track>\n";
+      // UTF-8 FIXME
       $xml=<<<XML
-<?xml version="1.0" encoding="UTF-8"? >
+<?xml version="1.0" encoding="UTF-8"?>
 <playlist version="1" xmlns="http://xspf.org/ns/0/">
   <title>XSPF Playlist</title>
   <info>XSPF Playlist</info>
