f5bbdcacbaa585d23610dffce4b425df319899c4
Won-Kyu Park <wkpark@gmail.com>
2007-10-11 09:34:00 +0000

    support google highlighting like as trac etc.

---
+++ local/googlehi.js
@@ -0,0 +1,69 @@
+// from http://www.edgewall.org/chrome/common/js/trac.js
+// Adapted from http://www.kryogenix.org/code/browser/searchhi/
+function MoniSearchHighlight() {
+  if (!document.createElement) return;
+
+  var div = document.getElementById("wikiBody");
+  if (!div) return;
+
+  function getSearchWords(url) {
+    if (url.indexOf('?') == -1) return [];
+    var queryString = url.substr(url.indexOf('?') + 1);
+    var params = queryString.split('&');
+    for (var p in params) {
+      var param = params[p].split('=');
+      if (param.length < 2) continue;
+      if (param[0] == 'q' || param[0] == 'p') { // q= for Google, p= for Yahoo
+        var query = decodeURIComponent(param[1].replace(/\+/g, ' '));
+        if (query[0] == '!') query = query.slice(1);
+        words = query.split(/(".*?")|('.*?')|(\s+)/);
+        var words2 = new Array();
+        for (var w in words) {
+          words[w] = words[w].replace(/^\s+$/, '');
+          if (words[w] != '') {
+            words2.push(words[w].replace(/^['"]/, '').replace(/['"]$/, ''));
+          }
+        }
+        return words2;
+      }
+    }
+    return [];
+  }
+
+  function highlightWord(node, word, searchwordindex) {
+    // If this node is a text node and contains the search word, highlight it by
+    // surrounding it with a span element
+    if (node.nodeType == 3) { // Node.TEXT_NODE
+      var pos = node.nodeValue.toLowerCase().indexOf(word.toLowerCase());
+      if (pos >= 0 && !/^searchword\d$/.test(node.parentNode.className)) {
+        var span = document.createElement("span");
+        span.className = "searchword" + (searchwordindex % 5);
+        span.appendChild(document.createTextNode(
+          node.nodeValue.substr(pos, word.length)));
+        node.parentNode.insertBefore(span, node.parentNode.insertBefore(
+          document.createTextNode(node.nodeValue.substr(pos + word.length)),
+            node.nextSibling));
+        node.nodeValue = node.nodeValue.substr(0, pos);
+        return true;
+      }
+    } else if (!node.nodeName.match(/button|select|textarea/i)) {
+      // Recurse into child nodes
+      for (var i = 0; i < node.childNodes.length; i++) {
+        if (highlightWord(node.childNodes[i], word, searchwordindex)) i++;
+      }
+    }
+    return false;
+  }
+
+  var words = getSearchWords(document.URL);
+  if (!words.length) words = getSearchWords(document.referrer);
+  if (words.length) {
+    for (var w in words) {
+      if (words[w].length) highlightWord(div, words[w], w);
+    }
+  }
+}
+
+
+if (window.addEventListener) window.addEventListener("load",MoniSearchHighlight,false);
+else if (window.attachEvent) window.attachEvent("onload",MoniSearchHighlight);
