0ce40f8737893d2134860173d4babd4aa2dbde9d
Won-Kyu Park <wkpark@gmail.com>
2008-04-10 09:35:20 +0000

    enalbe public cache

--- plugin/processor/pic.php
+++ plugin/processor/pic.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 2003-2006 Kim Jeong Yeon <see2002 at new-type.com>
+// Copyright 2003-2008 Kim Jeong Yeon <see2002 at new-type.com>
 // All rights reserved. Distributable under GPL see COPYING
 // a PIC plugin for the MoniWiki
 //
@@ -16,6 +16,8 @@ function processor_pic($formatter,$value="") {
 
   $vartmp_dir=&$DBInfo->vartmp_dir;
   $cache_dir=$DBInfo->upload_dir."/PIC";
+  $cache_url=$DBInfo->upload_url ? $DBInfo->upload_url.'/PIC':
+    $DBInfo->url_prefix.'/'.$cache_dir;
 
   if ($value[0]=='#' and $value[1]=='!')
     list($line,$value)=explode("\n",$value,2);
@@ -25,18 +27,29 @@ function processor_pic($formatter,$value="") {
   # print "<pre>$pic_script</pre>";
 
   $uniq=md5($pic_script);
+  if ($DBInfo->cache_public_dir) {
+    $fc=new Cache_text('pic',2,'png',$DBInfo->cache_public_dir);
+    $pngname=$fc->_getKey($uniq,0);
+    $outpath_png= $DBInfo->cache_public_dir.'/'.$pngname;
+
+    $png_url=
+      $DBInfo->cache_public_url ? $DBInfo->cache_public_url.'/'.$pngname:
+      $DBInfo->url_prefix.'/'.$png;
+  } else {
+    $outpath_png=$cache_dir.'/'.$uniq.'.png';
+    $png_url=$cache_url.'/'.$uniq.'.png';
+
+  }
   $outpath_pic="$vartmp_dir/$uniq.pic";
   $outpath_ps="$vartmp_dir/$uniq.ps";
-  $outpath_png="$cache_dir/$uniq.png";
 
-
-  if (!file_exists($cache_dir)) {
+  if (!file_exists(dirname($outpath_png))) {
     umask(000);
-    mkdir($cache_dir,0777);
+    _mkdir_p(dirname($outpath_png),0777);
     umask(022);
   }
 
-  if ($formatter->refresh || !file_exists("$cache_dir/$uniq.png")) {
+  if ($formatter->refresh || !file_exists($outpath_png)) {
     # write to pic script file
     $ifp=fopen("$outpath_pic","w");
     fwrite($ifp,$pic_script);
@@ -52,7 +65,7 @@ function processor_pic($formatter,$value="") {
     unlink($outpath_ps);
     unlink($outpath_pic);
   }
-  return "<img class='tex' src='$DBInfo->url_prefix/$cache_dir/$uniq.png' alt='pic' />";
+  return "<img class='tex' src='$png_url' alt='pic' />";
 }
 
 ?>
