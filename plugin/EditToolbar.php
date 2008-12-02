9eecc95e5bb04996aca4f363909813976fe1e415
Won-Kyu Park <wkpark@gmail.com>
2008-12-02 14:45:59 +0000

    reorderd toolbar to consistent with the WikiWyg. use toolbar.css if it exists

--- plugin/EditToolbar.php
+++ plugin/EditToolbar.php
@@ -15,26 +15,32 @@ function macro_EditToolbar($formatter,$value) {
     $iconset=!empty($DBInfo->toolbar_iconset) ? $DBInfo->toolbar_iconset:
         'moniwiki';
     $imgdir=$DBInfo->imgs_dir.'/plugin/EditToolbar/'.$iconset;
+    $formatter->register_javascripts("wikibits.js");
+    $fcss= 'imgs/plugin/EditToolbar/'.$iconset.'/toolbar.css';
+    $css='';
+    if (file_exists($fcss))
+        $css="<style type='text/css'>
+@import url('$imgdir/toolbar.css');
+</style>";
     $script=<<<EOS
-<script type="text/javascript" src="$DBInfo->url_prefix/local/wikibits.js"></script>
+$css
 <script language="JavaScript" type='text/javascript'>
 /*<![CDATA[*/
-document.writeln("<div id='toolbar'>");
+document.writeln("<div id='toolbar'><span>");
 addButton('$imgdir/button_bold.png','Bold text','\'\'\'','\'\'\'','Bold text');
 addButton('$imgdir/button_italic.png','Italic text','\'\'','\'\'','Italic text');
 addButton('$imgdir/button_link.png','Internal link','[',']','Link title');
 addButton('$imgdir/button_extlink.png','External link (remember http:// prefix)','[',']','http://www.example.com link title');
 addButton('$imgdir/button_headline.png','Level 2 headline','\\n== ',' ==\\n','Headline text');
+addLinkButton('$imgdir/button_math.png','Mathematical formula (LaTeX)','mathChooser');
+addButton('$imgdir/button_nowiki.png','Ignore wiki formatting','{{{','}}}','Insert non-formatted text here');
+addButton('$imgdir/button_hr.png','Horizontal line (use sparingly)','\\n----\\n','','');
 addButton('$imgdir/button_image.png','Embedded image','attachment:','','Example.jpg');
 addButton('$imgdir/button_media.png','Media file link','[[Media(',')]]','Example.mp3');
-addLinkButton('$imgdir/button_math.png','Mathematical formula (LaTeX)','mathChooser');
 addLinkButton('$imgdir/button_smiley.png','Smiley','smileyChooser');
-addButton('$imgdir/button_nowiki.png','Ignore wiki formatting','{{{','}}}','Insert non-formatted text here');
-
 addButton('$imgdir/button_sig.png','Your signature with timestamp','@SIG@','','');
-addButton('$imgdir/button_hr.png','Horizontal line (use sparingly)','\\n----\\n','','');
 addInfobox('Click a button to get an example text','Please enter the text you want to be formatted.\\\\n It will be shown in the infobox for copy and pasting.\\\\nExample:\\\\n\$1\\\\nwill become:\\\\n\$2');
-document.writeln("</div>");
+document.writeln("</span></div><div style='clear:both'></div>");
 /*]]>*/
 </script>
 EOS;
