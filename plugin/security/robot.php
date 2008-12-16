d64bceb48f52d6ab5a78071047d1c5009d3a6b6b
Won-Kyu Park <wkpark@gmail.com>
2008-12-16 13:45:30 +0000

    a robot security module.

---
+++ plugin/security/robot.php
@@ -0,0 +1,28 @@
+<?php
+# a robot protection security plugin for the MoniWiki
+# $Id$
+
+class Security_robot extends Security {
+  var $DB;
+
+  function Security_robot($DB="") {
+    $this->DB=$DB;
+  }
+
+# $options[page]: pagename
+# $options[id]: user id
+
+  function writable($options="") {
+    return $this->DB->_isWritable($options['page']);
+  }
+
+  function is_allowed($action="read",&$options) {
+    $allowed=array('read','show','ticket','titleindex','rss_rc');
+    if (in_array($action, $allowed)) {
+      return 1;
+    }
+    return 0;
+  }
+}
+
+?>
