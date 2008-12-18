99270fd3a7d6d582fb43be9bb6f16b9ba2919eb4
Won-Kyu Park <wkpark@gmail.com>
2008-12-18 03:36:56 +0000

    fix to work with mootools.

--- local/folding.js
+++ local/folding.js
@@ -1,17 +1,31 @@
-function foldingSection(btn,id)
+/*
+ * Builti-in section folding for MoniWiki
+ * 
+ * $Id$
+ */
+
+function foldingSection(btn, id)
 {
     var sect;
-    if (typeof id == 'object') sect=id;
+    if (typeof id == 'object') sect = id;
     else sect=document.getElementById(id);
     if (!sect) return;
-    var toggle=true;
+    var toggle = true;
     if (sect.style.display != 'none') {
-        toggle=false;
+        toggle = false;
     }
 
     var icon=null;
-    if (btn)
+    if (btn) {
         icon=btn.getElementsByTagName('img')[0];
+        if (!icon) {
+            icon = new Image();
+            icon.src = _url_prefix + '/imgs/plugin/arrdown.png';
+            icon.style.width = '12px';
+            btn.insertBefore(icon, btn.firstChild);
+            //btn.appendChild(icon);
+        }
+    }
 
     if (typeof Effect != 'undefined') { // prototype.js
         var dur = 0.5;
@@ -21,20 +35,19 @@ function foldingSection(btn,id)
           new Effect.SlideUp(sect, { duration: dur, afterFinish: function() {Element.hide(sect);} });
         }
     } else { // get sectpages for the first time.
-        var mySlide = new Fx.Slide(sect); // oops!!!
-        if (sect.style.height != 0) {
-            mySlide.slideIn();
-        } else {
-            mySlide.slideOut();
-        }
+        var mySlide = new Fx.Slide(sect); // mootools
+        mySlide.toggle();
     }
     if (icon) {
         var name=icon.getAttribute('class');
-        if (name == 'close')
+        if (name == 'close') {
+            icon.src = _url_prefix + '/imgs/plugin/arrup.png';
             icon.setAttribute('class','');
-        else
+        } else {
+            icon.src = _url_prefix + '/imgs/plugin/arrdown.png';
             icon.setAttribute('class','close');
+        }
     }
 }
 
-// vim:et:sts=4:sw:
+// vim:et:sts=4:sw=4:
