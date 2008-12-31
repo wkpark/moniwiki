01d83aafd77629746ef39703d3479e3fb3886cba
Won-Kyu Park <wkpark@gmail.com>
2008-12-31 11:13:24 +0000

    gettextize some msgs

--- local/js/locale/dummy.js
+++ local/js/locale/dummy.js
@@ -2,6 +2,10 @@ N_("Continue to edit current text ?");
 N_("Canceled!");
 N_("Please select the text you would like to turn into a link.");
 
+//
+N_("Auto saved text found.\nAre you sure to restore page ?\n(You can undo/redo with Ctrl-Z/Ctrl-Shift-Z)");
+N_("Current text is saved in a temporary file.");
+
 // sh
 N_("The code is in your clipboard now");
 N_('Printing...');
