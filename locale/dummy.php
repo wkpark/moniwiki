7cdfdfcc9fb0add385ea6c7eb47ab569b4671df2
Won-Kyu Park <wkpark@gmail.com>
2009-07-29 17:52:45 +0000

    update msgs

--- locale/dummy.php
+++ locale/dummy.php
@@ -4,7 +4,8 @@ $pagename=array(
   _("TitleIndex"), _("HelpContents"),
   _("DeletePage"), _("LikePages"), _("FindPage"), _("UploadFile"),
   _("UploadedFiles"), _("EditText"), _("ShowPage"), _("InterWiki"),
-  _("BlogChanges"),_("VisualTour"),_("TrackBack")
+  _("BlogChanges"),_("VisualTour"),_("TrackBack"),
+  _("source"),_("diff"), _("recall"), _("view")
 );
 
 ?>
