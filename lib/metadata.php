680be2a795ddc540e0f653b7cb75980ff658182d
Won-Kyu Park <wkpark@gmail.com>
2009-10-03 10:30:54 +0000

    cleanup

--- lib/metadata.php
+++ lib/metadata.php
@@ -4,28 +4,28 @@
 //
 // $Id$
 
-function getMetadata($raw,$mode=1,$opts=array()) {
+function getMetadata($raw, $mode = 1, $params = false) {
 
-    $metas=explode("\n",$raw);
-    $meta=array();
+    $metas = explode("\n", $raw);
+    $meta = array();
     foreach ($metas as $line) {
         if (!trim($line)) break;
-        if (($p=strpos($line,':'))!== false) {
-            list($mykey,$val)=explode(':',$line,2);
-            $val=trim($val);
+        if (($p = strpos($line, ':')) !== false) {
+            list($mykey, $val) = explode(':', $line, 2);
+            $val = trim($val);
             # strip leading bullet' * '
-            $mykey=preg_replace('/^\s*(\*|\d.)?\s*/','',$mykey);
-            if (strpos($mykey,' ')!== false)
-                $mykey=str_replace(' ','-',ucwords($mykey));
+            $mykey = preg_replace('/^\s*(\*|\d.)?\s*/', '', $mykey);
+            if (strpos($mykey,' ') !== false)
+                $mykey=str_replace(' ', '-', ucwords($mykey));
 
-            $mymeta=array();
+            $mymeta = array();
             if (trim($val)) {
-                $vals=explode(',',$val);
+                $vals = explode(',', $val);
                 foreach ($vals as $v) {
-                    $v=trim($v);
-                    if ($v) $mymeta[]=trim($v);
+                    $v = trim($v);
+                    if ($v) $mymeta[] = trim($v);
                 }
-                $meta[$mykey]=$mymeta;
+                $meta[$mykey] = $mymeta;
             }
 
 #            if (strpos($val,' ')!== false) {
@@ -36,11 +36,11 @@ function getMetadata($raw,$mode=1,$opts=array()) {
 #            $meta[$mykey][]=$val;
         }
     }
-    if ($mode==1) {
-        foreach ($meta as $k=>$v) {
+    if ($mode == 1) {
+        foreach ($meta as $k => $v) {
             array_unique($v);
-            $val=implode(", ",$v);
-            $metadata[$k]=$val;
+            $val = implode(', ', $v);
+            $metadata[$k] = $val;
         }
         return $metadata;
     }
@@ -49,30 +49,37 @@ function getMetadata($raw,$mode=1,$opts=array()) {
 
 
 function _get_metadata($body) {
-    $key='';
-    $val='';
-    $meta=array();
-    $metaok=0;
+    $key = '';
+    $val = '';
+    $meta = array();
+    $metaok = 0;
     while(!empty($body)) {
-        list($line, $body)= split("\n", $body,2);
-        if ($key and ($line{0}==' ' or $line{0}=="\t")) { $meta[$key].=$line."\n";continue;}
+        list($line, $body) = split("\n", $body, 2);
+        if ($key and ($line[0] == ' ' or $line[0] == "\t")) {
+            $meta[$key].= $line . "\n";
+            continue;
+        }
         if (!trim($line)) {
-            $metaok=1;
+            $metaok = 1;
             break;
         }
-        $key=strtok($line,' ');
-        $val=rtrim(strtok(''));
-        if ($key=='From') { # mail type
+        $key = strtok($line, ' ');
+        $val = rtrim(strtok(''));
+        if ($key == 'From') { # mail type
             if (empty($meta['magic'])) {
-                $meta['magic']=$line; continue;
+                $meta['magic'] = $line; continue;
             } else break;
         }
-        if (preg_match('/(^[A-Z][a-zA-Z0-9\-]+):$/',$key,$m))
-            $meta[$m[1]]=$val;
-        else break;
+        if (preg_match('/(^[A-Z][a-zA-Z0-9\-]+):$/', $key, $m))
+            $meta[$m[1]] = $val;
+        else
+            break;
     }
-    if ($metaok) return array($meta,$body);
-    return array(null,null);
+
+    if ($metaok)
+        return array($meta, $body);
+
+    return array(null, null);
 }
 
 // vim:et:sts=4:sw=4:
