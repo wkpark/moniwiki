719427245ee154ad32221f279f517340cf4f4fda
Won-Kyu Park <wkpark@gmail.com>
2010-08-26 12:04:16 +0000

    [#315634] * antialiasing captcha image.  * $use_ticket = 16 or $use_captcha = 16 to distort captcha image with KCAPTHCA randomization

--- plugin/ticket.php
+++ plugin/ticket.php
@@ -1,11 +1,11 @@
 <?php
-// Copyright 2005-2006 Won-Kyu Park <wkpark at kldp.org>
+// Copyright 2005-2010 Won-Kyu Park <wkpark at kldp.org>
 // All rights reserved. Distributable under GPL see COPYING
 // a simple CAPTCHA ticket plugin for the MoniWiki
 //
 // $Id$
 
-function _effect_distort($image,$factor=40,$grad=1) {
+function _effect_distort($image, $factor=40, $grad=1, $method = 0) {
     // from http://www.codeproject.com/aspnet/CaptchaNET_2.asp Farshid Hosseini
     $width = imagesx($image);
     $height = imagesy($image);
@@ -16,33 +16,60 @@ function _effect_distort($image,$factor=40,$grad=1) {
     $yf=rand(30,45)*$fact;
     $xf=rand(80,95)*$fact;
 
+    // from http://www.captcha.ru/captchas/multiwave/
+    // KCAPTCHA randomize
+
+    // periods
+    $rand1 = mt_rand(750000,1200000)/10000000;
+    $rand2 = mt_rand(750000,1200000)/10000000;
+    $rand3 = mt_rand(750000,1200000)/10000000;
+    $rand4 = mt_rand(750000,1200000)/10000000;
+    // phases
+    $rand5 = mt_rand(0,31415926)/10000000;
+    $rand6 = mt_rand(0,31415926)/10000000;
+    $rand7 = mt_rand(0,31415926)/10000000;
+    $rand8 = mt_rand(0,31415926)/10000000;
+    // amplitudes
+    $rand9 = mt_rand(330,420)/110;
+    $rand10 = mt_rand(330,450)/110;
+
+    // make new canvas img
     $canvas=imagecreate($width,$height);
-    $r1 = $g1 = $b1 = 0;
-    $new = imagecolorallocate($canvas, $r1, $g1, $b1);
+    $bgcolor = imagecolorsforindex($image, 0);
+    $new = imagecolorallocate($canvas, $bgcolor['red'], $bgcolor['green'], $bgcolor['blue']);
+    $fgcolor = imagecolorsforindex($image, 1);
+    $new = imagecolorallocate($canvas, $fgcolor['red'], $fgcolor['green'], $fgcolor['blue']);
     imagepalettecopy($canvas,$image);
 
     for ($y = 0; $y < $height; $y++) {
         for ($x = 0; $x < $width; $x++) {
-            // Adds a simple wave
-            $newX = 
-              ($x + ($disx * Sin(3.141592 * $y / $xf)));
+            if (empty($method)) {
+                // Adds a simple wave
+                $newX = ($x + ($disx * Sin(3.141592 * $y / $xf)));
 
-            $t=($x - $width/2) / $yf * 2.4;
-            $t=$t*$t;
-            $newY = 
-              #($y + ($distort * sin(1.5*3.141592 * $x / $yf)));
-              ($y + ($disy * exp(-$t)*sin(3.141592 * $x / $yf)));
-            $col = @imagecolorat($image, $newX, $newY);
-
-            if ($newY >$height or $newY < 0) $newY=0;
-            if ($newX < 0) $newX=0;
-            else if ($newX > $width) $newX=$width;
+                $t = ($x - $width/2) / $yf * 2.4;
+                $t = $t*$t;
+                $newY = 
+                    #($y + ($distort * sin(1.5*3.141592 * $x / $yf)));
+                    ($y + ($disy * exp(-$t)*sin(3.141592 * $x / $yf)));
+            } else {
+                // KCAPTCHA method
+                $newX = $x + (sin($x*$rand1+$rand5)+sin($y*$rand3+$rand6))*$rand9;
+                $newY = $y + (sin($x*$rand2+$rand7)+sin($y*$rand4+$rand8))*$rand10;
+            }
 
             if ($grad) { # with gradient effect based on above functions
-                $i = imagecolorsforindex($image, $col);
-                $r = $i['red'];
-                $g = $i['green'];
-                $b = $i['blue'];
+                if ($newY > $height or $newY < 0) $newY=0;
+                if ($newX < 0) $newX=0;
+                else if ($newX > $width) $newX=$width;
+
+                $newcolor = _antialias_color($image, $x, $y, $newX, $newY);
+                if ($newcolor === false) // this is background color
+                    $newcolor = $bgcolor;
+
+                $r = $newcolor['red'];
+                $g = $newcolor['green'];
+                $b = $newcolor['blue'];
 
                 $gratio=120;
                 $bratio=100;
@@ -53,12 +80,28 @@ function _effect_distort($image,$factor=40,$grad=1) {
                 $blue = (int)($newY/$height*$bgatio+$b/255*$pratio);
                 $new = imagecolorallocate($canvas, $red, $green, $blue);
                 $new = imagecolorclosest($canvas, $red, $green, $blue);
+
+                
                 imageSetPixel($canvas,$x,$y,$new);
             } else {
-                imageSetPixel($canvas,$x,$y,$col);
+                if ($newX <0 || $newY < 0 || $newX >= $width-1 || $newY >= $height-1) {
+                    continue;
+                }
+
+                $newcolor = _antialias_color($image, $x, $y, $newX, $newY);
+                // ignore background 
+                if ($newcolor === false) continue;
+
+                $red = $newcolor['red'];
+                $green = $newcolor['green'];
+                $blue = $newcolor['blue'];
+                $new = imagecolorallocate($canvas, $red, $green, $blue);
+                $new = imagecolorclosest($canvas, $red, $green, $blue);
+                imageSetPixel($canvas,$x,$y,$new);
             }
         }
     }
+
     imageCopy($image,$canvas,0,0,0,0,$width,$height);
 }
 
@@ -98,6 +141,51 @@ function _effect_wave($image) {
     imageCopy($image,$canvas2,0,0,0,0,$width,$height+5);
 }
 
+/**
+ * Please http://www.captcha.ru/captchas/multiwave/
+ * get antialiasing color of the new position base on the source img coord.
+ * rewriten for non truecolor img by wkpark at kldp.org
+ *
+ * @author wkpark@kldp.org
+ * @since  2010/08/26
+ */
+function _antialias_color($img, $x, $y, $sx, $sy) {
+    $frsx = $sx - floor($sx);
+    $frsy = $sy - floor($sy);
+    $frsx1 = 1 - $frsx;
+    $frsy1 = 1 - $frsy;
+
+    $color = array();
+    $color[] = imagecolorat($img, $sx, $sy) & 0xFF;
+    $color[] = imagecolorat($img, $sx+1, $sy) & 0xFF;
+    $color[] = imagecolorat($img, $sx, $sy+1) & 0xFF;
+    $color[] = imagecolorat($img, $sx+1, $sy+1) & 0xFF;
+
+    $test = 0;
+    foreach ($color as $c) {
+        $test+= $c;
+    }
+    // is it background color
+    if ($test == 0) return false;
+
+    $cc = array('red'=>array(), 'green'=>array(), 'blue'=>array());
+    foreach ($color as $c) {
+        $i = imagecolorsforindex($img, $c);
+        $cc['red'][] = $i['red'];
+        $cc['green'][] = $i['green'];
+        $cc['blue'][] = $i['blue'];
+    }
+
+    $newcolor = array();
+    foreach ($cc as $k=>$v) {
+        $newcolor[$k] = $v[0] * $frsx1 * $frsy1
+                + $v[1] * $frsx * $frsy1
+                + $v[2] * $frsx1 * $frsy
+                + $v[3] * $frsx * $frsy;
+        if ($newcolor[$k] > 255) $newcolor[$k] = 255;
+    }
+    return $newcolor;
+}
 
 function _effect_blur($image,$color,$dx=1,$dy=0) {
 // please see http://www.hudzilla.org/phpbook/read.php/11_2_23
@@ -210,7 +298,7 @@ function do_ticket($formatter,$options) {
         //$w=$size[4]+20; # margin=20 ?
         $w=$pointsize*$word_length + $margin;
         $h=$pointsize+$margin;
-        if ($DBInfo->use_ticket & 7) $h+=$pointsize/3;
+        if ($DBInfo->use_ticket & 23) $h+=$pointsize/3;
     } else {
         $FONT=5; // giant
         if (!empty($DBInfo->ticket_gdfont))
@@ -219,7 +307,6 @@ function do_ticket($formatter,$options) {
         $h=imagefontheight($FONT)+10;
     }
 
-    Header("Content-type: image/png");
     $im= ImageCreate($w,$h);
     $color=array();
     if (isset($DBInfo->captcha_bgcolor) and preg_match('/^#[0-9a-fA-F]$/', $DBInfo->captcha_bgcolor)) {
@@ -236,6 +323,7 @@ function do_ticket($formatter,$options) {
     $pen1=rand(3,19);
     for ($i=0;$i<18;$i++)
         $color[]= ImageColorAllocate($im,rand(100,200),rand(100,200),rand(100,200));
+
     if (!empty($use_ttf)) {
         $sx=$margin;
         $sy=$margin/2+$pointsize;
@@ -252,12 +340,24 @@ function do_ticket($formatter,$options) {
     if ($DBInfo->use_ticket & 8) $grad=1;
     if ($DBInfo->use_ticket & 4)
         _effect_distort($im,$pointsize,$grad);
+    else if ($DBInfo->use_ticket & 16)
+        _effect_distort($im,$pointsize,$grad, 1);
     if ($DBInfo->use_ticket & 1)
         _effect_blur($im,$color,1,1);
     if ($DBInfo->use_ticket & 2)
         _effect_grid($im,$color,$pen1);
 
-    ImagePng($im);
+    if (function_exists("imagepng")) {
+	header("Content-Type: image/png");
+	imagepng($im);
+    } else if(function_exists("imagegif")) {
+	header("Content-Type: image/gif");
+	imagegif($im);
+    } else if(function_exists("imagejpeg")) {
+        $jpeg_quality = 5;
+	header("Content-Type: image/jpeg");
+	imagejpeg($im, null, $jpeg_quality);
+    }
     ImageDestroy($im);
 }
 
