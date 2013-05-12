9f559111045c6850c8ef36d7e43e1112ba0d3714
Won-Kyu Park <wkpark@gmail.com>
2013-05-13 02:20:04 +0900

    some locale translations are included from the ToolbarPlugin

--- local/js/locale/dummy.js
+++ local/js/locale/dummy.js
@@ -12,3 +12,24 @@ N_('Printing...');
 N_("copy to clipboard");
 N_("view plain");
 N_("Toggle line numbers");
+
+// Toolbar
+N_('Bold text');
+N_('Italic text');
+N_('Internal link');
+N_('Link title');
+N_('External link (remember http:// prefix)');
+N_('http://www.example.com link title');
+N_('Level 2 headline');
+N_('Headline text');
+N_('Mathematical formula (LaTeX)');
+N_('Insert latex formula here');
+N_('Ignore wiki formatting');
+N_('Insert non-formatted text here');
+N_('Horizontal line (use sparingly)');
+N_('Embedded image');
+N_('Media file link');
+N_('Smiley');
+N_('Your signature with timestamp');
+N_('Click a button to get an example text');
+N_('Please enter the text you want to be formatted.\\\\n It will be shown in the infobox for copy and pasting.\\\\nExample:\\\\n\$1\\\\nwill become:\\\\n\$2');
