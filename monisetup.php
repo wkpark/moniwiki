25cd0765e06351f7849c958f9624ba78a8f77dfb
Won-Kyu Park <wkpark@gmail.com>
2013-05-15 01:53:54 +0900

    monisetup: set the $path config option for win32 packaging

--- monisetup.php
+++ monisetup.php
@@ -91,7 +91,9 @@ class MoniConfig {
       // http://kldp.net/forum/message.php?msg_id=7675
       // http://bugs.php.net/bug.php?id=22418
       //$config['version_class']="'RcsLite'";
-      $config['path']="./bin;c:/program files/vim/vimXX";
+      $config['path'] = './bin;';
+      if (is_dir('../bin')) $config['path'].= '../bin;'; // packaging for win32
+      $config['path'].= 'c:/program files/vim/vimXX';
       $config['vartmp_dir']="c:/tmp/";
     } else {
       $config['rcs_user']='root'; // XXX
