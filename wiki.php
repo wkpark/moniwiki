84eae712d51f7b3ba881d92bb03e2550514e28cb
Won-Kyu Park <wkpark@gmail.com>
2013-05-17 00:15:47 +0900

    bump up the revision to 1.2.0-RC3

--- wiki.php
+++ wiki.php
@@ -13,8 +13,8 @@
 //
 // $Id: wiki.php,v 1.639 2011/08/09 13:51:53 wkpark Exp $
 //
-$_revision = substr('$Revision: 1.714 $',1,-1);
-$_release = '1.2.0-RC2';
+$_revision = substr('$Revision: 1.730 $',1,-1);
+$_release = '1.2.0-RC3';
 
 #ob_start("ob_gzhandler");
 
