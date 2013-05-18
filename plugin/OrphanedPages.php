fb8f97307a3defc3d0e4b5edf1ca83989ba718e4
Won-Kyu Park <wkpark@gmail.com>
2013-05-18 16:45:18 +0900

    OrphanedPages plugin: check backlinks instead of pagelinks and paginate orphaned pages

--- plugin/OrphanedPages.php
+++ plugin/OrphanedPages.php
@@ -18,29 +18,70 @@
 function macro_OrphanedPages($formatter,$value, $params = array()) {
     global $DBInfo;
 
+    // set as dynamic macro
+    if ($formatter->_macrocache and empty($params['call']))
+        return $formatter->macro_cache_repl('OrphanedPages', $value);
+
     $pagelinks = $formatter->pagelinks; // save
     $save = $formatter->sister_on;
     $formatter->sister_on = 0;
 
-    $pages = $DBInfo->getPageLists();
-    $orphaned = array_flip($pages);
-    $cache = new Cache_text('pagelinks');
+    $offset = 0;
+    if (!empty($params['offset'])) {
+        if (is_numeric($params['offset']) and $params['offset'] > 0)
+            $offset = $params['offset'];
+    }
+    $param = array();
+    if (!empty($offset)) $param['offset'] = $offset;
+    $pages = $DBInfo->getPageLists($param);
+
+    $start = 0;
+    if (!empty($params['start']) and is_numeric($params['start'])) {
+        if ($params['start'] > 0) $start = $params['start'];
+    }
+
+    // set time_limit
+    $mt = explode(' ', microtime());
+    $timestamp = $mt[0] + $mt[1];
+    $j = 0;
+
+    $time_limit = isset($DBInfo->process_time_limit[0]) ?
+            $DBInfo->process_time_limit : 3; // default 3-seconds
+
+    $orphaned = array();
+    $cache = new Cache_text('backlinks');
+    set_time_limit(0);
+    $j = 0;
+    $i = 1;
     foreach ($pages as $page_name) {
         $links = $cache->fetch($page_name);
-        if (empty($links)) continue;
-        foreach ($links as $link) {
-            if (isset($orphaned[$link]))
-                unset($orphaned[$link]);
+        if (empty($links)) {
+            $orphaned[$page_name] = $i;
+            if ($i++ == $pagecount) break;
+        }
+        $j++;
+
+        // check time_limit
+        if ($time_limit and ++$j % 20 == 0) {
+            $mt = explode(' ', microtime());
+            $now = $mt[0] + $mt[1];
+            if ($now - $timestamp > $time_limit) break;
         }
     }
     $out = '';
+    $j+= $offset + 1;
+    $i+= $start - 1;
+
+    $start = ' start="'.$start.'"';
 
     $orphaned = array_flip($orphaned);
-    $out = "<ol>\n";
+    $out = "<ol$start>\n";
     foreach ($orphaned as $page) {
         $out.= "<li>" . $formatter->link_tag($page, '', htmlspecialchars($page)) . "</li>\n";
     }
     $out.= "</ol>\n";
+    $out.= $formatter->link_to("?action=orphanedpages&amp;offset=$j&amp;start=$i", _("Show next page"));
+
     return $out;
 }
 
