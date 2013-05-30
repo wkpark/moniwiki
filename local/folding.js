f9a949cfff0591959ad5c181fba3424bb8b77850
Won-Kyu Park <wkpark@gmail.com>
2013-05-31 06:13:33 +0900

    SectionFolding: dynamic section folding

--- local/folding.js
+++ local/folding.js
@@ -1,9 +1,9 @@
 /*
  * Builti-in section folding for MoniWiki
  * 
- * $Id$
  */
 
+(function() {
 function foldingSection(btn, id)
 {
     var sect;
@@ -26,22 +26,10 @@ function foldingSection(btn, id)
         }
     }
 
-    if (typeof Effect != 'undefined') { // prototype.js
-        var dur = 0.5;
-        if (toggle) {
-          new Effect.SlideDown(sect, { duration: dur, afterFinish: function() {Element.show(sect);} });
-        } else {
-          new Effect.SlideUp(sect, { duration: dur, afterFinish: function() {Element.hide(sect);} });
-        }
-    } else if (typeof MooTools != 'undefined') { // get sectpages for the first time.
-        var mySlide = new Fx.Slide(sect); // mootools
-        mySlide.toggle();
+    if (sect.style.display == 'none') {
+        sect.style.display = 'block';
     } else {
-        if (sect.style.display == 'none') {
-            sect.style.display = 'block';
-        } else {
-            sect.style.display = 'none';
-        }
+        sect.style.display = 'none';
     }
     if (icon) {
         var name=icon.getAttribute('class');
@@ -55,4 +43,55 @@ function foldingSection(btn, id)
     }
 }
 
+function hideSections(hide) {
+    var content = document.getElementById('wikiContent');
+    if (!content) return;
+    var sects = content.getElementsByClassName('section');
+    var is_mobile = /iphone|android/i.test(navigator.userAgent);
+
+    if (typeof hide == 'undefined') {
+        if (is_mobile) hide = 'none';
+        else hide = '';
+    }
+
+    for (var i = 0; i < sects.length; i++) {
+        var head;
+        for (head = sects[i].firstChild; head; head = head.nextSibling) {
+            if (head.nodeType != 3 && head.tagName.match(/^H/)) break;
+        }
+        if (head == null) continue; // ignore
+        var div;
+        for (div = head.nextSibling; div; div = div.nextSibling) {
+            if (div.nodeType != 3 && div.tagName == 'DIV') break;
+        }
+
+        if (div.childNodes.length == 0) continue;
+        if (typeof div.firstChild == 'undefined') continue;
+        if (div.childNodes.length == 1 && div.firstChild.nodeType == 3)
+            continue;
+
+        head.onclick = (function(obj, id) { return function() { foldingSection(obj, id); }; })(head, div);
+        var icon = new Image();
+        if (hide != '') {
+            icon.src = _url_prefix + '/imgs/misc/open.png';
+            icon.setAttribute('class', 'close');
+        } else {
+            icon.src = _url_prefix + '/imgs/misc/close.png';
+        }
+        if (is_mobile)
+            head.appendChild(icon);
+        else
+            head.insertBefore(icon, head.firstChild);
+
+        div.style.display = hide;
+    }
+}
+
+// onload
+var oldOnload = window.onload;
+window.onload = function(ev) {
+    try { oldOnload(); } catch(e) {};
+    hideSections();
+}
+})();
 // vim:et:sts=4:sw=4:
