3cdd59ae9925a6b444c45475dd39fe509dc8c5bb
Won-Kyu Park <wkpark@gmail.com>
2013-09-27 21:52:06 +0900

    Fetch plugin: more fixup the fetch plugin
     - $fetch_timeout config option added
     - show error msg for image mimetype using gd functions
     - check $formatter->refresh
     - return retval correctly
     - use set_time_limit(0)
     - enable macro [[Fetch(url)]]

--- plugin/fetch.php
+++ plugin/fetch.php
@@ -16,6 +16,7 @@
 // Param: fetch_max_size=3*1024*1024
 // Param: fetch_maxage=24*60*60
 // Param: fetch_action=http://foo.bar/wiki.php?action=fetch&url=
+// Param: fetch_timeout=15
 // Param: fetch_use_cache_url=0
 //
 // Usage:[[Fetch(url)]] or ?action=fetch&url=http://...
@@ -37,48 +38,94 @@ function do_fetch($formatter, $params = array()) {
         return;
     }
 
-    $err = '';
-    $params['err'] = &$err;
+    $ret = array();
+    $params['retval'] = &$ret;
     $params['call'] = true;
+    if ($formatter->refresh) $params['refresh'] = 1;
     macro_Fetch($formatter, $url, $params);
 
-    if (!empty($params['err'])) {
-        echo $err;
-        return;
+    if (!empty($ret['error'])) {
+        if (!empty($ret['mimetype']) and preg_match('/^image\//', $ret['mimetype'])) {
+            $font_size = 2;
+            $str = 'ERROR: '.$ret['error'];
+            $w = imagefontwidth($font_size) * strlen($str);
+            $h = imagefontheight($font_size);
+            $im = ImageCreate($w, $h);
+            ImageColorAllocate($im, 255, 255, 255); // white background
+            ImageColorAllocate($im, 0, 0, 0); // black
+            ImageString($im, $font_size, 0, 0, $str, 1);
+
+            if (function_exists("imagepng")) {
+                header("Content-Type: image/png");
+                imagepng($im);
+            } else if(function_exists("imagegif")) {
+                header("Content-Type: image/gif");
+                imagegif($im);
+            } else if(function_exists("imagejpeg")) {
+                $jpeg_quality = 5;
+                header("Content-Type: image/jpeg");
+                imagejpeg($im, null, $jpeg_quality);
+            }
+            ImageDestroy($im);
+            return;
+        }
     }
+    echo $ret['error'];
 }
 
 function macro_Fetch($formatter, $url = '', $params = array()) {
     global $DBInfo;
 
     if (empty($url)) {
-        $params['err'] = _("Empty URL");
-        return null;
+        $params['retval']['error'] = _("Empty URL");
+        return false;
     }
 
     require_once "lib/HTTPClient.php";
 
     $sz = 0;
 
-    // check if it is valid or not
-    if (!preg_match('/^(https?|ftp):\/\/.*\.(jpe?g|gif|png)(?:\?|&)?/i', $url)) {
-        if (empty($DBInfo->fetch_mime_check)) {
-            $params['err'] = _("Is it valid image ? file URL ?");
-            return null;
-        }
+    $allowed = 'png|jpeg|jpg|gif';
+    if (!empty($DBInfo->fetch_exts)) {
+        $allowed = $DBInfo->fetch_exts;
     }
 
+    // check if it is valid or not
+    if (!preg_match('/^(?:https?|ftp):\/\/.*\.('.$allowed.')(?:\?|&)?/i', $url, $m)) {
+        if (empty($DBInfo->fetch_mime_check)) {
+            $params['retval']['error'] = _("Is it a valid fetch type ?");
+            return false;
+        }
+    }
+    $ext = '.'.$m[1];
+
     // set default params
-    $maxage = !empty($DBInfo->fetch_maxage) ? $DBInfo->fetch_maxage : 60*60*24*7;
+    $maxage = !empty($DBInfo->fetch_maxage) ? (int) $DBInfo->fetch_maxage : 60*60*24*7;
+    $timeout = !empty($DBInfo->fetch_timeout) ? (int) $DBInfo->fetch_timeout : 15;
 
     // check connection
     $http = new HTTPClient();
     $sc = new Cache_text('fetchinfo');
 
+    $error = null;
+
     if ($sc->exists($url) and $sc->mtime($url) < time() + $maxage) {
         $info = $sc->fetch($url);
         $sz = $info['size'];
         $mimetype = $info['mimetype'];
+        $error = !empty($info['error']) ? $info['error'] : null;
+
+        // already retrived and found some error
+        if (empty($params['refresh']) and !empty($error)) {
+            $params['retval']['status'] = $info['status'];
+            $params['retval']['error'] = $error;
+            $params['retval']['mimetype'] = $mimetype;
+            $params['retval']['size'] = $sz;
+            return false;
+        }
+
+        // do not refresh for no error cases
+        if (empty($error)) unset($params['refresh']);
     } else {
         // get file header
         $http->nobody = true;
@@ -88,8 +135,8 @@ function macro_Fetch($formatter, $url = '', $params = array()) {
         //
         //}
         if ($http->status != 200) {
-            $params['err'] = sprintf(_("Invalid Status %d"), $http->status);
-            return null;
+            $params['retval']['error'] = sprintf(_("Invalid Status %d"), $http->status);
+            return false;
         }
 
         if (isset($http->resp_headers['content-length']))
@@ -106,31 +153,50 @@ function macro_Fetch($formatter, $url = '', $params = array()) {
     // size info
     if (is_numeric($sz)) {
         $unit = array('Bytes', 'KB', 'MB', 'GB');
+        $tmp = $sz;
         for ($i = 0; $i < 4; $i++) {
-            if ($sz <= 1024) {
+            if ($tmp <= 1024) {
                 break;
             }
-            $sz = $sz / 1024;
+            $tmp = $tmp / 1024;
         }
-        $hsz = round($sz, 2).' '.$unit[$i];
+        $hsz = round($tmp, 2).' '.$unit[$i];
 
-        if (!empty($DBInfo->fetch_max_size)) {
-            if ($sz > $DBInfo->fetch_max_size) {
-                $params['err'] = sprintf(_("Fetch file size %s is too big. Please contact WikiMasters to increase \$fetch_max_size"), $hsz);
-                return null;
-            }
+        if (!empty($DBInfo->fetch_max_size) and $sz > $DBInfo->fetch_max_size) {
+            $params['retval']['error'] = sprintf(_("Too big file size (%s). Please contact WikiMasters to increase \$fetch_max_size"), $hsz);
+            $params['retval']['mimetype'] = $mimetype;
+            return false;
         }
     } else {
-        $params['err'] = _("Can't get file size info");
-        return null;
+        $params['retval']['error'] = _("Can't get file size info");
+        $params['retval']['mimetype'] = $mimetype;
+        return false;
     }
 
-    if (!preg_match('/^image\/(jpe?g|gif|png)$/', $mimetype, $m)) {
-        // always check the content-type
-        $params['err'] = sprintf(_("Invalid mime-type %s"), $mimetype);
-        return null;
+    $is_image = false;
+    if (preg_match('/^image\/(jpe?g|gif|png)$/', $mimetype, $m)) {
+        $ext = isset($m[1]) ? '.'.$m[1] : '';
+        $is_image = true;
     }
-    $ext = isset($m[1]) ? '.'.$m[1] : '';
+
+    if (!empty($DBInfo->fetch_images_only) and !$is_image) {
+        // always check the content-type
+        $params['retval']['error'] = sprintf(_("Invalid mime-type %s"), $mimetype);
+        $params['retval']['mimetype'] = $mimetype;
+        return false;
+    }
+
+    if (empty($params['call'])) {
+        if ($is_image) {
+            $img_url = $formatter->link_url('', '?action=fetch&amp;url='.$url);
+            return '<div class="externalImage"><img src="'.$img_url.'">'.
+                '<div><a href="'.$url.'"><span>['.strtoupper($m[1]).' external image'.
+                ' ('. $hsz.')'.
+                ']</span></a></div>';
+        }
+
+        return $formatter->link_to('?action=fetch&amp;url='.$url, $mimetype.' ('. $hsz.')');
+    }
 
     // cache dir/filename/cache url
     if (!empty($DBInfo->cache_public_dir) and
@@ -150,11 +216,11 @@ function macro_Fetch($formatter, $url = '', $params = array()) {
     }
 
     // real fetch job.
-    if (!file_exists($fetchfile)) {
+    if (!empty($params['refresh']) or !file_exists($fetchfile)) {
         $fp = fopen($fetchfile, 'w');
         if (!is_resource($fp)) {
-            $params['err'] = sprintf(_("Fail to open %s"), $fetchfile);
-            return null;
+            $params['retval']['error'] = sprintf(_("Fail to open %s"), $fetchfile);
+            return false;
         }
 
         // retry to get all info
@@ -162,15 +228,39 @@ function macro_Fetch($formatter, $url = '', $params = array()) {
         //if (!empty($DBInfo->fetch_max_size)) {
         //    $http->max_bodysize = $DBInfo->fetch_max_size;
         //}
+
+        $save = ini_get('max_execution_time');
+        set_time_limit(0);
+        $http->timeout = $timeout;
         $http->sendRequest($url, array(), 'GET');
+        set_time_limit($save);
+
+        if ($http->status != 200) {
+            fclose($fp);
+            unlink($fetchfile);
+
+            // Error found! save error status to the info cache
+            $params['retval']['status'] = sprintf(_("Invalid Status %d"), $http->status);
+            $params['retval']['error'] = $http->error;
+            $params['retval']['mimetype'] = $mimetype;
+            $params['retval']['size'] = $sz;
+            $sc->update($url, array('size'=>$sz,
+                        'mimetype'=>$mimetype,
+                        'error'=>$http->error,
+                        'status'=>$params['retval']['status']));
+            return false;
+        }
 
         if (!empty($http->resp_body)) {
             fwrite($fp, $http->resp_body);
         }
         fclose($fp);
-    }
 
-    if (empty($params['call'])) return null;
+        // update error status.
+        if (!empty($error))
+            $sc->update($url, array('size'=>$sz,
+                        'mimetype'=>$mimetype));
+    }
 
     if (!empty($fetch_url) and !empty($DBInfo->fetch_use_cache_url)) {
         $formatter->send_header(array('Status: 302', 'Location: '.$fetch_url));
