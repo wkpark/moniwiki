c5d2062d5872ca9efaa952e8228431278d52008a
Won-Kyu Park <wkpark@gmail.com>
2013-10-04 19:36:42 +0900

    release 1.2.1

--- wiki.php
+++ wiki.php
@@ -13,8 +13,8 @@
 //
 // $Id: wiki.php,v 1.639 2011/08/09 13:51:53 wkpark Exp $
 //
-$_revision = substr('$Revision: 1.913 $',1,-1);
-$_release = '1.2.1-RC3';
+$_revision = substr('$Revision: 1.919 $',1,-1);
+$_release = '1.2.1';
 
 #ob_start("ob_gzhandler");
 
