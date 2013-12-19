018a3de120857669b91a59426f96b7fc282d9402
Won-Kyu Park <wkpark@gmail.com>
2013-12-19 18:44:06 +0900

    HTML processor: parse width,height params correctly

--- plugin/processor/html.php
+++ plugin/processor/html.php
@@ -17,13 +17,24 @@
 //
 // $Id: html.php,v 1.1 2008/11/27 01:12:24 wkpark Exp $
 
-function processor_html($formatter="",$value="") {
+function processor_html($formatter, $value = '') {
     if ($value[0]=='#' and $value[1]=='!')
         list($line,$value)=explode("\n",$value,2);
 
+    // check some iframes
     if (preg_match("/^\s*<(?:iframe|object)/", $value) and preg_match("@</(?:iframe|object)>\s*$@", $value)) {
         if (preg_match("@https?://(?:[a-z-]+[.])?youtube(?:[.][a-z-]+)+/(?:watch[?].*v=|v/|embed/)([a-z0-9_-]+)@i", $value, $m)) {
-            return $formatter->macro_repl('Play', $m[0]);
+            $val = $value;
+            // parse width,height
+            if (preg_match_all("/(?:width|height)=(['\"])?\d+(?:px)?\\1/", $value, $match)) {
+                if (!empty($match[0]))
+                $arg = array();
+                foreach ($match[0] as $v) $arg[] = $v;
+                $args = implode(',', $arg);
+                $val = $m[0];
+                if (!empty($args)) $val.= ','.$args;
+            }
+            return $formatter->macro_repl('Play', $val);
         }
     }
     return $value;
