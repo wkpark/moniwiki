b85ccdb1ac1dd39e01494e77e59dc47a75a7203d
Won-Kyu Park <wkpark@gmail.com>
2013-12-26 01:46:01 +0900

    ImageFileSize plugin: use fetch cache to retrieve size info
    and make it dynamic macro

--- plugin/ImageFileSize.php
+++ plugin/ImageFileSize.php
@@ -14,21 +14,35 @@
 // Usage: [[ImageUrlSize(url)]]
 //
 
-function macro_ImageFileSize($formatter, $value = '') {
+function macro_ImageFileSize($formatter, $value = '', $params = array()) {
     global $Config;
 
     if (empty($value)) return '';
 
-    require_once "lib/HTTPClient.php";
-
     $sz = 0;
     // check if it is valid or not
-    if (preg_match('/^(https?|ftp):\/\/.*(\.(:jpg|jpeg|gif|png))?(?:\?|&)?/', $value)) {
+    while (preg_match('/^(https?|ftp):\/\/.*(\.(:jpg|jpeg|gif|png))?(?:\?|&)?/', $value)) {
+        $value = _urlencode($value);
+
+        // check the file size saved by the fetch plugin
+        $si = new Cache_text('fetchinfo');
+
+        if ($si->exists($value) && ($info = $si->fetch($value)) !== false) {
+            $sz = $info['size'];
+            break;
+        }
+
         $sc = new Cache_text('imagefilesize');
 
         if ($sc->exists($value) and $sc->mtime($value) < time() + 60*60*24*20) {
             $sz = $sc->fetch($value);
         } else {
+            // dynamic macro
+            if ($formatter->_macrocache and empty($params['call']))
+                return $formatter->macro_cache_repl('ImageFileSize', $value);
+
+            require_once dirname(__FILE__).'/../lib/HTTPClient.php';
+
             $http = new HTTPClient();
 
             // set referrer
@@ -57,7 +71,9 @@ function macro_ImageFileSize($formatter, $value = '') {
                 $sz = $http->resp_headers['content-length'];
             $sc->update($value, $sz);
         }
-
+        break;
+    }
+    if ($sz > 0) {
         $unit = array('Bytes', 'KB', 'MB', 'GB');
         for ($i = 0; $i < 4; $i++) {
           if ($sz <= 1024) {
