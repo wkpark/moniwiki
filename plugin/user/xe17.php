062e109be4eebfe065b0d6602d20479f396bf0da
Won-Kyu Park <wkpark@gmail.com>
2015-04-30 10:42:42 +0900

    make xe17 user module faster, support the varnish cache server

--- plugin/user/xe17.php
+++ plugin/user/xe17.php
@@ -1,16 +1,18 @@
 <?php
-// Copyright 2013 Won-Kyu Park <wkpark at kldp.org>
+// Copyright 2013-2015 Won-Kyu Park <wkpark at kldp.org>
 // All rights reserved. Distributable under GPL see COPYING
 // a sample plugin for the MoniWiki
 //
 // Author: Won-Kyu Park <wkpark@kldp.org>
 // Since: 2013-07-04
+// Modified: 2015-04-30
 // Name: XE 1.7 User plugin
 // Description: XE 1.7 user plugin
 // URL: MoniWiki:XeUserPlugin
-// Version: $Revision: 1.0 $
+// Version: $Revision: 1.5 $
 // License: GPL
 //
+// Param: xe_root_dir='/home/path_to_the_root_of_installed_xe/'; # default xe root path
 // Usage: set $user_class = 'xe17'; in the config.php
 //
 
@@ -40,31 +42,85 @@ class User_xe17 extends WikiUser {
                     array(&$oSessionController, 'gc')
            );
         }
-        session_start();
-
-        if ($sess = $_POST[session_name()]) {
-            session_id($sess);
-        }
     }
 
     function User_xe17($id = '') {
         global $DBInfo;
 
-        define('__XE__', true);
+        $this->css = isset($_COOKIE['MONI_CSS']) ? $_COOKIE['MONI_CSS'] : '';
+        $this->theme = isset($_COOKIE['MONI_THEME']) ? $_COOKIE['MONI_THEME'] : '';
+        $this->bookmark = isset($_COOKIE['MONI_BOOKMARK']) ? $_COOKIE['MONI_BOOKMARK'] : '';
+        $this->trail = isset($_COOKIE['MONI_TRAIL']) ? _stripslashes($_COOKIE['MONI_TRAIL']) : '';
+        $this->tz_offset = isset($_COOKIE['MONI_TZ']) ?_stripslashes($_COOKIE['MONI_TZ']) : '';
+        $this->nick = isset($_COOKIE['MONI_NICK']) ?_stripslashes($_COOKIE['MONI_NICK']) : '';
+        if ($this->tz_offset == '') $this->tz_offset = date('Z');
 
-        $zbxe_root_dir = !empty($DBInfo->zbxe_root_dir) ?
-                $DBInfo->zbxe_root_dir : __DIR__.'/../../../xe'; //"/home/httpd/xe/"; // XE root dir
+        $cookie_id = '';
+        // get the current Cookie vals
+        if (isset($_COOKIE['MONI_ID'])) {
+            $this->ticket = substr($_COOKIE['MONI_ID'], 0, 32);
+            $cookie_id = urldecode(substr($_COOKIE['MONI_ID'], 33));
+        }
 
-        require_once($zbxe_root_dir."/config/config.inc.php");
+        // is it a valid user ?
+        $udb = new UserDB($DBInfo);
 
-        $context = &Context::getInstance();
-        $this->xe_context_init($context); // simplified init context method
-        //$context->init(); // very slow
+        $update = false;
+        if (!empty($cookie_id)) {
+            $tmp = $udb->getUser($cookie_id);
 
-        $oMemberModel = &getModel('member');
-        $oMemberController = &getController('member');
+            // not found
+            if ($tmp->id == 'Anonymous') {
+                $update = true;
+                $cookie_id = '';
+            } else {
+                // check ticket
+                $ticket = getTicket($tmp->id, $_SERVER['REMOTE_ADDR']);
+                if ($this->ticket != $ticket) {
+                    // not a valid user
+                    $this->ticket = '';
+                    $this->setID('Anonymous');
+                    $update = true;
+                    $cookie_id = '';
+                } else {
+                    // OK good user
+                    $this->setID($cookie_id);
+                    $id = $cookie_id;
+                }
+            }
+        } else {
+            // empty cookie
+            $update = true;
+        }
 
-        if ($oMemberModel->isLogged()) {
+        $sessid = session_name(); // PHPSESSID
+        // set the session_id() using saved cookie
+        if (isset($_COOKIE[$sessid])) {
+            session_id($_COOKIE[$sessid]);
+        }
+
+        // do not use cookies for varnish cache server
+        ini_set("session.use_cookies", 0);
+        session_cache_limiter('');
+        session_start();
+
+        // set xe_root_dir config option
+        $xe_root_dir = !empty($DBInfo->xe_root_dir) ?
+                $DBInfo->xe_root_dir : dirname(__FILE__).'/../../../xe';
+        // default xe_root_dir is 'xe' subdirectory of the parent dir of the moniwiki
+
+        if ($update && !empty($_SESSION['is_logged'])) {
+            define('__XE__', true);
+
+            require_once($xe_root_dir."/config/config.inc.php");
+
+            $context = &Context::getInstance();
+            $this->xe_context_init($context); // simplified init context method
+            // $context->init(); // slow slow
+
+            $oMemberModel = &getModel('member');
+            $oMemberController = &getController('member');
+
             $oMemberController->setSessionInfo();
             $member = new memberModel();
             $info = $member->getLoggedInfo();
@@ -75,36 +131,30 @@ class User_xe17 extends WikiUser {
             $udb = new UserDB($DBInfo);
             $tmp = $udb->getUser($id);
 
-            // get timezone and make timezone offset
-            $tz_offset = date('Z');
-            $update = 0;
-            if ($tz_offset != $tmp->info['tz_offset'])
-                $update = 1;
-
-            // not registered user ?
-            if ($update or empty($tmp->info['nick'])) {
-                // automatically save/register user
-                $tmp->info['tz_offset'] = $tz_offset;
-                $tmp->info['nick'] = $info->nick_name;
-                $udb->saveUser($tmp);
+            // not a registered user ?
+            if ($tmp->id == 'Anonymous' || $update || empty($tmp->info['nick'])) {
+                $this->setID($id);
+                $this->tz_offset = $tz_offset;
+                $this->info['nick'] = $info->nick_name;
+                $this->info['tz_offset'] = $tz_offset;
             }
         } else {
-            $this->setID('Anonymous');
+            // not logged in
+            if (empty($_SESSION['is_logged'])) {
+                if (!empty($cookie_id))
+                    header($this->unsetCookie());
+                $this->setID('Anonymous');
+                $id = 'Anonymous';
+            }
         }
 
-        $this->css = isset($_COOKIE['MONI_CSS']) ? $_COOKIE['MONI_CSS']:'';
-        $this->theme = isset($_COOKIE['MONI_THEME']) ? $_COOKIE['MONI_THEME']:'';
-        $this->bookmark = isset($_COOKIE['MONI_BOOKMARK']) ? $_COOKIE['MONI_BOOKMARK']:'';
-        $this->trail = isset($_COOKIE['MONI_TRAIL']) ? _stripslashes($_COOKIE['MONI_TRAIL']):'';
-        $this->tz_offset = isset($_COOKIE['MONI_TZ']) ?_stripslashes($_COOKIE['MONI_TZ']):'';
-        $this->nick = isset($_COOKIE['MONI_NICK']) ?_stripslashes($_COOKIE['MONI_NICK']):'';
-        if ($this->tz_offset == '') $this->tz_offset = date('Z');
+        if ($update || !empty($id) and $id != 'Anonymous') {
+            if ($cookie_id != $id)
+                header($this->setCookie());
 
-        if (!empty($id) and $id != 'Anonymous') {
-            $udb = new UserDB($DBInfo);
-
-            if (!$udb->_exists($id)) {
-	        $dummy = $udb->saveUser($this);
+            if ($update || !$udb->_exists($id)) {
+                // automatically save/register user
+                $dummy = $udb->saveUser($this);
             }
         }
     }
