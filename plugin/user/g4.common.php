9feb48a72106be5073e5058d00de466ad9c4cee7
Won-Kyu Park <wkpark@gmail.com>
2015-05-07 17:11:46 +0900

    update g4 plugin for Gnuboard v5

--- plugin/user/g4.common.php
+++ plugin/user/g4.common.php
@@ -1,21 +1,34 @@
 <?php
 /**
  * simplified common.php by wkpark @ gmail.com
- * 2015/04/29
+ * since 2015/04/29
  */
 
 function g4_get_member() {
-    global $g4, $g4_root_dir;
+    global $g4, $g5, $g4_root_dir;
 
     // init
     $member = array();
 
-    include_once("$g4_root_dir/lib/constant.php");  // constants
+    if (!defined('G5_VERSION')) {
+        include_once("$g4_root_dir/lib/constant.php");  // constants
+    }
     include_once("$g4_root_dir/lib/common.lib.php"); // common library
-    include_once("$g4_root_dir/dbconfig.php"); // db configs
+    if (defined('G5_VERSION')) {
+        include_once("$g4_root_dir/data/dbconfig.php"); // db configs
+    } else {
+        include_once("$g4_root_dir/dbconfig.php"); // db configs
+    }
 
-    $connect_db = sql_connect($mysql_host, $mysql_user, $mysql_password);
-    $select_db = sql_select_db($mysql_db, $connect_db);
+    if (defined('G5_VERSION')) {
+        $connect_db = sql_connect(G5_MYSQL_HOST, G5_MYSQL_USER, G5_MYSQL_PASSWORD);
+        $select_db  = sql_select_db(G5_MYSQL_DB, $connect_db);
+        $g5['connect_db'] = $connect_db;
+        sql_query(" set names utf8 ");
+    } else {
+        $connect_db = sql_connect($mysql_host, $mysql_user, $mysql_password);
+        $select_db = sql_select_db($mysql_db, $connect_db);
+    }
 
     // is it a valid PHPSESSID ?
     if ($_REQUEST['PHPSESSID'] && $_REQUEST['PHPSESSID'] != session_id()) {
--- plugin/user/g4.php
+++ plugin/user/g4.php
@@ -5,10 +5,11 @@
 //
 // Author: Won-Kyu Park <wkpark@kldp.org>
 // Since: 2015-04-29
+// Date: 2015-05-07
 // Name: GnuBoard4 User plugin
 // Description: GnuBoard4 user plugin
 // URL: MoniWiki:GnuBoard4UserPlugin
-// Version: $Revision: 1.5 $
+// Version: $Revision: 1.6 $
 // License: GPL
 //
 // Param: g4_root_dir = '/home/path_to_g4_installed_dir/';
@@ -33,6 +34,8 @@ class User_g4 extends WikiUser {
         $g4_root_dir = !empty($DBInfo->g4_root_dir) ?
                 $DBInfo->g4_root_dir : __DIR__.'/../../../gb4';
 
+        $g5_path = array();
+        $g5_path['path'] = realpath($g4_root_dir);
         include_once("$g4_root_dir/config.php"); // g4 config file
 
         ini_set("url_rewriter.tags", "");
@@ -44,7 +47,10 @@ class User_g4 extends WikiUser {
         //ini_set("session.gc_divisor", 100); // default
 
         session_set_cookie_params(0, "/");
-        ini_set("session.cookie_domain", $g4['cookie_domain']);
+        if (defined('G5_VERSION'))
+            ini_set("session.cookie_domain", G5_COOKIE_DOMAIN);
+        else
+            ini_set("session.cookie_domain", $g4['cookie_domain']);
         // do not use cookies for varnish cache server
         ini_set("session.use_cookies", 0);
 
@@ -93,6 +99,8 @@ class User_g4 extends WikiUser {
                     // OK good user
                     $this->setID($cookie_id);
                     $id = $cookie_id;
+                    $this->nick = $user->info['nick'];
+                    $this->tz_offset = $user->info['tz_offset'];
                 }
             }
         } else {
@@ -110,10 +118,16 @@ class User_g4 extends WikiUser {
                 if ($user->id == 'Anonymous' || $update || empty($user->info['nick'])) {
                     $this->setID($id);
 
-                    $this->tz_offset = $tz_offset;
-                    $this->nick = $member['mb_nick'];
-                    $this->info['nick'] = $member['mb_nick'];
-                    $this->info['tz_offset'] = $tz_offset;
+                    if (isset($member['mb_nick']) and $this->nick != $member['mb_nick']) {
+                        // G4
+                        $this->info['nick'] = $member['mb_nick'];
+                        $this->nick = $member['mb_nick'];
+                    } else if (isset($member['nick']) and $this->nick != $member['nick']) {
+                        // G5
+                        $this->info['nick'] = $member['nick'];
+                        $this->nick = $member['nick'];
+                    }
+                    $this->info['tz_offset'] = $this->tz_offset;
                 }
             }
         } else {
@@ -128,18 +142,18 @@ class User_g4 extends WikiUser {
 
         // update timezone
         if ($this->tz_offset != $user->info['tz_offset']) {
-            $this->info['tz_offset'] = $tz_offset;
+            $this->info['tz_offset'] = $this->tz_offset;
             $update = true;
         }
 
         if ($update || !empty($id) and $id != 'Anonymous') {
             if ($cookie_id != $id)
                 header($this->setCookie());
+        }
 
-            if ($update || !$udb->_exists($id)) {
-                // automatically save/register user
-                $dummy = $udb->saveUser($this);
-            }
+        if ($update || !$udb->_exists($id)) {
+            // automatically save/register user
+            $dummy = $udb->saveUser($this);
         }
     }
 }
