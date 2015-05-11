6d7289606c4317691390749263ae847e55baf98b
Won-Kyu Park <wkpark@gmail.com>
2015-05-11 23:01:22 +0900

    fixed #22 - base64url pagekey class added

---
+++ lib/pagekey.base64url.php
@@ -0,0 +1,39 @@
+<?php
+//
+// MoniWiki PageKey class
+//
+// @since  2015/05/09
+// @author wkpark@kldp.org
+//
+
+require_once(dirname(__FILE__).'/pagekey.base.php');
+
+class PageKey_base64url extends PageKey_base {
+    var $DB;
+
+    function PageKey_base64url($DB) {
+        $this->DB = &$DB;
+    }
+
+    function getPageKey($pagename) {
+        $name = $this->_getPageKey($pagename);
+        return $this->DB->text_dir . '/' . $name;
+    }
+
+    function pageToKeyname($pagename) {
+        return $this->_getPageKey($pagename);
+    }
+
+    function keyToPagename($key) {
+        $pagename = base64_decode(strtr($key, '-_', '+/').(($m = strlen($key) % 4) > 0 ? substr('====', $m) : ''));
+        $pagename = strtr($pagename, array("\x0a" => "\x1a")); // HACK "%0a" char bug
+        return $pagename;
+    }
+
+    // normalize a pagename to uniq key
+    function _getPageKey($pagename) {
+        return rtrim(strtr(base64_encode($pagename), '+/', '-_'), '=');
+    }
+}
+
+// vim:et:sts=4:sw=4:
