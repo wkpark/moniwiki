db53160c525d5287bd899603c66f8b1c9edd02a6
Won-Kyu Park <wkpark@gmail.com>
2015-05-13 20:17:29 +0900

    fixed #18: merge some feature from the revert.php

--- plugin/reverse.php
+++ plugin/reverse.php
@@ -1,16 +1,17 @@
 <?php
 // Copyright 2010 Apple.D
 // All rights reserved. Distributable under GPL see COPYING
-// a sample plugin for the MoniWiki
+// a revert plugin for the MoniWiki
 //
 // Author: Apple.D
-// Date: 2010-01-01
+// Since: 2010-01-01
 // Name: Revert plugin
 // Description: a enha version of the Revert Plugin
-// URL: to_plugin url/interwiki name etc.
-// Version: $Revision: 1.0 $
-// License: GPL
+// URL: MoniWiki:ReversePlugin
+// Version: $Revision: 1.1 $
+// License: GPLv2
 //
+// Usage: ?action=reverse&rev=1.100
 
 function do_reverse($formatter, $options = array()) {
     global $DBInfo;
@@ -19,25 +20,54 @@ function do_reverse($formatter, $options = array()) {
 
     $pagename = $formatter->page->urlname;
 
+    $force = 1;
+    if (isset($_POST['rev'][0]) && $DBInfo->hasPage($options['page'])) {
+        $force = 0;
+        if ($_POST['force']) $force = 1;
+    }
+
     // validate rev
-    if (!empty($options['rev'])) {
-        $info = $formatter->page->get_info($options['rev']);
+    $rev = isset($_POST['rev'][0]) ? $_POST['rev'] : $options['rev'];
+    if (!empty($rev)) {
+        $info = array();
+        if (preg_match('/^[a-zA-Z0-9\.]+$/', $rev))
+            $info = $formatter->page->get_info($rev);
         if (empty($info[0])) {
+            // no version found
+            unset($rev);
             unset($options['rev']);
-            if (!empty($_POST['rev'])) unset($_POST['rev']);
+            unset($_POST['rev']);
         }
     }
 
-    if (!empty($pagename) and $pagename == $options['name'] and !empty($options['rev'])) {
-        $formatter->page->body = $formatter->page->get_raw_body($options);
-        $ret = $DBInfo->savePage($formatter->page,
-                sprintf(_("Rollback to revision %s"), $options['rev']).
-                (!empty($options['comment']) ? ': '.$options['comment'] : ''), $options);
+    // check ticket
+    $ticket = getTicket($formatter->page->mtime().$options['id'].$_SERVER['REMOTE_ADDRESS']);
+    if ($force and !empty($pagename) and !empty($_POST['rev']) and $ticket == $options['ticket']) {
+        // simple comment check
+        $comment = trim($options['comment']);
+        $default = sprintf(_("Rollback to revision %s"), $rev);
+        if (isset($comment[0]) && ($p = strpos($comment, $default)) === 0) {
+            $comment = substr($comment, strlen($default));
+            $comment = trim($comment);
+            $comment = ltrim($comment, ': ');
+        }
+        $comment = isset($comment[0]) ? $default.': '.$comment : $default;
 
-        if ($ret != -1) {
-            $title = sprintf(_("%s is successfully rollbacked."), $page->name);
+        // get current revision
+        $current_body = $formatter->page->_get_raw_body();
+        // get old revision
+        $body = $formatter->page->get_raw_body($options);
+
+        if ($body == $current_body) {
+            $title = sprintf(_("No change found."));
         } else {
-            $title = sprintf(_("Failed to rollback %s page"), $page->name);
+            $formatter->page->write($body);
+            $ret = $DBInfo->savePage($formatter->page, $comment, $options);
+            if ($ret != -1) {
+                $title = sprintf(_("%s is successfully rollbacked."), _html_escape($page->name));
+            } else {
+                $title = sprintf(_("Failed to rollback %s page"), _html_escape($page->name));
+            }
         }
         $formatter->send_header('', $options);
         $formatter->send_title($title, '', $options);
@@ -45,22 +75,47 @@ function do_reverse($formatter, $options = array()) {
         return;
     }
 
-    if (empty($options['rev'])) {
-        $title = _("Please select the target revision first.");
-        $formatter->send_header('', $options);
-        $formatter->send_title($title, '', $options);
-        $formatter->send_footer('', $options);
-        return;
+    $extra = '';
+    if (empty($options['rev']))
+        $title = _("Please select old revision to revert.");
+    else {
+        if ($DBInfo->hasPage($formatter->page->name)) {
+            if ($_POST['rev'])
+                $title = sprintf(_("Please check force overwrite to revert %s revision."),
+                    $rev);
+            else
+                $title = sprintf(_("Are you really want to overwrite %s page to %s revision ?"),
+                    $options['page'], $rev);
+            $extra = '<input type="checkbox" name="force" />'._("Force overwrite").'<br />';
+        } else {
+            $title = sprintf(_("Are you really want to revert %s page to %s revision ?"),
+                $options['page'], $rev);
+        }
     }
-
-    $title = sprintf(_("Are you sure to revert %s page to %s revision ?"), $options['page'], $options['rev']);
 
     $formatter->send_header('', $options);
     $formatter->send_title($title, '', $options);	
 
-    $msg = _("Summary");
-    $ok = _("OK");
-    print "<form method='post'><span>$msg: </span><input name='comment' size='80' maxlength='80' value='' /><input type='hidden' name='action' value='reverse' /><input type='hidden' name='rev' value='".$options['rev']."' /><input type='hidden' name='name' value='".$pagename."' /><input type='submit' value='$ok' /></form>";
+    if ($rev) {
+        $msg = _("Summary");
+        $btn = _("Revert page");
+        $comment = sprintf(_("Rollback to revision %s"), $rev);
+        $hidden = '<input type="hidden" name="ticket" value="'.$ticket.'" />';
+        echo "<form method='post'>\n",
+            "<span>$msg: </span><input name='comment' size='80' maxlength='80' value='$comment: ' />\n",
+            "<input type='hidden' name='action' value='reverse' />\n",
+            "<input type='hidden' name='rev' value='".$rev."' />\n",
+            $hidden,
+            "<br /><input type='submit' value='$btn' />$extra\n",
+            "</form>";
+    }
+
+    $params = array();
+    $params['page'] = $options['page'];
+    $params['info_actions'] = array('recall'=>'view','reverse'=>'revert');
+    $params['title'] = '<h3>'.
+        sprintf(_("Old Revisions of the %s"),_html_escape($formatter->page->name)).'</h3>';
+    echo $formatter->macro_repl('Info', '', $params);
 
     $formatter->send_footer('', $options);
 }
