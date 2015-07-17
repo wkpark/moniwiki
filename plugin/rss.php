f501afa51641e8ef1ad65c6a0a11d01f9899f516
Won-Kyu Park <wkpark@gmail.com>
2015-07-17 23:10:27 +0900

    RSS macro: support $date_fmt, $title_width options etc.
     * parse pubDate.
     * cleanup html using list

--- plugin/rss.php
+++ plugin/rss.php
@@ -10,8 +10,20 @@ class WikiRSSParser {
    var $description = "";
    var $link = "";
    var $date = "";
+   var $date_fmt = '[m-d h:i a]';
+   var $title_width = 0; // do not cut title
 
-   function WikiRSSParser() {
+   function WikiRSSParser($charset = 'UTF-8') {
+       $this->tz_offset = date('Z');
+       $this->charset = $charset;
+   }
+
+   function setDateFormat($format) {
+       $this->date_fmt = $format;
+   }
+
+   function setTitleWidth($width) {
+       $this->title_width = $width;
    }
 
    function startElement($parser, $tagName, $attrs) {
@@ -27,10 +39,16 @@ class WikiRSSParser {
 
    function endElement($parser, $tagName) {
        if ($tagName == "ITEM") {
-           if ($this->status) print "[$this->status] ";
-           printf("<a href='%s' target='_content'>%s</a>",
+           //if ($this->status) print "[$this->status] ";
+
+           $title = trim($this->title);
+           if (!empty($this->title_width) && function_exists('mb_strimwidth')) {
+             $title = mb_strimwidth($title, 0, $this->title_width, '...', $this->charset);
+           }
+           $title = sprintf("<a href='%s' title='%s' target='_content'>%s</a>",
              trim($this->link),
-             _html_escape(trim($this->title)));
+             _html_escape($this->title),
+             _html_escape($title));
            #printf("<p>%s</p>",
            #  _html_escape(trim($this->description)));
            if ($this->date) {
@@ -40,10 +58,10 @@ class WikiRSSParser {
              # http://www.w3.org/TR/NOTE-datetime
              $zone=str_replace(":","",substr($date,19));
              $time=strtotime(substr($date,0,19).$zone);
-             $date=date("@ m-d [h:i a]",$time);
-             printf(" %s<br />\n", _html_escape(trim($date)));
-           } else
-             printf("<br />\n");
+             $date = date($this->date_fmt, $time);
+           }
+           echo '<li>', $date, ' ', $title, '</li>',"\n";
+
            $this->title = "";
            $this->description = "";
            $this->link = "";
@@ -68,6 +86,14 @@ class WikiRSSParser {
                case "DC:DATE":
                $this->date .= $data;
                break;
+               case "PUBDATE":
+               $data = trim($data);
+               if (empty($data))
+                   break;
+               $time = strtotime($data);
+               $date = gmdate("Y-m-d\TH:i:s", $time + $this->tz_offset).'+00:00';
+               $this->date = $date;
+               break;
                case "WIKI:STATUS":
                $this->status .= $data;
                break;
@@ -89,16 +115,37 @@ function do_Rss($formatter,$options) {
 function macro_Rss($formatter,$value) {
   global $DBInfo;
 
-  $xml_parser = xml_parser_create();
-
-  $rss_parser = new WikiRSSParser();
-  xml_set_object($xml_parser,$rss_parser);
-  xml_set_element_handler($xml_parser, "startElement", "endElement");
-  xml_set_character_data_handler($xml_parser, "characterData");
+  $rsscache = new Cache_text('rsscache');
 
   $key=_rawurlencode($value);
+  // rss TTL = 7200 sec (60*60*2)
+  if (!$formatter->refresh && $rsscache->exists($key) && time() < $rsscache->mtime($key) + 7200) {
+    return $rsscache->fetch($key);
+  }
 
-  $cache= new Cache_text("rss");
+  if ($rsscache->exists($key.'.lock')) {
+    return '';
+  }
+
+  $args = explode(',', $value);
+
+  // first arg assumed to be a date fmt arg
+  $date_fmt = '';
+  $title_width = 0;
+  if (count($args) > 1) {
+    if (preg_match("/^[\s\/\-:aABdDFgGhHiIjmMOrSTY\[\]]+$/", $args[0])) {
+      $date_fmt = array_shift($args);
+    }
+    if (is_numeric($args[0])) {
+      $title_width = array_shift($args);
+      // too small or too big
+      if ($title_width < 10 || $title_width > 40)
+        $title_width = 0;
+    }
+    $value = implode(',', $args);
+  }
+
+  $cache= new Cache_text("rssxml");
   # reflash rss each 7200 second (60*60*2)
   if (!$cache->exists($key) or (time() > $cache->mtime($key) + 7200 )) {
     $fp = @fopen("$value","r");
@@ -111,12 +158,26 @@ function macro_Rss($formatter,$value) {
   } else
     $xml_data=$cache->fetch($key);
 
+  // detect charset
+  $charset = 'UTF-8';
   list($line,$dummy)=explode("\n",$xml_data,2);
   preg_match("/\sencoding=?(\"|')([^'\"]+)/",$line,$match);
   if ($match) $charset=strtoupper($match[2]);
-  else $charset='UTF-8';
-  # override $charset for php5
+
+  // override $charset for php5
   if ((int)phpversion() >= 5) $charset='UTF-8';
+
+  $xml_parser = xml_parser_create();
+
+  $rss_parser = new WikiRSSParser($charset);
+  if (!empty($date_fmt))
+    $rss_parser->setDateFormat($date_fmt);
+  if (!empty($title_width))
+    $rss_parser->setTitleWidth($title_width);
+
+  xml_set_object($xml_parser,$rss_parser);
+  xml_set_element_handler($xml_parser, "startElement", "endElement");
+  xml_set_character_data_handler($xml_parser, "characterData");
 
   ob_start();
   $ret= xml_parse($xml_parser, $xml_data);
@@ -128,12 +189,16 @@ function macro_Rss($formatter,$value) {
   $out=ob_get_contents();
   ob_end_clean();
   xml_parser_free($xml_parser);
+  $out = '<ul class="rss">'.$out.'</ul>';
 
   #  if (strtolower(str_replace("-","",$options['oe'])) == 'euckr')
   if (function_exists('iconv') and strtoupper($DBInfo->charset) != $charset) {
     $new=iconv($charset,$DBInfo->charset,$out);
     if ($new !== false) return $new;
   }
+
+  $rsscache->remove($key.'.lock');
+  $rsscache->update($key, $out);
 
   return $out;
 }
