a552d5f196c94b9d5e83290bebfdc6b59b0a0ab5
Won-Kyu Park <wkpark@gmail.com>
2015-09-25 17:53:32 +0900

    fixed #50: refactoring WantedPages pluin: make watedpages scalable using offset, limit params.

--- plugin/WantedPages.php
+++ plugin/WantedPages.php
@@ -1,57 +1,104 @@
 <?php
-// Copyright 2003-2006 Won-Kyu Park <wkpark at kldp.org>
-// All rights reserved. Distributable under GPL see COPYING
+// Copyright 2003-2015 Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPLv2 see COPYING
 // a WantedPages macro plugin for the MoniWiki
+//
+// Author: Won-Kyu Park <wkpark@kldp.org>
+// Since: 2003-08-15
+// Date: 2015-09-25
+// Name: Wanted Pages Plugin
+// Description: Wanted Pages of this Wiki
+// URL: MoniWiki:WantedPagesPlugin
+// Version: $Revision: 1.8 $
+// License: GPLv2
+//
+// Usage: [[WantedPages]]
 //
 // $Id: WantedPages.php,v 1.7 2010/10/05 22:28:54 wkpark Exp $
 
-function macro_WantedPages($formatter,$value='') {
-  global $DBInfo;
-  $pages = $DBInfo->getPageLists();
+function macro_WantedPages($formatter, $value = '', $params = array()) {
+    global $DBInfo;
 
-  $pagelinks=$formatter->pagelinks; // save
-  $save=$formatter->sister_on;
-  $formatter->sister_on=0;
+    // set as dynamic macro
+    if ($formatter->_macrocache and empty($params['call']))
+        return $formatter->macro_cache_repl('WantedPages', $value);
 
-  $cache=new Cache_text("pagelinks");
+    // set default page_limit
+    if (empty($params['limit']))
+        $params['limit'] = 100;
 
-  foreach ($pages as $page) {
-    $dum='';
-    $p= new WikiPage($page);
-    $f= new Formatter($p);
-    $pi=$f->page->get_instructions($dum);
-    if (!in_array($pi['#format'], array('wiki', 'monimarkup'))) continue;
-    $links=$f->get_pagelinks();
-    if ($links) {
-      $lns=&$links;
-      foreach($lns as $link) {
-        if (!$link or $DBInfo->hasPage($link)) continue;
-        if ($link and !$wants[$link])
-          $wants[$link]="[\"$page\"]";
-        else $wants[$link].=" [\"".$page."\"]";
-      }
+    $offset = 0;
+    if (!empty($params['offset'])) {
+        if (is_numeric($params['offset']) and $params['offset'] > 0)
+            $offset = $params['offset'];
     }
-  }
-  $formatter->pagelinks=$pagelinks; // save
-  $formatter->sister_on=$save;
-  if (!count($wants)) return '';
-  $pagelinks=$formatter->pagelinks; // save
-  $formatter->sister_on=0;
 
-  asort($wants);
-  $out="<ul>\n";
-  while (list($name,$owns) = each($wants)) {
-    $owns=str_replace('<','&lt;',$owns);
-    $nowns = preg_replace_callback("/(".$formatter->wordrule.")/",
-      array(&$formatter, 'link_repl'), $owns);
-    $out.="<li>".$formatter->link_repl($name,_html_escape($name)). ": $nowns</li>";
-  }
-  $out.="</ul>\n";
-  $formatter->sister_on=$save;
-  $formatter->pagelinks=$pagelinks; // save
+    $param = array();
+    if (!empty($offset)) $param['offset'] = $offset;
+    $param['limit'] = $params['limit'];
 
-  return $out;
+    $pages = $DBInfo->getPageLists($param);
+
+    $pagelinks = $formatter->pagelinks; // save
+    $save = $formatter->sister_on;
+    $formatter->sister_on = 0;
+
+    $cache = new Cache_text('pagelinks');
+
+    $j = 0;
+    foreach ($pages as $page) {
+        $dum = '';
+        $p = new WikiPage($page);
+        $f = new Formatter($p);
+        $pi = $f->page->get_instructions($dum);
+        if (!in_array($pi['#format'], array('wiki', 'monimarkup'))) continue;
+        $links = $f->get_pagelinks();
+        if ($links) {
+            $lns = &$links;
+            foreach($lns as $link) {
+                if (empty($link) or $DBInfo->hasPage($link)) continue;
+                if (empty($wants[$link]))
+                    $wants[$link] = array('[["'.$page.'"]]');
+                else
+                    $wants[$link][] = '[["'.$page.'"]]';
+            }
+        }
+        $j++;
+    }
+    if (!count($wants)) return '';
+    $pagelinks = $formatter->pagelinks; // save
+    $formatter->sister_on = 0;
+
+    asort($wants);
+
+    $out = "<ul>\n";
+    $old_owns = null;
+    foreach ($wants as $name=>$owns) {
+        if ($old_owns != $owns) {
+            $olinks = array_map(array($formatter, 'link_repl'), $owns);
+            $olink = implode(', ', $olinks);
+            if ($old_owns)
+                $out.= "</ul>\n</li>\n";
+            $out.= "<li>\n".$olink.'<ul>';
+            $old_owns = $owns;
+        }
+        $out.= '<li>'.$formatter->link_repl($name, _html_escape($name)).'</li>'."\n";
+    }
+    $out.= "</ul>\n</li>\n</ul>\n";
+
+    $out.= $formatter->link_to("?action=wantedpages&amp;offset=$j", _("Show next page"));
+
+    $formatter->sister_on = $save;
+    $formatter->pagelinks = $pagelinks; // restore
+
+    return $out;
 }
 
-// vim:et:sts=2:
-?>
+function do_wantedpages($formatter, $params) {
+    $formatter->send_header('', $params);
+    $formatter->send_title('', '', $params);
+    echo macro_WantedPages($formatter, $params['sec'], $params);
+    $formatter->send_footer($args, $params);
+}
+
+// vim:et:sts=4:sw=4:
