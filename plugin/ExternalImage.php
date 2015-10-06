90b36cf28722689db2ea1cd5350769323253ed03
Won-Kyu Park <wkpark@gmail.com>
2015-10-06 17:47:35 +0900

    fixed #76: add a simple ExternalImage macro plugin

---
+++ plugin/ExternalImage.php
@@ -0,0 +1,34 @@
+<?php
+// Copyright 2003-2015 Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPL see COPYING
+// a simple ExternalImage plugin for the MoniWiki
+//
+// Author: Won-Kyu Park <wkpark at kldp.org>
+// Date: 2015-10-05
+// Name: External Image Plugin
+// Description: External Image Plugin
+// URL: MoniWiki:ExternalImage
+// Version: $Revision: 1.0 $
+// License: GPLv2
+//
+// Usage: [[ExternalImage(url)]]
+//
+
+function macro_ExternalImage($formatter, $value, $params = array()) {
+    // Wikimedia Commons cases
+    // http://upload.wikimedia.org/wikipedia/commons/5/55/APS_underwater_rifle_REMOV.jpg
+    // http://upload.wikimedia.org/wikipedia/commons/e/e6/Aps_protei_ida71_00.jpg
+    // http://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/3-Tastenmaus_Microsoft.jpg/250px-3-Tastenmaus_Microsoft.jpg
+
+    if (preg_match('@^https?://upload\.wikimedia\.org/wikipedia/commons/(thumb/)?./../([^/]+\.(?:gif|jpe?g|png|svg))(?(1)/(\d+px)-\2)@', $value, $m)) {
+        $args = '';
+        if (!empty($m[3]))
+            $args.= ',width='.$m[3];
+        return $formatter->macro_repl('WikimediaCommons', $m[2].$args, $params);
+    }
+
+
+    return $value;
+}
+
+// vim:et:sts=4:sw=4:
