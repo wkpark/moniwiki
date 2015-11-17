cc865c4ff5de200e38c6f5d48eba78ce6e49a575
Won-Kyu Park <wkpark@gmail.com>
2015-11-17 22:00:19 +0900

    Atom plugin: support conditional get, caching, $atom_ttl option added

--- plugin/atom.php
+++ plugin/atom.php
@@ -1,14 +1,15 @@
 <?php
-// Copyright 2006 Won-Kyu Park <wkpark at kldp.org>
-// All rights reserved. Distributable under GPL see COPYING
+// Copyright 2003-2015 Won-Kyu Park <wkpark at kldp.org>
+// All rights reserved. Distributable under GPLv2 see COPYING
 // a atom action plugin for the MoniWiki
 //
-// Date: 2006-08-15
+// Since: 2006-08-15
+// LastModified: 2015-11-17
 // Name: Atom feeder
 // Description: Atom Plugin
 // URL: MoniWiki:AtomPlugin
-// Version: $Revision: 1.3 $
-// License: GPL
+// Version: $Revision: 1.4 $
+// License: GPLv2
 //
 // $Id: atom.php,v 1.3 2010/07/09 11:03:27 wkpark Exp $
 // $orig Id: rss_rc.php,v 1.12 2005/09/13 09:10:52 wkpark Exp $
@@ -25,11 +26,68 @@ function do_atom($formatter,$options) {
   $options['quick']=1;
   if ($options['c']) $options['items']=$options['c'];
   $lines= $DBInfo->editlog_raw_lines($days,$options);
-    
+
+  // HTTP conditional get
+  $mtime = $DBInfo->mtime();
+  $lastmod = gmdate('D, d M Y H:i:s \G\M\T', $mtime);
+  $cache_ttl = !empty($DBInfo->atom_ttl) ? $DBInfo->atom_ttl : 60*30; /* 30 minutes */
+
+  // make etag based on some options and mtime.
+  $check_opts = array('quick', 'items', 'c');
+  $check = array();
+  foreach ($check_opts as $c) {
+    if (isset($options[$c])) $check[$c] = $options[$c];
+  }
+
+  $etag = md5($mtime . $DBInfo->logo_img . serialize($check) . $cache_ttl . $options['id']);
+
+  $headers = array();
+  $headers[] = 'Pragma: cache';
+  $maxage = $cache_ttl;
+
+  $public = 'public';
+  if ($options['id'] != 'Anonymous')
+    $public = 'private';
+
+  $headers[] = 'Cache-Control: '.$public.', max-age='.$maxage;
+  $headers[] = 'Last-Modified: '.$lastmod;
+  $headers[] = 'ETag: "'.$etag.'"';
+  $need = http_need_cond_request($mtime, $lastmod, $etag);
+  if (!$need)
+    $headers[] = 'HTTP/1.0 304 Not Modified';
+  foreach ($headers as $h)
+    header($h);
+  if (!$need) {
+    @ob_end_clean();
+    return;
+  }
+
+  $cache = new Cache_Text('atom');
+
+  $cache_delay = min($cache_ttl, 30);
+  $mtime = $cache->mtime($etag);
+
   $time_current= time();
-#  $secs_per_day= 60*60*24;
-#  $days_to_show= 30;
-#  $time_cutoff= $time_current - ($days_to_show * $secs_per_day);
+  $val = false;
+  if (empty($formatter->refresh)) {
+    if (($val = $cache->fetch($etag)) !== false and $DBInfo->checkUpdated($mtime, $cache_delay)) {
+      header("Content-Type: application/xml");
+      echo $val;
+      return;
+    }
+  }
+  // need to update cache
+  if ($val !== false and $cache->exists($etag.'.lock')) {
+    header("Content-Type: application/xml");
+    echo $val.'<!-- cached at '.date('Y-m-d H:i:s', $mtime).' -->';
+    return;
+  }
+  if ($cache->exists($etag.'.lock')) {
+    header("Content-Type: application/xml");
+    echo '';
+    return;
+  }
+  $cache->update($etag.'.lock', array('lock'), 30); // 30s lock
 
   $URL=qualifiedURL($formatter->prefix);
   $img_url=qualifiedURL($DBInfo->logo_img);
@@ -62,7 +120,7 @@ CHANNEL;
     $ed_time= $parts[2];
     $user= $parts[4];
     $user_uri='';
-    if ($DBInfo->hasPage($user)) {
+    if ($user != 'Anonymous' && $DBInfo->hasPage($user)) {
       $user_uri= $formatter->link_url(_rawurlencode($user),"",$user);
       $user_uri='<uri>'.$user_uri.'</uri>';
     }
@@ -138,8 +196,13 @@ CHANNEL;
 -->\n
 HEAD;
   header("Content-Type: application/xml");
-  if ($new) print $head.$new;
-  else print $head.$channel.$updated.$items.$form;
-  print "</feed>\n";
+  $out = '';
+  if ($new) $out = $head.$new;
+  else $out = $head.$channel.$updated.$items.$form;
+  $out.= "</feed>\n";
+  echo $out;
+
+  $cache->update($etag, $out);
+  $cache->remove($etag.'.lock');
 }
 ?>
