cddd000519b1aba349e99deafea7d1ed593edf0a
Won-Kyu Park <wkpark@gmail.com>
2015-12-04 15:45:49 +0900

    fixup #96: update folding.js script

--- local/folding.js
+++ local/folding.js
@@ -1,94 +1,44 @@
-/*
- * Builti-in section folding for MoniWiki
- * 
+/**
+ * section folding script for MoniWiki
+ *
+ * @author   Won-Kyu Park <wkpark at gmail.com>
+ * @since    2007/10/30
+ * @modified 2015/11/30
+ * @license  GPLv2
+ * @depends  jQuery
  */
 
 (function() {
-
-// Add a getElementsByClassName function if the browser doesn't have one
-// Limitation: only works with one class name
-// Copyright: Eike Send http://eike.se/nd
-// License: MIT License
-
-if (!document.getElementsByClassName) {
-    getElementsByClassName = function(search) {
-        var d = document, elements, pattern, i, results = [];
-        if (d.querySelectorAll) { // IE8
-            return d.querySelectorAll("." + search);
-        }
-        if (d.evaluate) { // IE6, IE7
-            pattern = ".//*[contains(concat(' ', @class, ' '), ' " + search + " ')]";
-            elements = d.evaluate(pattern, d, null, 0, null);
-            while ((i = elements.iterateNext())) {
-                results.push(i);
-            }
-        } else {
-            elements = d.getElementsByTagName("*");
-            pattern = new RegExp("(^|\\s)" + search + "(\\s|$)");
-            for (i = 0; i < elements.length; i++) {
-                if ( pattern.test(elements[i].className) ) {
-                    results.push(elements[i]);
-                }
-            }
-        }
-        return results;
-    }
-} else {
-    getElementsByClassName = function(q) { return document.getElementsByClassName(q) };
-}
-
-function foldingSection(btn, id)
+function foldingSection(head, id)
 {
     var sect;
     if (typeof id == 'object') sect = id;
-    else sect=document.getElementById(id);
+    else sect = document.getElementById(id);
     if (!sect) return;
-    var toggle = true;
-    if (sect.style.display != 'none') {
-        toggle = false;
-    }
 
-    var icon=null;
-    if (btn) {
-        icon=btn.getElementsByTagName('i')[0];
-        if (!icon) {
-            icon = document.createElement('i');
-            btn.insertBefore(icon, btn.firstChild);
-            //btn.appendChild(icon);
-        }
-    }
-
-    if (sect.style.display == 'none') {
-        sect.style.display = 'block';
+    if ($(head).hasClass("closed")) {
+        $(head).removeClass("closed");
+        $(head).addClass("opened");
+        $(sect).removeClass("closed");
     } else {
-        sect.style.display = 'none';
-    }
-    if (icon) {
-        var name=icon.getAttribute('class');
-        if (name == 'icon-close') {
-            icon.setAttribute('class','icon-open');
-        } else {
-            icon.setAttribute('class','icon-close');
-        }
+        $(head).removeClass("opened");
+        $(head).addClass("closed");
+        $(sect).addClass("closed");
     }
 }
 
-function hideSections() {
-    var content = document.getElementById('wikiContent');
-    if (!content) return;
-    var sects = getElementsByClassName('section');
-    var is_mobile = /iphone|android/i.test(navigator.userAgent);
-
-    if (is_mobile) hide = 'none';
-    else hide = '';
+function attachFolding() {
+    var sects = $(".section.styling");
 
     for (var i = 0; i < sects.length; i++) {
         var head;
+        // try to find first heading
         for (head = sects[i].firstChild; head; head = head.nextSibling) {
             if (head.nodeType != 3 && head.tagName.match(/^H/)) break;
         }
         if (head == null) continue; // ignore
         var div;
+        // try to find first div
         for (div = head.nextSibling; div; div = div.nextSibling) {
             if (div.nodeType != 3 && div.tagName == 'DIV') break;
         }
@@ -98,24 +48,12 @@ function hideSections() {
         if (div.childNodes.length == 1 && div.firstChild.nodeType == 3)
             continue;
 
-        head.onclick = (function(obj, id) { return function() { foldingSection(obj, id); }; })(head, div);
-        var icon = document.createElement('i');
-        if (hide != '') {
-            icon.setAttribute('class', 'icon-open');
-        } else {
-            icon.setAttribute('class', 'icon-close');
-        }
-        if (is_mobile)
-            head.appendChild(icon);
-        else
-            head.insertBefore(icon, head.firstChild);
-
-        div.style.display = hide;
+        head.onclick = (function(id) { return function() { foldingSection(this,id); }; })(div);
     }
 }
 
 // onload
-if (window.addEventListener) window.addEventListener("load",hideSections,false);
-else if (window.attachEvent) window.attachEvent("onload",hideSections);
+if (window.addEventListener) window.addEventListener("load",attachFolding,false);
+else if (window.attachEvent) window.attachEvent("onload",attachFolding);
 })();
 // vim:et:sts=4:sw=4:
