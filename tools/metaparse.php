d7f104e7b8de97b91c926bb81be21ee930871cf1
Won-Kyu Park <wkpark@gmail.com>
2015-12-10 14:52:54 +0900

    tools/metaparse.php added

---
+++ tools/metaparse.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * metaparse.php
+ *
+ * @author wkpark at kldp.org
+ * @since 2015/12/09
+ * @license GPLv2
+ */
+
+define('INC_MONIWIKI', 1);
+$topdir = realpath(dirname(__FILE__).'/../');
+include_once($topdir.'/wiki.php');
+
+// Start Main
+// check a local config file config.local.php
+if (file_exists($topdir.'/config.local.php'))
+    $Config = getConfig($topdir.'/config.local.php');
+else
+    $Config = getConfig($topdir.'/config.php');
+require_once($topdir.'/wikilib.php');
+require_once($topdir.'/lib/wikiconfig.php');
+require_once($topdir.'/lib/timer.php');
+require_once($topdir.'/lib/win32fix.php');
+require_once($topdir.'/lib/cache.text.php');
+
+include_once(dirname(__FILE__).'/utils.php');
+
+// setup some variables
+$Config = wikiConfig($Config);
+$DBInfo = new WikiDB($Config);
+
+$params = array();
+if (class_exists('Timer')) {
+    $timing = new Timer();
+    $params['timer'] = &$timing;
+    $params['timer']->Check("load");
+}
+
+// get args
+$options = array();
+$options[] = array("i", "interwiki", "interwiki name");
+$options[] = array("w", '', "overwrite");
+$short_opts = ''; // list of short options.
+foreach ($options as $item) {
+    $opt = $item[0];
+    if ($item[1]) { // if long option exists
+        $opt.= ':';
+    }
+    $short_opts.= $opt;
+}
+
+$options[] = array('-help', '', "\tdisplay this message");
+
+if (!empty($argv) && in_array('--help', $argv)) {
+    print "Usage: $argv[0] [option]...\n\n";
+    print "Options:\n";
+    foreach($options as $message) {
+        if ($message[1]) $value = "<$message[1]>";
+        else $value = "";
+        print "\t-$message[0] $value\t$message[2]\n";
+    }
+    exit;
+}
+
+$feedback = ''; // global error messege
+$args = getopt($short_opts);
+
+$overwrite = false;
+if (isset($args['w'])) {
+    $overwrite = true;
+}
+
+if (empty($args['i'])) {
+    $interwiki = 'KoWikiPedia';
+}
+
+// get remain $argv array
+foreach($args as $k=>$v) {
+    while($i = array_search('-'.$k, $argv)) {
+        if ($i)
+            unset($argv[$i]);
+        if (preg_match("/^.*".$k.":.*$/i", $short_opts))
+            unset($argv[$i + 1]);
+    }
+}
+$argv = array_merge($argv);
+
+set_time_limit(0);
+
+// check titleindex list
+$titleindex = $argv[1];
+if (!is_file($titleindex)) {
+    echo "Titleindex file not found\n";
+    exit;
+}
+
+if (empty($DBInfo->metadb)) $DBInfo->initMetaDB();
+if (!method_exists($DBInfo->metadb, 'parse')) {
+    echo "ERROR: 'parse' method is not found\n";
+    exit;
+}
+
+$ret = $DBInfo->metadb->parse($titleindex, $interwiki);
+if ($ret === false) {
+    echo "ERROR: parse error\n";
+    echo "$titleindex, $interwiki\n";
+    exit;
+}
+
+$params['timer']->Check('done');
+echo $params['timer']->Write();
+
+// vim:et:sts=4:sw=4:
