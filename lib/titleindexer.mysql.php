96dd7ec7739309d5c2e0dfdc2187237cf1673f56
Won-Kyu Park <wkpark@gmail.com>
2015-12-16 20:41:45 +0900

    update and cleanup titleindexer.mysql.php
     * use mysql_real_escape_string()
     * $use_regexp option added
     * cleanup

--- lib/titleindexer.mysql.php
+++ lib/titleindexer.mysql.php
@@ -3,23 +3,24 @@
  * A MySQL TitleIndexer
  *
  * @since 2015/05/17
+ * @since 1.2.5
  * @author Won-Kyu Park <wkpark at kldp.org>
  * @license GPLv2
  */
 
 class TitleIndexer_mysql {
-    var $text_dir = '';
     var $conn = NULL;
     var $host = 'localhost';
     var $db = 'moniwiki';
     var $passwd = '';
     var $charset = 'utf-8';
+    var $use_regexp = false;
+    var $pages_limit = 5000;
 
     function TitleIndexer_mysql($name = 'titleindex')
     {
         global $Config;
 
-        $this->text_dir = $Config['text_dir'];
         // setup mysql config
         if (!empty($Config['config_mysql']) and
                 file_exists('config/mysql.'.$Config['config_mysql'].'.php')) {
@@ -36,6 +37,10 @@ class TitleIndexer_mysql {
             $this->user = !empty($Config['mysql_user']) ? $Config['mysql_user'] : 'moniwiki';
             $this->passwd = !empty($Config['mysql_passwd']) ? $Config['mysql_passwd'] : '';
         }
+
+        // set default pages_limit
+        $this->pages_limit = isset($Config['pages_limit']) ?
+                $Config['pages_limit'] : $this->pages_limit;
 
         $this->charset = strtolower($Config['charset']);
         register_shutdown_function(array(&$this,'close'));
@@ -57,6 +62,11 @@ class TitleIndexer_mysql {
         @mysql_query('set names '.$charset);
     }
 
+    function mtime()
+    {
+        return time(); // FIXME
+    }
+
     /**
      * update selected page
      *
@@ -64,8 +74,7 @@ class TitleIndexer_mysql {
      */
     function update($pagename)
     {
-        $ret = $this->addPage($pagename);
-        return $ret;
+        return $this->addPage($pagename);
     }
 
     function init()
@@ -75,19 +84,7 @@ class TitleIndexer_mysql {
 
     function init_module()
     {
-        global $DBInfo;
-
-        // exclusive lock to prevent multiple init() calls
-        $eslock = $DBInfo->vartmp_dir.'/mysql.lock';
-        $lock = @fopen($eslock, 'x');
-        if (is_resource($lock)) {
-            if (flock($lock, LOCK_EX)) {
-                $this->init();
-                flock($lock, LOCK_UN);
-            }
-            fclose($lock);
-            unlink($eslock);
-        }
+        // NOOP
     }
 
     function getPagesByIds($ids)
@@ -97,6 +94,7 @@ class TitleIndexer_mysql {
         // get total pages
         $res = mysql_query('SELECT COUNT(*) from `titleindex`');
         $row = mysql_fetch_array($res);
+        mysql_free_result($res);
         if (!$row)
             return array();
 
@@ -114,6 +112,7 @@ class TitleIndexer_mysql {
             if ($row)
                 $selected[] = $row[0];
         }
+        mysql_free_result($res);
 
         return $selected;
     }
@@ -124,6 +123,7 @@ class TitleIndexer_mysql {
         $this->_connect();
         $res = mysql_query('SELECT COUNT(*) from `titleindex` WHERE `is_deleted` = 0');
         $row = mysql_fetch_array($res);
+        mysql_free_result($res);
         if ($row)
             return $row[0];
         return 0;
@@ -143,9 +143,10 @@ class TitleIndexer_mysql {
         $page = $DBInfo->getPage($pagename);
         $body = $page->_get_raw_body();
         $mtime = $page->mtime();
-        $pgname = mysql_escape_string($pagename);
-        $body = mysql_escape_string($body);
+
         $this->_connect();
+        $pgname = @mysql_real_escape_string($pagename);
+        $body = @mysql_real_escape_string($body);
         $res = mysql_query('SELECT `title`,_id from `titleindex` where `title` = \''.$pgname.'\'');
         $row = mysql_fetch_array($res);
         if ($row) {
@@ -166,15 +167,13 @@ class TitleIndexer_mysql {
 
     function deletePage($pagename)
     {
-        global $DBInfo;
-
         if (!isset($pagename[0])) return false;
 
-        $pgname = mysql_escape_string($pagename);
-
         $this->_connect();
+        $pgname = @mysql_real_escape_string($pagename);
         $res = mysql_query('SELECT title,_id from `titleindex` WHERE `title` = \''.$pgname.'\'');
         $row = mysql_fetch_array($res);
+        mysql_free_result($res);
         if (!$row)
             // not exists
             return false;
@@ -190,19 +189,21 @@ class TitleIndexer_mysql {
         if (!isset($oldname[0])) return false;
         if (!isset($newname[0])) return false;
 
-        $pgname = mysql_escape_string($oldname);
-
         $this->_connect();
+        $pgname = @mysql_real_escape_string($oldname);
+
         $res = mysql_query('SELECT `title`,_id from `titleindex` WHERE `title` = \''.$pgname.'\'');
         $row = mysql_fetch_array($res);
+        mysql_free_result($res);
         if (!$row)
             // page not exists
             return -1;
         $id = $row['_id'];
 
-        $n_pgname = mysql_escape_string($newname);
+        $n_pgname = @mysql_real_escape_string($newname);
         $res = mysql_query('SELECT `title` from `titleindex` WHERE `title` = \''.$n_pgname.'\'');
         $row = mysql_fetch_array($res);
+        mysql_free_result($res);
         if ($row)
             // new page exists
             return -2;
@@ -216,50 +217,87 @@ class TitleIndexer_mysql {
     {
         if (!isset($needle[0])) return false; // null needle
 
-        // Workaround bug: escape \n char
-        $needle = str_replace("\x0a", "\x1a", $needle);
-
-        $pages = array();
-
-        $pre = '%';
-        $suf = '%';
-        if ($needle[0] == '^') {
-            $pre = '';
-            $needle = substr($needle, 1);
-        }
-        if (substr($needle, -1) == '$') {
-            $suf = '';
-            $needle = substr($needle, 0, -1);
-        }
-
-        // FIXME
-        $query = 'SELECT `title` FROM `titleindex` WHERE `title` LIKE \''.$pre.$needle.$suf.'\'';
-        if ($limit > 0)
-                $query.= ' LIMIT '.intval($limit);
-
-        $this->_connect();
-        $res = mysql_query($query);
-        if ($res) {
-            while ($rows = mysql_fetch_row($res)) {
-                $pages[] = $rows[0];
-            }
-        }
-
-        return $pages;
-    }
-
-    function getPages($params) {
-        global $DBInfo;
-
         $offset = 0;
         if (!empty($params['offset']) and
                 is_numeric($params['offset']) and
                 $params['offset'] > 0)
             $offset = $params['offset'];
 
-        // set page_limit
-        $pages_limit = isset($DBInfo->pages_limit) ?
-                $DBInfo->pages_limit : 5000; // 5000 pages
+        // Workaround bug: escape \n char
+        $needle = str_replace("\x0a", "\x1a", $needle);
+
+        $this->_connect();
+
+        $pages = array();
+
+        // MySQL REGEXP is not work with multibyte chars
+        if (!$this->use_regexp) {
+            $search = 'LIKE';
+
+            $pre = '%';
+            $suf = '%';
+            if ($needle[0] == '^') {
+                $pre = '';
+                $needle = substr($needle, 1);
+            }
+            if (substr($needle, -1) == '$') {
+                $suf = '';
+                $needle = substr($needle, 0, -1);
+            }
+
+            $escaped = @mysql_real_escape_string($needle);
+            $expr = $pre.$escaped.$suf;
+        } else {
+            $search = 'REGEXP';
+
+            $pre = '';
+            $suf = '';
+            if ($needle[0] == '^') {
+                $pre = '^';
+                $needle = substr($needle, 1);
+            }
+            if (substr($needle, -1) == '$') {
+                $suf = '$';
+                $needle = substr($needle, 0, -1);
+            }
+
+            $escaped = @mysql_real_escape_string($needle);
+            $expr = $pre.$escaped.$suf;
+        }
+
+        $query = 'SELECT `title` FROM `titleindex` WHERE `title` '.$search.' \''.$expr.'\'';
+        if ($limit > 0)
+            $query.= ' LIMIT '.intval($limit);
+        if ($offset > 0)
+            $query.= ' OFFSET '.$offset;
+
+        $res = mysql_query($query);
+        if ($res) {
+            while ($rows = mysql_fetch_row($res)) {
+                $pages[] = $rows[0];
+            }
+        }
+        mysql_free_result($res);
+
+        // return
+        $info = array();
+        $info['offset'] = $offset;
+        $info['count'] = count($pages);
+        if (isset($params['ret'])) $params['ret'] = $info;
+        else if (isset($params['retval'])) $params['retval'] = $info;
+
+        return $pages;
+    }
+
+    function getPages($params) {
+        $offset = 0;
+        if (!empty($params['offset']) and
+                is_numeric($params['offset']) and
+                $params['offset'] > 0)
+            $offset = $params['offset'];
+
+        // get pages_limit
+        $pages_limit = $this->pages_limit;
 
         $total = $this->pageCount();
         $size = $pages_limit;
@@ -283,7 +321,7 @@ class TitleIndexer_mysql {
                     $against.= ' +'.$incl;
             }
 
-            $against = mysql_escape_string($against);
+            $against = @mysql_real_escape_string($against);
             $query = 'SELECT `title` FROM `titleindex` WHERE MATCH(`body`) '.
                 'AGAINST(\''.$against.'\' IN BOOLEAN MODE)';
 
@@ -292,22 +330,24 @@ class TitleIndexer_mysql {
             $query = 'SELECT `title` FROM `titleindex` ORDER BY `title` DESC ';
         }
         if ($pages_limit > 0)
-                ' LIMIT '.intval($pages_limit);
+            $query .= ' LIMIT '.intval($pages_limit);
 
         if ($offset > 0)
-                ' OFFSET '.intval($offset);
+            $query .= ' OFFSET '.intval($offset);
 
-        $this->_connect();
         $res = mysql_query($query);
         if ($res) {
             while ($rows = mysql_fetch_row($res)) {
                 $pages[] = $rows[0];
             }
         }
+        mysql_free_result($res);
 
+        $info = array();
         $info['offset'] = $offset;
         $info['count'] = count($pages);
         if (isset($params['ret'])) $params['ret'] = $info;
+        else if (isset($params['retval'])) $params['retval'] = $info;
 
         return $pages;
     }
