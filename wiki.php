78bbbadfb8923c5796eb5d635b47952095de2040
Won-Kyu Park <wkpark@gmail.com>
2015-12-23 01:46:17 +0900

    fixup #52 partial revert commit e80e6af827 "support GoogleAds" to simplify parser

--- wiki.php
+++ wiki.php
@@ -3760,10 +3760,6 @@ class Formatter {
 
     $formatter=&$this;
 
-    # google ads
-    $this->noads = !empty($DBInfo->use_google_ads) ? false : true;
-    $this->noads = !empty($this->pi['#noads']) ? $this->pi['#noads'] : $this->noads;
-
     $ii = isset($pi['start_line']) ? $pi['start_line'] : 0;
     if (isset($formatter->pi['#linenum']) and empty($formatter->pi['#linenum']))
       $this->linenum = -99999;
@@ -4149,21 +4145,6 @@ class Formatter {
 
         while($in_div > 0)
           $p_closeopen.=$this->_div(0,$in_div,$div_enclose);
-
-        // show google ads
-        while (!$this->noads && empty($this->preview)) {
-          if ($this->sect_num < 3)
-            break;
-          if ($DBInfo->use_google_ads > 1)
-            $select_sect = $DBInfo->use_google_ads;
-          else
-            $select_sect = 3;
-          if ($select_sect == $this->sect_num) {
-            $p_closeopen.= $this->macro_repl('GoogleAds', '', $options);
-            $this->noads = true;
-          }
-          break;
-        }
 
         // check section styling
         $cls = '';
