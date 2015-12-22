6f01e3d520e7f57086fc4f22484446bb0a725ef0
Won-Kyu Park <wkpark@gmail.com>
2015-12-22 14:45:29 +0900

    fixed #104: fixed heading bug in the table.

--- wiki.php
+++ wiki.php
@@ -4135,7 +4135,7 @@ class Formatter {
       #$line=preg_replace("/(".$wordrule.")/e","\$this->link_repl('\\1')",$line);
 
       # Headings
-      while (preg_match("/(?<!=)(={1,})\s+(.*)\s+\\1\s?$/sm",$line,$m)) {
+      while (!$in_table && preg_match("/(?<!=)(={1,})\s+(.*)\s+\\1\s?$/sm", $line, $m)) {
         if ($in_bq) {
           $dummy = null;
           $line = $this->head_repl(strlen($m[1]), $m[2], $dummy);
